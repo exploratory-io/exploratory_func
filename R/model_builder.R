2f0bcbc01a3647ffc4153dbd3cd6a634ed41884e
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-05-13 22:26:15 -0700

    .source.data class to the column

--- R/model_builder.R
+++ R/model_builder.R
@@ -5,12 +5,13 @@ do_data <- function(funcname) {
   ret <- function(df, ..., keep.source = TRUE){
     loadNamespace("dplyr")
     if (keep.source) {
-      output <- df %>% dplyr::do(.model = do.call(funcname, list(data = ., ...)), .source.data = (.))
+      output <- df  %>%  dplyr::do(.model = do.call(funcname, list(data = ., ...)), .source.data = (.))
     } else {
-      output <- df %>% dplyr::do(.model= do.call(funcname, list(data = ., ...)))
+      output <- df  %>%  dplyr::do(.model= do.call(funcname, list(data = ., ...)))
     }
 
     class(output$.model) <- c("list", paste0("model-", funcname))
+    class(output$.source.data) <- c("list", ".source.data")
     output
   }
   ret
