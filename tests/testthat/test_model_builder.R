1c6ec0fcdb37f9c0352e1d4488c0ac636ae2101e
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-05-16 15:11:49 -0700

    add NA filtering in do_kmeans

--- tests/testthat/test_model_builder.R
+++ tests/testthat/test_model_builder.R
@@ -1,7 +1,11 @@
 context("test model builders")
 
 loadNamespace("exploratory")
-test_df <- data.frame(vec1=seq(10), vec2=10-seq(10), rand=runif(10, min = 0, max=10))
+test_df <- data.frame(
+  vec1=seq(10),
+  vec2=10-seq(10),
+  rand=runif(10, min = 0, max=10),
+  na=as.vector(replicate(5,c(NA,5))))
 
 test_that("test do_glm and broom tidy", {
   if(requireNamespace("broom")){
@@ -43,5 +47,15 @@ test_that("test do_kmeans with invalid argument", {
       expect_equal(e$message, "groups is undefined in the data frame and argument")
       return
     })
+  }
+})
+
+test_that("test do_kmeans ignore NA rows", {
+  if(requireNamespace("broom")){
+    result <- (
+      test_df
+      %>%  do_kmeans(vec1, vec2, na, centers=2, keep.source=TRUE)
+      %>%  broom::augment(.model, data=.source.data))
+    expect_equal(dim(result)[[1]], 5)
   }
 })
