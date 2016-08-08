6b955de2a156fa1063a7a1effed48397b9ef92de
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-08-08 13:25:46 -0700

    add a test for fun.aggregate

--- tests/testthat/test_pairwise.R
+++ tests/testthat/test_pairwise.R
@@ -28,6 +28,20 @@ test_that("test do_cosine_sim.kv", {
   expect_equal(result[1, "sim.value"][[1]], (1*4+2*5+3*6)/sqrt(1^2+2^2+3^2)/sqrt(4^2+5^2+6^2))
 })
 
+test_that("test sparse_cast with duplicate", {
+  test_df <- data.frame(
+    rowname = rep(c("row1", "row02", "row3"), each=3),
+    colname = c("col1", "col1", "col5", "col02", "col3", "col1", "col02", "col4", "col5"),
+    val = seq(9),
+    stringsAsFactors = FALSE
+  )
+  result <- (
+    test_df %>%
+      do_cosine_sim.kv(rowname, colname, val, fun.aggregate=min)
+  )
+  expect_equal(result[1, 3][[1]], (1*6)/sqrt(1^2+3^2)/sqrt(4^2+5^2+6^2))
+})
+
 test_that("test do_cosine_sim.kv with NA value", {
   loadNamespace("dplyr")
   result <- (
