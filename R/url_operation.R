3d9135141f1e5fae1863083d81371bd8f2119782
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-09-12 13:02:04 +0900

    add functions

--- R/url_operation.R
+++ R/url_operation.R
@@ -1,20 +1,77 @@
+#' get domain from url string
+#' @export
+get_url_domain <- function(url) {
+  loadNamespace("urltools")
+  urltools::domain(url)
+}
+
+#' get fragment from url string
+#' @export
+get_url_fragment <- function(url) {
+  loadNamespace("urltools")
+  urltools::fragment(url)
+}
+
+#' get decoded query parameters from url string
+#' @export
+get_url_parameters <- function(url) {
+  loadNamespace("urltools")
+  params <- urltools::parameters(url)
+  # if NA is passed to URLdecode, it is removed, so only not NA values should be used
+  # URLdecode is not vectorized, so vapply has to be used
+  params[!is.na(params)] <- vapply(params[!is.na(params)], URLdecode, FUN.VALUE = "")
+  params
+}
+
+#' get path from url string
+#' @export
+get_url_path <- function(url) {
+  loadNamespace("urltools")
+  urltools::path(url)
+}
+
+#' get port from url string
+#' @export
+get_url_port <- function(url) {
+  loadNamespace("urltools")
+  urltools::port(url)
+}
+
+#' get scheme from url string
+#' @export
+get_url_scheme <- function(url) {
+  loadNamespace("urltools")
+  urltools::scheme(url)
+}
+
 #' get suffix from url string
 #' @export
 get_url_suffix <- function(url) {
+  loadNamespace("urltools")
   urltools::suffix_extract(urltools::url_parse(url)$domain)$suffix
 }
 
 #' get subdomain from url string
 #' @export
 get_url_subdomain <- function(url) {
+  loadNamespace("urltools")
   urltools::suffix_extract(urltools::url_parse(url)$domain)$subdomain
+}
+
+#' get suffix from url string
+#' @export
+get_url_tld <- function(url) {
+  loadNamespace("urltools")
+  urltools::tld_extract(urltools::url_parse(url)$domain)$tld
 }
 
 #' get decoded query parameter from url string
 #' @export
 get_url_param <- function(url, parameter_name){
+  loadNamespace("urltools")
   param <- urltools::param_get(url, parameter_name)[[parameter_name]]
   # if NA is passed to URLdecode, it is removed, so only not NA values should be used
-  param[!is.na(param)] <- URLdecode(param[!is.na(param)])
+  # URLdecode is not vectorized, so vapply has to be used
+  param[!is.na(param)] <- vapply(param[!is.na(param)], URLdecode, FUN.VALUE = "")
   param
 }
--- tests/testthat/test_url_operation.R
+++ tests/testthat/test_url_operation.R
@@ -1,12 +1,73 @@
 context("test url tools wrappers")
 
+test_that("domain", {
+  url <- c(
+    "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    NA
+  )
+  ret <- get_url_domain(url)
+  expect_equal(ret, c("twitter.com", NA))
+})
+
+
+test_that("fragment", {
+  url <- c(
+    "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    "https://en.wikipedia.org/wiki/Aaron_Halfaker?debug=true#test",
+    NA
+  )
+  ret <- get_url_fragment(url)
+  expect_equal(ret, c(NA, "test", NA))
+})
+
+test_that("parameters", {
+  url <- c(
+    "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    "https://en.wikipedia.org/wiki/Aaron_Halfaker?debug=true#test",
+    NA
+  )
+  ret <- get_url_parameters(url)
+  expect_equal(ret, c("ref_src=twsrc^google|twcamp^serp|twgr^author", "debug=true", NA))
+})
+
+test_that("path", {
+  url <- c(
+    "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    "https://twitter.com/reddit?ref_src=test",
+    "http://www.computerhope.com/jargon/num/domains.htm"
+  )
+  ret <- get_url_path(url)
+  expect_equal(ret, c("reddit", "reddit", "jargon/num/domains.htm"))
+})
+
+test_that("port", {
+  url <- c(
+    "https://twitter.com:222/reddit",
+    "https://twitter.com:33/reddit?ref_src=test",
+    "http://www.computerhope.com/jargon/num/domains.htm"
+  )
+  ret <- get_url_port(url)
+  expect_equal(ret, c("222", "33", NA))
+})
+
+test_that("scheme", {
+  url <- c(
+    "https://twitter.com:222/reddit",
+    "not url",
+    "http://www.computerhope.com/jargon/num/domains.htm"
+  )
+  ret <- get_url_scheme(url)
+  expect_equal(ret, c("https", NA, "http"))
+})
+
 test_that("suffix", {
   url <- c(
     "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
-    "http://www.computerhope.com/jargon/num/domains.htm"
+    "http://www.computerhope.com/jargon/num/domains.htm",
+    "http://sample.edu.co"
     )
   ret <- get_url_suffix(url)
-  expect_equal(ret, c("com", "com"))
+  expect_equal(ret, c("com", "com", "edu.co"))
 })
 
 test_that("subdomain", {
@@ -18,11 +79,22 @@ test_that("subdomain", {
   expect_equal(ret, c(NA, "www"))
 })
 
+test_that("top level domain", {
+  url <- c(
+    "https://twitter.com:222/reddit",
+    "http://sample.edu.co",
+    "not url"
+  )
+  ret <- get_url_tld(url)
+  expect_equal(ret, c("com", "co", NA))
+})
+
 test_that("param", {
   url <- c(
     "https://twitter.com/reddit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    "https://twitter.com/reddit?ref_src=test",
     "http://www.computerhope.com/jargon/num/domains.htm"
   )
   ret <- get_url_param(url, "ref_src")
-  expect_equal(ret, c("twsrc^google|twcamp^serp|twgr^author", NA))
+  expect_equal(ret, c("twsrc^google|twcamp^serp|twgr^author", "test", NA))
 })
