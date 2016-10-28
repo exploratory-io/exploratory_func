c9112ad143a7fbbdccb2c45e219280f0b9e6b927
Hidetaka Kojima <hidekoji@gmail.com>
2016-10-27 23:12:11 -0700

    Incorta integration: authenticate and schema API hook

---
+++ R/incorta.R
@@ -0,0 +1,65 @@
+#' API to get JSESSION ID from Incorta
+#' @param server - incorta server (i.e http://<host>:<port>/incorta)
+#' @param tenant - tenant
+#' @param user - user id
+#' @param pass - password.
+#' @export
+getIncortaJSessionID <- function(server, tenant, user, pass){
+  loadNamespace("httr")
+  loadNamespace("stringr")
+  loadNamespace("dplyr")
+  r <- httr::POST(url = stringr::str_c(server, "/authservice/login"), body = list(tenant = tenant, user = user, pass = pass))
+  jsessionId <-  (r$cookies %>% filter(name == "JSESSIONID") %>% select(value))$value
+  jsessionId
+}
+
+#' API to get XSRF-TOKEN from Incorta
+#' @param server - incorta server (i.e http://<host>:<port>/incorta)
+#' @param jsessionId - JSESSIONID.
+#' @export
+getIncortaXsrfToken <- function(server, jsessionId){
+  loadNamespace("httr")
+  loadNamespace("stringr")
+  loadNamespace("dplyr")
+  res <- httr::GET(url = stringr::str_c(server, "/service/user/isLoggedIn"), httr::set_cookies(` JSESSIONID` = jsessionId))
+  xsrfToken <- (res$cookies %>% filter(name == "XSRF-TOKEN") %>% select(value))$value
+  xsrfToken
+}
+
+#' API to get schemas (JSON format) from Incorta
+#' @param server - incorta server (i.e http://<host>:<port>/incorta)
+#' @param jsessionId - JSESSIONID.
+#' @param xsrfToken - XSRF-TOKEN
+#' @export
+getIncortaSchemas <- function(server, jsessionId, xsrfToken){
+  loadNamespace("httr")
+  loadNamespace("stringr")
+  loadNamespace("dplyr")
+  h <- c(xsrfToken)
+  names(h) <- "X-XSRF-TOKEN"
+  resSchemas <- httr::GET(url = str_c(server, "/service/schema/getSchemas"),
+                          httr::set_cookies(` JSESSIONID` = jsessionId, `XSRF-TOKEN` = xsrfToken),
+                          httr::add_headers(.headers = h))
+  schemas <- httr::content(resSchemas)
+  jsonlite::toJSON(schemas)
+}
+
+#' API to get schema (XML string) from Incorta
+#' @param server - incorta server (i.e http://<host>:<port>/incorta)
+#' @param jsessionId - JSESSIONID.
+#' @param xsrfToken - XSRF-TOKEN
+#' @param schemaId - ID of the schema
+#' @export
+getIncortaSchema <- function(server, jsessionId, xsrfToken, schemaId){
+  loadNamespace("httr")
+  loadNamespace("stringr")
+  loadNamespace("dplyr")
+  h <- c(xsrfToken)
+  names(h) <- "X-XSRF-TOKEN"
+  resSchema <- httr::GET(url = str_c(server, "/service/schema/getSchema", "?schemaId=", schemaId),
+                         httr::set_cookies(` JSESSIONID` = jsessionId, `XSRF-TOKEN` = xsrfToken),
+                         httr::add_headers(.headers = h))
+  # convert XML as text
+  schema <- httr::content(resSchema, type = "text", encoding = "UTF-8")
+  schema
+}
