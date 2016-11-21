05c4402ebd17d53cba18ec940fca791b1d659f1d
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-21 22:00:38 +0900

    add keep.source and augment

--- R/build_lm.R
+++ R/build_lm.R
@@ -1,7 +1,13 @@
 #' lm wrapper with do
 #' @return deta frame which has lm model
+#' @param data Data frame to be used as data
+#' @param formula Formula for lm
+#' @param ... Parameters to be passed to lm function
+#' @param keep.source Whether source should be kept in source.data column
+#' @param augment Whether the result should be augmented immediately
+#' @param group_cols A vector with columns names to be used as group columns
 #' @export
-build_lm <- function(data, formula, ..., output = "model", keep.source = TRUE, augment = FALSE, group_cols = NULL){
+build_lm <- function(data, formula, ..., keep.source = TRUE, augment = FALSE, group_cols = NULL){
   if(!is.null(group_cols)){
     data <- dplyr::group_by_(data, .dots =  group_cols)
   }
@@ -9,9 +15,10 @@ build_lm <- function(data, formula, ..., output = "model", keep.source = TRUE, a
   grouped_col <- grouped_by(data)
 
   model_col <- avoid_conflict(grouped_col, "model")
+  source_col <- avoid_conflict(grouped_col, "source.data")
 
   raw_call <- match.call()
-  # get argument named list (-1 means removing this function name)
+  # get named list or arguments (-1 means removing this function name)
   cl <- as.list(match.call())[-1]
   # keep just ... argument
   nodots <- as.list(match.call(expand.dots = FALSE))[-1]
@@ -30,7 +37,11 @@ build_lm <- function(data, formula, ..., output = "model", keep.source = TRUE, a
   }
 
   ret <- tryCatch({
-    data %>% dplyr::do_(.dots = setNames(list(~build_glm_each(.)), model_col))
+    if(keep.source || augment){
+      data %>% dplyr::do_(.dots = setNames(list(~build_glm_each(.), ~(.)), c(model_col, source_col)))
+    } else {
+      data %>% dplyr::do_(.dots = setNames(list(~build_glm_each(.)), model_col))
+    }
   }, error = function(e){
     if(e$message == "contrasts can be applied only to factors with 2 or more levels"){
       stop("more than 2 unique values are needed for categorical predictor columns")
@@ -39,8 +50,11 @@ build_lm <- function(data, formula, ..., output = "model", keep.source = TRUE, a
       stop("no data after removing NA")
     }
     stop(e$message)
-
   })
-  class(ret[[model_col]]) <- c("list", ".model", ".model.lm")
+  if(augment){
+    ret <- do.call(broom::augment, list(ret, model_col, source_col))
+  } else {
+    class(ret[[model_col]]) <- c("list", ".model", ".model.lm")
+  }
   ret
 }
--- tests/testthat/test_build_lm.R
+++ tests/testthat/test_build_lm.R
@@ -9,6 +9,9 @@ test_that("test build_lm summary output ", {
     with_NA = rep(c(letters[5:6], NA, NA), 5)
   )
   trial <- test_df %>% build_lm(num1 ~ num2 + category + with_NA, weights = weights)
+
+  expect_equal(colnames(trial), c("model", "source.data"))
+
   res <- capture.output(summary(trial$model[[1]]))
   expect_lt(length(res), 50) # the output of summary should be less than 50 lines
 })
@@ -23,4 +26,16 @@ test_that("test build_lm with grouped ", {
   trial <- test_df %>% build_lm(num1 ~ num2, group_cols = c("group1", "group2"))
   expect_equal(length(trial[["group2"]]), 8)
   expect_equal(length(trial[["group1"]]), 8)
+})
+
+test_that("test build_lm with augment TRUE", {
+  test_df = data.frame(
+    num1 = seq(20) / 10.0,
+    num2 = seq(20) - 10,
+    group1 = rep(letters[1:4], 5),
+    group2 = rep(letters[1:2], each = 10)
+  )
+  trial <- test_df %>% build_lm(num1 ~ num2, group_cols = c("group1", "group2"), augment = TRUE)
+  expect_equal(length(trial[["group2"]]), 20)
+  expect_equal(length(trial[["group1"]]), 20)
 })
