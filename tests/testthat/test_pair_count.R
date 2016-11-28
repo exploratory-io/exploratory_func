85505617a60ff260f0416d5968ed614de86ad2eb
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-28 21:23:51 +0900

    add test

--- tests/testthat/test_pair_count.R
+++ tests/testthat/test_pair_count.R
@@ -12,3 +12,26 @@ test_that("pair_count", {
   expect_equal(nrow(ret), 25)
   expect_true(all(ret[["n"]] == 2))
 })
+
+test_that("pair_count", {
+  test_df <- data.frame(
+    group = c(rep(c("group1", "group2", "group3"), each = 5), "group3"),
+    chars = c(
+      "b", "a", "d", "b", "a",
+      "a", "d", "d", "d", "c",
+      "a", "c", "c", "d", "d", NA
+    ),
+    stringsAsFactors = FALSE
+  )
+  ret1 <- pair_count(test_df, group, chars)
+
+  expect_equal(ret1[["value1"]], c("b", "b", "a", "a", "d", "a", "d","c"))
+  expect_equal(ret1[["value2"]], c("a", "d", "d", "c", "c", NA, NA, NA))
+  expect_equal(ret1[["n"]], c(1, 1, 3, 2, 2, 1, 1, 1))
+
+  ret2 <- pair_count(test_df, group, chars, self = TRUE, sort = TRUE, unique_pair = FALSE)
+
+  expect_equal(ret2[[1]], c("a", "d", "a", "d", "c", "c", "a", "d", "c", "b", "a", "d", "b", NA, "b", NA, NA, "a", "d", "c", NA))
+  expect_equal(ret2[[2]], c("a", "a", "d", "d", "a", "d", "c", "c", "c", "b", "b", "b", "a", "a", "d", "d", "c", NA, NA, NA, NA))
+  expect_equal(ret2[[3]], c(3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+})
