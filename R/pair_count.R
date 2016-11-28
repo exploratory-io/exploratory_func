a232d114a7d0947676cb06b4d0d57d73b9558df2
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-29 00:21:57 +0900

    add comments

--- R/pair_count.R
+++ R/pair_count.R
@@ -24,15 +24,19 @@ pair_count_ <- function (df,
                          self = FALSE,
                          sort = FALSE){
   loadNamespace("Matrix")
+  # sparse matrix by group rows and value columns
   sparse_tab <- xtabs(data = df[, c(group_col, value_col)], sparse = TRUE, exclude = NULL, na.action=na.pass)
 
+  # this is cross product, so this returns cross table of values count
   count_mat <- Matrix::t(sparse_tab > 0) %*% (sparse_tab > 0)
-  cnames <- c("value1", "value2", "n")
 
+  # gather the matrix
+  cnames <- c("value1", "value2", "n")
   ret <- upper_gather(count_mat, diag = self, cnames = cnames)
-  ret <- ret[ret[[3]] > 0 & !is.na(ret[[3]]), ]
+  ret <- ret[ret[[3]] > 0, ]
 
   if(!unique_pair){
+    # bind rows by swapping the first and second columns
     # remove self to avoid duplicate
     copy <- upper_gather(count_mat, diag = FALSE, cnames = cnames[c(2,1,3)])
     copy <- copy[copy[[3]] > 0 & !is.na(copy[[3]]), ]
@@ -40,8 +44,10 @@ pair_count_ <- function (df,
   }
 
   if (sort) {
+    # sort based on the last column
     ret <- ret[sort.list(ret[, ncol(ret)], decreasing=TRUE),]
   }
+  # reset row names because binding and sorting mess up them
   rownames(ret) <- NULL
   ret
 }
