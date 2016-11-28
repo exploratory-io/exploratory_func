677b7aa5fdee7fda05d5a2a4ba74f6fa5d70f802
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-28 18:23:14 +0900

    add new lines

--- R/pair_count.R
+++ R/pair_count.R
@@ -1,25 +1,38 @@
 
 #' @export
-pair_count <- function (data, group, value, unique_pair = TRUE, self = FALSE,
-          sort = FALSE)
+pair_count <- function (df,
+                        group,
+                        value,
+                        unique_pair = TRUE,
+                        self = FALSE,
+                        sort = FALSE)
 {
   group_col <- col_name(substitute(group))
   value_col <- col_name(substitute(value))
-  pair_count_(data, group_col, value_col, unique_pair = unique_pair,
+  pair_count_(df, group_col, value_col, unique_pair = unique_pair,
               self = self, sort = sort)
 }
 
 #' @export
-pair_count_ <- function (data, group_col, value_col, unique_pair = TRUE, self = FALSE,
-          sort = FALSE){
-  requireNamespace("Matrix")
-  value_vals <- unique(data[[value_col]])
-  value_indices <- match(data[[value_col]], value_vals)
-  data[[".value_indices"]] <- value_indices
-  sparse_mat <- tidytext::cast_sparse_(data, group_col, ".value_indices",
+pair_count_ <- function (df,
+                         group_col,
+                         value_col,
+                         unique_pair = TRUE,
+                         self = FALSE,
+                         sort = FALSE){
+  loadNamespace("Matrix")
+  unique_values <- unique(df[[value_col]])
+  value_indice <- match(df[[value_col]], unique_values)
+
+  indice_col <- avoid_conflict(colnames(df), "indice")
+
+  df[[indice_col]] <- value_indice
+  sparse_mat <- tidytext::cast_sparse_(df, group_col, indice_col,
                              value_col = 1)
   co <- Matrix::t(sparse_mat) %*% sparse_mat
-  matrix_indices <- as.integer(rownames(co))
+
+  matrix_indice <- as.integer(rownames(co))
+
   triplets <- Matrix::summary(co)
   if (unique_pair) {
     triplets <- dplyr::filter(triplets, i <= j)
@@ -27,10 +40,10 @@ pair_count_ <- function (data, group_col, value_col, unique_pair = TRUE, self = 
   if (!self) {
     triplets <- dplyr::filter(triplets, i != j)
   }
-  ret <- triplets %>% dplyr::transmute(value1 = value_vals[matrix_indices[i]],
-                                value2 = value_vals[matrix_indices[j]], n = x) %>% dplyr::tbl_df()
+  ret <- triplets %>% dplyr::transmute(value1 = unique_values[matrix_indice[i]],
+                                value2 = unique_values[matrix_indice[j]], n = x) %>% dplyr::tbl_df()
   if (sort) {
-    ret <- dplyr::arrange(ret, desc(n))
+    ret <- dplyr::arrange(ret, dplyr::desc(n))
   }
   ret
 }
