392473383b4fbbcb7ff942683e864144f2e0a8f3
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-28 22:35:55 +0900

    update pair_count

--- R/pair_count.R
+++ R/pair_count.R
@@ -5,12 +5,15 @@ pair_count <- function (df,
                         value,
                         unique_pair = TRUE,
                         self = FALSE,
-                        sort = FALSE)
-{
+                        sort = FALSE){
   group_col <- col_name(substitute(group))
   value_col <- col_name(substitute(value))
-  pair_count_(df, group_col, value_col, unique_pair = unique_pair,
-              self = self, sort = sort)
+  pair_count_(df,
+              group_col,
+              value_col,
+              unique_pair = unique_pair,
+              self = self,
+              sort = sort)
 }
 
 #' @export
@@ -21,29 +24,24 @@ pair_count_ <- function (df,
                          self = FALSE,
                          sort = FALSE){
   loadNamespace("Matrix")
-  unique_values <- unique(df[[value_col]])
-  value_indice <- match(df[[value_col]], unique_values)
+  sparse_tab <- xtabs(data = df[, c(group_col, value_col)], sparse = TRUE, exclude = NULL, na.action=na.pass)
 
-  indice_col <- avoid_conflict(colnames(df), "indice")
+  count_mat <- Matrix::t(sparse_tab > 0) %*% (sparse_tab > 0)
+  cnames <- c("value1", "value2", "n")
 
-  df[[indice_col]] <- value_indice
-  sparse_mat <- tidytext::cast_sparse_(df, group_col, indice_col,
-                             value_col = 1)
-  co <- Matrix::t(sparse_mat) %*% sparse_mat
+  ret <- upper_gather(count_mat, diag = self, cnames = cnames)
+  ret <- ret[ret[[3]] > 0 & !is.na(ret[[3]]), ]
 
-  matrix_indice <- as.integer(rownames(co))
-
-  triplets <- Matrix::summary(co)
-  if (unique_pair) {
-    triplets <- dplyr::filter(triplets, i <= j)
+  if(!unique_pair){
+    # remove self to avoid duplicate
+    copy <- upper_gather(count_mat, diag = FALSE, cnames = cnames)
+    copy <- copy[copy[[3]] > 0 & !is.na(copy[[3]]), ]
+    ret <- dplyr::bind_rows(ret, copy[, c(2,1,3)])
   }
-  if (!self) {
-    triplets <- dplyr::filter(triplets, i != j)
-  }
-  ret <- triplets %>% dplyr::transmute(value1 = unique_values[matrix_indice[i]],
-                                value2 = unique_values[matrix_indice[j]], n = x) %>% dplyr::tbl_df()
+
   if (sort) {
-    ret <- dplyr::arrange(ret, dplyr::desc(n))
+    ret <- ret[sort.list(ret[, ncol(ret)], decreasing=TRUE),]
   }
+  rownames(ret) <- NULL
   ret
 }
--- tests/testthat/test_pair_count.R
+++ tests/testthat/test_pair_count.R
@@ -25,13 +25,13 @@ test_that("pair_count", {
   )
   ret1 <- pair_count(test_df, group, chars)
 
-  expect_equal(ret1[["value1"]], c("b", "b", "a", "a", "d", "a", "d","c"))
-  expect_equal(ret1[["value2"]], c("a", "d", "d", "c", "c", NA, NA, NA))
-  expect_equal(ret1[["n"]], c(1, 1, 3, 2, 2, 1, 1, 1))
+  expect_equal(ret1[["value1"]], c("a", "a", "a", "a", "b", "c", "c", "d"))
+  expect_equal(ret1[["value2"]], c("b", "c", "d", NA, "d", "d", NA, NA))
+  expect_equal(ret1[["n"]],  c(1, 2, 3, 1, 1, 2, 1, 1))
 
   ret2 <- pair_count(test_df, group, chars, self = TRUE, sort = TRUE, unique_pair = FALSE)
 
-  expect_equal(ret2[[1]], c("a", "d", "a", "d", "c", "c", "a", "d", "c", "b", "a", "d", "b", NA, "b", NA, NA, "a", "d", "c", NA))
-  expect_equal(ret2[[2]], c("a", "a", "d", "d", "a", "d", "c", "c", "c", "b", "b", "b", "a", "a", "d", "d", "c", NA, NA, NA, NA))
+  expect_equal(ret2[[1]], c("a", "a", "d", "a", "a", "c", "c", "a", "c", "a", "a", "b", "b", "c", "d", NA, "a", "a", "b", "c", "d"))
+  expect_equal(ret2[[2]], c("a", "d", "d", "d", "c", "c", "d", "c", "d", "b", NA, "b", "d", NA, NA, NA, "b", NA, "d", NA, NA))
   expect_equal(ret2[[3]], c(3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
 })
