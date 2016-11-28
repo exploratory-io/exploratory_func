e9a7e03c5e9d5fb967aaa771999eb434c2667bc0
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-11-28 13:18:01 +0900

    remove unnecessary test

--- tests/testthat/test_generate_examples.R
+++
@@ -1,8 +0,0 @@
-loadNamespace("knitr")
-
-test_that("do_tokenize examples", {
-  ex_df <- data.frame(
-    index=c("First", "Second"),
-    text=c("It was a great party.", "She has just left. She will be off tomorrow."), stringsAsFactors = TRUE)
-  knitr::kable(ex_df, format="markdown")
-})
