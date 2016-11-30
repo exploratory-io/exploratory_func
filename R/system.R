87afab4c77f8107565cbe1bfd33364bf4095b349
Hideaki Hayashi <hihayash@gmail.com>
2016-11-29 19:53:34 -0800

    Add fp_code to origin/destination of statecode()

--- R/system.R
+++ R/system.R
@@ -1028,6 +1028,11 @@ checkSourceConflict <- function(files){
   ret
 }
 
+state.fp_code <- c("01", "02", "04", "05", "06", "08", "09", "10", "12", "13", "15", "16", "17",
+                   "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                   "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "44",
+                   "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56")
+
 #' Converts between state name and state code of United States.
 #'
 #' Example:
@@ -1045,8 +1050,8 @@ checkSourceConflict <- function(files){
 statecode <- function(sourcevar, origin, destination, ignore.case=TRUE) {
 
   # supported codes
-  codes_origin <- c("abb", "code", "name")
-  codes_destination <- c("abb", "code", "name", "division", "region")
+  codes_origin <- c("abb", "code", "name", "fp_code")
+  codes_destination <- c("abb", "code", "name", "division", "region", "fp_code")
 
   if (!origin %in% codes_origin){
     stop("Origin code not supported")
