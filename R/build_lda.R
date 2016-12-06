bdabaf7e678a5e4855439426fc114ca08c86310b
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-12-06 11:51:27 +0900

    adjustment

--- R/build_lda.R
+++ R/build_lda.R
@@ -2,11 +2,12 @@
 build_lda <- function(df, document, token, count = NULL,
                       n_topics,
                       method = "VEM",
-                      iter = 1000,
-                      burnin = 1000,
+                      iter = 2000,
+                      burnin = 0,
                       keep = 50,
                       seed = 0,
-                      keep.source = FALSE){
+                      keep.source = FALSE,
+                      group_cols = NULL){
   loadNamespace("dplyr")
   loadNamespace("lazyeval")
   loadNamespace("tidyr")
@@ -16,24 +17,69 @@ build_lda <- function(df, document, token, count = NULL,
   col_col <- col_name(substitute(token))
   value_col <- col_name(substitute(value))
 
-  grouped_column <- grouped_by(df)
-  model_column <- avoid_conflict(grouped_column, "model")
-  source_column <- avoid_conflict(grouped_column, "source.data")
+  # deal with group columns by index because those names might be changed
+  group_col_index <- colnames(df) %in% group_cols
+
+  # change column names to avoid name conflict when tidy or glance are executed
+  reserved_names <- c(
+    "model",
+    # for tidy matrix beta
+    "topic", "term", "beta",
+    # for tidy matrix gamma
+    "document", "gamma",
+    # for glance
+    "iter", "terms", "alpha",
+    "statistic", "p.value", "df", "logLik", "AIC", "BIC", "deviance",
+    "df.residual"
+  )
+
+  # colnames(data)[group_col_index] <- avoid_conflict(
+  #   reserved_names,
+  #   colnames(df)[group_col_index],
+  #   ".group"
+  # )
+
+  # make column names unique
+  colnames(df) <- make.unique(colnames(df), sep = "")
+
+  if(!is.null(group_cols)){
+    df <- dplyr::group_by_(df, .dots =  colnames(df)[group_col_index])
+  } else {
+    df <- dplyr::ungroup(df)
+  }
+
+  model_col <- "model"
+  source_col <- "source.data"
 
   build_lda_each <- function(df){
     mat <- sparse_cast(df, row = row_col, col = col_col, count = TRUE) %>% slam::as.simple_triplet_matrix()
-    lda <- topicmodels::LDA(mat, method = method, n_topics, control = list(seed = seed, iter = iter, keep = keep))
+    lda <- if(method == "VEM"){
+      topicmodels::LDA(mat, method = method, n_topics, control = list(seed = seed, keep = keep))
+    } else {
+      topicmodels::LDA(mat, method = method, n_topics, control = list(seed = seed,
+                                                                      iter = iter,
+                                                                      burnin = burnin,
+                                                                      keep = keep))
+    }
     model_set <- lda
   }
 
+  # the column names of source data should be modified for augment
+  # because it tries to join source data and the result by joining "document" and "term" columns
   rename_source <- function(df){
     colnames(df)[colnames(df) == row_col] <- "document"
     colnames(df)[colnames(df) == col_col] <- "term"
     df
   }
 
-  ret <- df %>%
-    dplyr::do_(.dots=setNames(list(~build_lda_each(.), ~rename_source(.)), c(model_column, "source.data")))
-  class(ret[["source.data"]]) <- c("list", ".source.data")
+  if(keep.source){
+    ret <- df %>%
+      dplyr::do_(.dots=setNames(list(~build_lda_each(.), ~rename_source(.)), c("model", "source.data")))
+    class(ret[["source.data"]]) <- c("list", ".source.data")
+  } else {
+    ret <- df %>%
+      dplyr::do_(.dots=setNames(list(~build_lda_each(.)), "model"))
+  }
+  class(ret[["model"]]) <- c("list", ".model", ".model.LDA")
   ret
 }
