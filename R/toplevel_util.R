6521c3589a26a4cf4c122f27eafa39e63ff5f127
Yosuke Yasuda <double.y.919.quick@gmail.com>
2016-12-28 23:23:17 +0900

    add row_as_header

---
+++ R/toplevel_util.R
@@ -0,0 +1,20 @@
+#' Use a row as column names
+#' @param df Data frame
+#' @param row_index Row index to use as column names
+#' @param prefix Prefix for new column names
+#' @param clean_name If janitor::clean_names should be used
+#' @export
+row_as_header <- function(df, row_index = 1, prefix = "", clean_names = TRUE){
+  loadNamespace("stringr")
+  loadNamespace("janitor")
+  names <- as.character(df[row_index, ])
+  if (prefix != ""){
+    names <- stringr::str_c(prefix, names)
+  }
+  ret <- safe_slice(df, row_index, remove = TRUE)
+  colnames(ret) <- names
+  if (clean_names) {
+    ret <- janitor::clean_names(ret)
+  }
+  ret
+}
