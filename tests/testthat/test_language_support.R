3c83e259585606e919b4e32905da813f548c57fe
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-01-02 21:29:58 +0900

    modify test

--- tests/testthat/test_language_support.R
+++ tests/testthat/test_language_support.R
@@ -3,11 +3,11 @@ context("language support")
 test_that("text replacement", {
   text <- c("半角スペ2  全角スペ2　　", "半角2!!全角2！！")
 
-  ret1 <- convert_han_zen(text, to="han")
+  ret1 <- convert_zen_han(text, to="han")
 
   expect_equal(ret1, c("半角スペ2  全角スペ2  ", "半角2!!全角2!!"))
 
-  ret2 <- convert_han_zen(text, to="zen")
+  ret2 <- convert_zen_han(text, to="zen")
 
   expect_equal(ret2, c("半角スペ２　　全角スペ２　　", "半角２！！全角２！！"))
 
