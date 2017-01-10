e009235b0ade7a105ade1156fcc48245cd01acf1
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-01-10 23:02:31 +0900

    add imported functions

--- R/importer.R
+++ R/importer.R
@@ -1,3 +1,23 @@
 #' @importFrom dplyr %>%
 #' @export
 dplyr::`%>%`
+
+#' @importFrom tibble add_row
+#' @export
+tibble::add_row
+
+#' @importFrom tibble add_row
+#' @export
+tibble::add_row
+
+#' @importFrom urltools param_remove
+#' @export
+urltools::param_remove
+
+#' @importFrom urltools param_remove
+#' @export
+urltools::url_encode
+
+#' @importFrom urltools param_remove
+#' @export
+urltools::url_decode
