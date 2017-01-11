9f432bdd87f6242720415dd10b9169b6416a3ebd
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-01-11 10:44:40 +0900

    import functions from tibble and url tools

--- R/importer.R
+++ R/importer.R
@@ -6,18 +6,14 @@ dplyr::`%>%`
 #' @export
 tibble::add_row
 
-#' @importFrom tibble add_row
-#' @export
-tibble::add_row
-
 #' @importFrom urltools param_remove
 #' @export
 urltools::param_remove
 
-#' @importFrom urltools param_remove
+#' @importFrom urltools url_encode
 #' @export
 urltools::url_encode
 
-#' @importFrom urltools param_remove
+#' @importFrom urltools url_decode
 #' @export
 urltools::url_decode
