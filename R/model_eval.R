ab7c150e7a8da7890cf304ada369293e801e1972
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-01-16 13:36:11 +0900

    modify argument name

--- R/model_eval.R
+++ R/model_eval.R
@@ -1,25 +1,25 @@
 #' Non standard evaluation version of do_roc_
 #' @param df Model data frame that can work prediction
 #' @export
-do_roc <- function(df, actual_value, predicted_prob){
-  actual_value_col <- col_name(substitute(actual_value))
-  predicted_prob_col <- col_name(substitute(predicted_prob))
-  do_roc_(df, actual_value_col, predicted_prob_col)
+do_roc <- function(df, actual_val, pred_prob){
+  actual_val_col <- col_name(substitute(actual_val))
+  pred_prob_col <- col_name(substitute(pred_prob))
+  do_roc_(df, actual_val_col, pred_prob_col)
 }
 
 #' Return cordinations for ROC curve
 #' @param df Model data frame that can work prediction
 #' @export
-do_roc_ <- function(df, actual_value_col, predicted_prob_col){
+do_roc_ <- function(df, actual_val_col, pred_prob_col){
   group_cols <- grouped_by(df)
 
   tpr_col <- avoid_conflict(group_cols, "true_positive_rate")
   fpr_col <- avoid_conflict(group_cols, "false_positive_rate")
 
   do_roc_each <- function(df){
-    arranged <- df[order(df[[predicted_prob_col]]), ]
+    arranged <- df[order(df[[pred_prob_col]]), ]
 
-    val <- arranged[[actual_value_col]][!is.na(arranged[[actual_value_col]])]
+    val <- arranged[[actual_val_col]][!is.na(arranged[[actual_val_col]])]
     if(is.factor(val)){
       # Need to subtract 1 because the first level in factor is regarded as 1
       # though it should be FALSE.
