872d313e75dd07a01a36c260040f82af20174322
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-02-03 17:10:22 +0900

    add test for loess and gam

--- tests/testthat/test_broom_wrapper.R
+++ tests/testthat/test_broom_wrapper.R
@@ -239,3 +239,50 @@ test_that("test prediction binary", {
   model_coef(model_data, conf_int = "default")
 
 })
+
+test_that("test loess", {
+  test_data <- structure(
+    list(
+      CANCELLED = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0),
+      `Carrier Name` = c("Delta Air Lines", "American Eagle", "American Airlines", "Southwest Airlines", "SkyWest Airlines", "Southwest Airlines", "Southwest Airlines", "Delta Air Lines", "Southwest Airlines", "Atlantic Southeast Airlines", "American Airlines", "Southwest Airlines", "US Airways", "US Airways", "Delta Air Lines", "Atlantic Southeast Airlines", NA, "Atlantic Southeast Airlines", "Delta Air Lines", "Delta Air Lines"),
+      CARRIER = c("DL", "MQ", "AA", "DL", "MQ", "AA", "DL", "DL", "MQ", "AA", "AA", "WN", "US", "US", "DL", "EV", "9E", "EV", "DL", "DL"),
+      DISTANCE = c(1587, 173, 646, 187, 273, 1062, 583, 240, 1123, 851, 852, 862, 361, 507, 1020, 1092, 342, 489, 1184, 545)), row.names = c(NA, -20L),
+    class = c("tbl_df", "tbl", "data.frame"), .Names = c("CANCELLED", "Carrier Name", "CARRIER", "DISTANCE"))
+
+  test_data[["rand"]] <- runif(nrow(test_data), min=-5, max = 5)
+
+  for (i in seq(5)){
+    test_data <- dplyr::bind_rows(test_data, test_data)
+  }
+
+  model_data <- build_model(test_data, model_func = loess, formula = rand ~ DISTANCE, test_rate = 0.2)
+
+  prediction_ret <- prediction(model_data)
+
+  expect_true(any(colnames(prediction_ret) %in% "conf_low"))
+  expect_true(any(colnames(prediction_ret) %in% "conf_high"))
+})
+
+test_that("test gam", {
+  test_data <- structure(
+    list(
+      CANCELLED = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0),
+      `Carrier Name` = c("Delta Air Lines", "American Eagle", "American Airlines", "Southwest Airlines", "SkyWest Airlines", "Southwest Airlines", "Southwest Airlines", "Delta Air Lines", "Southwest Airlines", "Atlantic Southeast Airlines", "American Airlines", "Southwest Airlines", "US Airways", "US Airways", "Delta Air Lines", "Atlantic Southeast Airlines", NA, "Atlantic Southeast Airlines", "Delta Air Lines", "Delta Air Lines"),
+      CARRIER = c("DL", "MQ", "AA", "DL", "MQ", "AA", "DL", "DL", "MQ", "AA", "AA", "WN", "US", "US", "DL", "EV", "9E", "EV", "DL", "DL"),
+      DISTANCE = c(1587, 173, 646, 187, 273, 1062, 583, 240, 1123, 851, 852, 862, 361, 507, 1020, 1092, 342, 489, 1184, 545)), row.names = c(NA, -20L),
+    class = c("tbl_df", "tbl", "data.frame"), .Names = c("CANCELLED", "Carrier Name", "CARRIER", "DISTANCE"))
+
+  for (i in seq(5)){
+    test_data <- dplyr::bind_rows(test_data, test_data)
+  }
+
+  test_data[["rand"]] <- runif(nrow(test_data), min=-5, max = 5)
+
+  model_data <- build_model(test_data, model_func = mgcv::gam, formula = rand ~ DISTANCE, test_rate = 0.2)
+
+  prediction_ret <- prediction(model_data)
+
+  expect_true(any(colnames(prediction_ret) %in% "conf_low"))
+  expect_true(any(colnames(prediction_ret) %in% "conf_high"))
+})
+
