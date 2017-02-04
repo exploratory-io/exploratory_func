171fe36e5f1b3496b1a73892cfd671afca59a781
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-02-04 09:55:35 +0900

    modify tests

--- tests/testthat/test_broom_wrapper.R
+++ tests/testthat/test_broom_wrapper.R
@@ -236,7 +236,7 @@ test_that("test prediction binary", {
 
   model_data <- build_lr(test_data, CANCELLED ~ DISTANCE, test_rate = 0.2)
 
-  model_coef(model_data, conf_int = "default")
+  coef_ret <- model_coef(model_data, conf_int = "default")
 
 })
 
