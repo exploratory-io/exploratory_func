7366c1db2b7c90a1b15dacd4420067a0ebf832a6
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-02-05 14:43:18 +0900

    add vec argument to anonymizer

--- R/importer.R
+++ R/importer.R
@@ -20,6 +20,6 @@ urltools::url_decode
 
 #' @importFrom anonymizer anonymize
 #' @export
-anonymize <- function(algo = "sha256", seed = 0, chars = letters, n_chars = 5L, ...){
-  anonymizer::anonymize(.algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
+anonymize <- function(vec, algo = "sha256", seed = 0, chars = letters, n_chars = 5L, ...){
+  anonymizer::anonymize(vec, .algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
 }
