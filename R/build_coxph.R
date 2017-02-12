317cfdb46558a8ed2ea5c29323a359293fe3bfe7
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-02-12 09:09:37 +0900

    roll back survival functions

--- R/build_coxph.R
+++ R/build_coxph.R
@@ -10,40 +10,36 @@
 #' @param seed Random seed to control test data sampling
 #' @export
 build_coxph <- function(data, formula, ...){
-  # using lazyeval is needed for non standard evaluation arguments like weights
-  lz_dots <- lazyeval::lazy_dots(...)
-  lz_dots[["data"]] <- lazyeval::as.lazy(quote(data))
-  lz_dots[["formula"]] <- lazyeval::lazy(formula)
-  lz_dots[["model_func"]] <- lazyeval::as.lazy(quote(survival::coxph))
-  lz_dots[["reserved_colnames"]] <- lazyeval::as.lazy(quote(c(
-    # model_coef can add following columns at the next step
-    "y.level",
-    "term",
-    "estimate",
-    "std_error",
-    "t_ratio",
-    "p_value",
-    # model_stats can add following columns at the next step
-    "edf",
-    "deviance",
-    "AIC",
-    # prediction_survfit can add following columns at the next step
-    "time",
-    "n.risk",
-    "n_risk",
-    "n.event",
-    "n_event",
-    "n.censor",
-    "n_censor",
-    "estimate",
-    "std.error",
-    "std_error",
-    "conf.high",
-    "conf_high",
-    "conf.low",
-    "conf_low"
-  )))
-  .call <- lazyeval::make_call(quote(build_model), lz_dots)
-  .call$env <- environment()
-  lazyeval::lazy_eval(.call)
+  build_model(data = data,
+              formula = formula,
+              model_func = survival::coxph,
+              reserved_colnames =  c(
+                # model_coef can add following columns at the next step
+                "y.level",
+                "term",
+                "estimate",
+                "std_error",
+                "t_ratio",
+                "p_value",
+                # model_stats can add following columns at the next step
+                "edf",
+                "deviance",
+                "AIC",
+                # prediction_survfit can add following columns at the next step
+                "time",
+                "n.risk",
+                "n_risk",
+                "n.event",
+                "n_event",
+                "n.censor",
+                "n_censor",
+                "estimate",
+                "std.error",
+                "std_error",
+                "conf.high",
+                "conf_high",
+                "conf.low",
+                "conf_low"
+              ),
+              ...)
 }
