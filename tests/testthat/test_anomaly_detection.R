67c0eaaf8173f9d87325df67961ba920cc722ee1
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-02-26 23:36:26 +0900

    update test

--- tests/testthat/test_anomaly_detection.R
+++ tests/testthat/test_anomaly_detection.R
@@ -1,14 +1,23 @@
 context("test anomaly detection functions")
 
+test_that("do_anomary_detection with aggregation", {
+  data("raw_data", package = "AnomalyDetection")
+  raw_data$timestamp <- as.POSIXct(raw_data$timestamp)
+  first_ten <- raw_data %>% dplyr::slice(1:10)
+  raw_data <- dplyr::bind_rows(raw_data, first_ten)
+  ret <- raw_data %>%
+    do_anomaly_detection(timestamp, e_value=TRUE, time_unit = "hour")
+})
+
 test_that("do_anomary_detection", {
   data("raw_data", package = "AnomalyDetection")
   raw_data$timestamp <- as.POSIXct(raw_data$timestamp)
   ret <- raw_data %>%
-    do_anomaly_detection(timestamp, count, e_value=TRUE)
+    do_anomaly_detection(timestamp, count, e_value=TRUE, time_unit = "hour")
   expect_equal(ncol(ret), 7)
 })
 
-test_that("do_anomary_detection with dupe", {
+test_that("do_anomary_detection with aggregation", {
   data("raw_data", package = "AnomalyDetection")
   raw_data$timestamp <- as.POSIXct(raw_data$timestamp)
   first_ten <- raw_data %>% dplyr::slice(1:10)
@@ -16,7 +25,7 @@ test_that("do_anomary_detection with dupe", {
   expect_error({
     raw_data %>%
       do_anomaly_detection(timestamp, count, e_value=TRUE)
-  }, "There are duplicated values in Date/Time column.")
+  }, "Not enogh data to detect anomaly. Try smaller time unit.")
 })
 
 test_that("do_anomary_detection grouped case", {
@@ -33,4 +42,12 @@ test_that("do_anomary_detection grouped case", {
       dplyr::group_by(count) %>%
       do_anomaly_detection(timestamp, count, e_value=TRUE)
   }, "count is grouped. Please ungroup it.")
+})
+
+test_that("do_anomary_detection without value_col", {
+  data("raw_data", package = "AnomalyDetection")
+  raw_data$timestamp <- as.POSIXct(raw_data$timestamp)
+  raw_data <- dplyr::bind_rows(raw_data, raw_data, raw_data)
+  ret <- raw_data %>%
+    do_anomaly_detection(timestamp, e_value=TRUE, time_unit = "hour")
 })
