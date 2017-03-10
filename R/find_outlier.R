0318d1770800342e204ecf18a7765adc64182a13
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-03-10 21:04:27 +0900

    add comments

--- R/find_outlier.R
+++ R/find_outlier.R
@@ -1,7 +1,10 @@
 #' Find outlier values
+#' ref: https://www.r-bloggers.com/outlier-detection-and-treatment-with-r/
 #' @export
 find_outlier <- function (vec) {
   q <- quantile(vec, na.rm = TRUE)
+  # q is with 0%, 25%, 50%, 75%, 100% quartiles
+  # IQR is difference between 75% and 25%
   IQR <- q[4]-q[2]
   upper_whisker <- q[4]+1.5*IQR
   lower_whisker <- q[2]-1.5*IQR
