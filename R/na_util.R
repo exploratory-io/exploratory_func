fb45ce6a12a2a9da70933fa568e1f02d1ffc063d
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-03-11 13:16:05 +0900

    change predict_na to impute_na

--- R/na_util.R
+++ R/na_util.R
@@ -1,15 +1,28 @@
 #' Guess missing values by lm
 #' @export
-predict_na <- function(target, ...) {
-  # list(...) is a list of vectors
-  df <- as.data.frame(list(...))
-  df$target <- target
-  lm_model <- lm(data = df, target ~ ., na.action = na.omit)
-  ret <- target
-  ret[is.na(ret)] <- suppressWarnings({
-    # predict where target is NA
-    predict(lm_model, df[is.na(target),])
+impute_na <- function(target, ..., type = "lm_predict") {
+  ret <- switch(type, lm_predict = {
+    # list(...) is a list of vectors
+    df <- as.data.frame(list(...))
+    df$target <- target
+    lm_model <- lm(data = df, target ~ ., na.action = na.omit)
+    # predict return double, so values should be double
+    ret <- as.double(target)
+    try({
+      ret[is.na(ret)] <- suppressWarnings({
+        # predict where target is NA
+        predict(lm_model, df[is.na(target),])
+      })
+    })
+    ret
+  }, mean = {
+    val <- mean(target, na.rm = TRUE)
+    target[is.na(target)] <- val
+    target
+  }, median = {
+    val <- median(target, na.rm = TRUE)
+    target[is.na(target)] <- val
+    target
   })
-
   ret
 }
