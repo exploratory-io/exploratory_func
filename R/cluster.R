942bb9090125dd3791395c3a0587435386911dbe
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-03-12 11:23:42 +0900

    update cluster function

--- R/cluster.R
+++ R/cluster.R
@@ -1,13 +1,21 @@
-#' cluster based on vectors
+#' Output cluster labels based on vectors
+#' @param ...  numeric vectors whose legthes are the same.
+#' @param type  Type of cluster. Currently, only kmeans is supported.
+#' @param n_cluster  Number of cluster.
 #' @export
 cluster <- function(..., type = "kmeans", n_cluster = 3) {
   data <- list(...) %>% as.data.frame() %>% as.matrix()
   na_removed <- data %>% na.omit()
   if(nrow(na_removed) == 0) {
-    stop("no valid data to cluster")
+    stop("not enough valid data to perform clustering")
+  } else if (nrow(na_removed) <= n_cluster) {
+    stop("n_cluster must be smaller than the number of valid data")
   }
   model <- kmeans(na_removed, centers = n_cluster)
   cluster <- if (ncol(data) == 1) {
+    # in this case, cluster id should be assigned based on
+    # how large the centers are
+
     # sort by centers
     sorted <- sort(model$centers[,1], index.return = TRUE)
     # create mapping
@@ -15,6 +23,9 @@ cluster <- function(..., type = "kmeans", n_cluster = 3) {
     map[sorted$ix] <- seq(n_cluster)
     map[model$cluster]
   } else {
+    # in this case, cluster id should be assigned based on
+    # the size of clusters (the largest cluster is 1)
+
     # sort by size
     sorted <- sort(model$size, index.return = TRUE, decreasing = TRUE)
     # create mapping
--- tests/testthat/test_cluster.R
+++ tests/testthat/test_cluster.R
@@ -36,3 +36,37 @@ test_that("test cluster with one column", {
   expect_equal(length(levels(ret$cluster)), 3)
   expect_true(is.na(ret$cluster[[4]]))
 })
+
+test_that("test cluster with more n_cluster than data", {
+  set.seed(1)
+  test_data <- data.frame(
+    runif(12) + rep(seq(3), 4)
+  )
+  colnames(test_data) <- c("num 1")
+
+  test_data[["num 1"]][[4]] <- NA
+
+  expect_error(
+    {
+      ret <- test_data %>%
+        dplyr::mutate(cluster = cluster(`num 1`, n_cluster = 100))
+    }, "n_cluster must be smaller than the number of valid data"
+  )
+})
+
+test_that("test cluster with one row", {
+  set.seed(1)
+  test_data <- data.frame(
+    runif(1),
+    runif(1),
+    runif(1)
+  )
+  colnames(test_data) <- c("num 1", "num 2", "num 3")
+
+  expect_error(
+    {
+      ret <- test_data %>%
+        dplyr::mutate(cluster = cluster(`num 1`, `num 2`, `num 3`, n_cluster = 1))
+    }, "n_cluster must be smaller than the number of valid data"
+  )
+})
