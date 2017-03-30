5f7c486ddaccc81f78f863a6ddcca3f8bbe9775f
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-03-30 17:05:12 +0900

    add copied token

---
+++ R/oauth_wrapper.R
@@ -0,0 +1,24 @@
+#' @export
+CopiedToken2.0 <- R6::R6Class("CopiedToken2.0", inherit = httr::Token2.0, list(
+  initialize = function(
+    ...,
+    appname,
+    key,
+    secret,
+    credentials = list()
+  ){
+    self$endpoint <- httr::oauth_endpoint(
+      ...
+    )
+    self$app <- httr::oauth_app(
+      appname = appname,
+      key = key,
+      secret = secret
+    )
+    self$credentials <- credentials
+    # this is needed to use this token in httr::GET function
+    self$params$as_header <- TRUE
+  }
+))
+
+
