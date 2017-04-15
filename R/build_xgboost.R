8a75341c0f2d9d7ec122d91aad52b9ed509a5556
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-04-15 13:22:10 +0900

    update handling of sparse argument

--- R/build_xgboost.R
+++ R/build_xgboost.R
@@ -1,6 +1,6 @@
 #' formula version of xgboost
 #' @export
-fml_xgboost <- function(data, formula, nrounds= 10, weights = NULL, watchlist_rate = 0, na.action = na.pass, sparse = NULL, ...) {
+fml_xgboost <- function(data, formula, nrounds= 10, weights = NULL, watchlist_rate = 0, na.action = na.pass, sparse = FALSE, ...) {
   term <- terms(formula, data = data)
   # do.call is used to substitute weights
   md_frame <- tryCatch({
@@ -23,15 +23,6 @@ fml_xgboost <- function(data, formula, nrounds= 10, weights = NULL, watchlist_ra
     y <- y[!is.na(y)]
 
     md_mat <- tryCatch({
-      if(is.null(sparse)){
-        sparse <- FALSE
-        # If any variable is factor, it uses sparse matrix
-        for(var in all.vars(term)){
-          if(is.factor(data[[var]])) {
-            sparse = TRUE
-          }
-        }
-      }
       if(sparse){
         Matrix::sparse.model.matrix(term, data = md_frame)
       } else {
@@ -263,7 +254,7 @@ augment.xgboost_multi <- function(x, data = NULL, newdata = NULL, ...) {
       data
     }
 
-    mat <- if(x$is_sparse){
+    mat <- if(!is.null(x$is_sparse) && x$is_sparse){
       Matrix::sparse.model.matrix(x$terms, ret_data, xlev = x$xlevels)
     } else {
       model.matrix(x$terms, model.frame(ret_data, na.action = na.pass, xlev = x$xlevels))
@@ -329,7 +320,7 @@ augment.xgboost_binary <- function(x, data = NULL, newdata = NULL, ...) {
       data
     }
 
-    mat <- if(x$is_sparse){
+    mat <- if(!is.null(x$is_sparse) && x$is_sparse){
       Matrix::sparse.model.matrix(x$terms, model.frame(ret_data, na.action = na.pass, xlev = x$xlevels))
     } else {
       model.matrix(x$terms, model.frame(ret_data, na.action = na.pass, xlev = x$xlevels))
@@ -399,7 +390,7 @@ augment.xgboost_reg <- function(x, data = NULL, newdata = NULL, ...) {
       ret_data[[y_name]] <- rep(0, nrow(ret_data))
     }
 
-    mat_data <- if(x$is_sparse){
+    mat_data <- if(!is.null(x$is_sparse) && x$is_sparse){
       Matrix::sparse.model.matrix(x$terms, data = model.frame(ret_data, na.action = na.pass, xlev = x$xlevels))
     } else {
       model.matrix(x$terms, model.frame(ret_data, na.action = na.pass, xlev = x$xlevels))
