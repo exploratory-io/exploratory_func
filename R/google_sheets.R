e5c2ed584e5754b79a080e4e49927b255443dad7
Hidetaka Kojima <hidekoji@gmail.com>
2017-04-18 23:34:11 -0700

    Move Google Sheets API to new file.

--- R/google_sheets.R
+++ R/google_sheets.R
@@ -8,3 +8,32 @@ uploadGoogleSheet <- function(filepath, title){
   googlesheets::gs_auth(token)
   sheet <- googlesheets::gs_upload(filepath, title)
 }
+
+#' @export
+getGoogleSheet <- function(title, sheetNumber, skipNRows, treatTheseAsNA, firstRowAsHeader, commentChar, tokenFileId=NULL){
+  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  token <- getGoogleTokenForSheet(tokenFileId)
+  googlesheets::gs_auth(token)
+  gsheet <- googlesheets::gs_title(title)
+  df <- gsheet %>% googlesheets::gs_read(ws = sheetNumber, skip = skipNRows, na = treatTheseAsNA, col_names = firstRowAsHeader, comment = commentChar)
+  df
+}
+
+#' API to get a list of available google sheets
+#' @export
+getGoogleSheetList <- function(tokenFileId=""){
+  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  token = getGoogleTokenForSheet(tokenFileId)
+  googlesheets::gs_auth(token)
+  googlesheets::gs_ls()
+}
+
+#' API to get a list of available google sheets
+#' @export
+getGoogleSheetWorkSheetList <- function(tokenFileId="", title){
+  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  token = getGoogleTokenForSheet(tokenFileId)
+  googlesheets::gs_auth(token)
+  sheet <- googlesheets::gs_title(title)
+  googlesheets::gs_ws_ls(sheet)
+}
