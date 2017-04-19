8f5c8ac6cd1806275456801861fcfe88009db50f
Hidetaka Kojima <hidekoji@gmail.com>
2017-04-18 22:35:09 -0700

    Added comments

--- R/google_sheets.R
+++ R/google_sheets.R
@@ -1,4 +1,7 @@
+#' API to upload local CSV file to Google Sheets
 #' @export
+#' @param filepath path of source CSV file that you want to upload to Google Sheet
+#' @param title name of the new sheet on Google Sheets.
 uploadGoogleSheet <- function(filepath, title){
   if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
   token <- getGoogleTokenForSheet("")
