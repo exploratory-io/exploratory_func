4976bfac947558512132569ad74031f81127ae52
Hideaki Hayashi <hihayash@gmail.com>
2017-05-23 12:10:19 -0700

    Add time to arguments of do_causal_impact

--- R/causal_impact.R
+++ R/causal_impact.R
@@ -2,14 +2,25 @@
 
 #' NSE version of do_causal_impact_
 #' @export
-do_causal_impact <- function(df, formula, ...) {
-  do_causal_impact_(df, formula = formula, ...)
+do_causal_impact <- function(df, time, formula, ...) {
+  time_col <- col_name(substitute(time))
+  do_causal_impact_(df, time_col, formula = formula, ...)
 }
 
-do_causal_impact_ <- function(df, formula, ...) {
+do_causal_impact_ <- function(df, time_col, formula, ...) {
   y_column_name <- as.character(lazyeval::f_lhs(formula))
   predictor_column_names <- all.vars(lazyeval::f_rhs(formula))
   all_column_names <- all.vars(formula)
+  grouped_col <- grouped_by(df)
+
+  # column name validation
+  if(!time_col %in% colnames(df)){
+    stop(paste0(time_col, " is not in column names"))
+  }
+
+  if(time_col %in% grouped_col){
+    stop(paste0(time_col, " is grouped. Please ungroup it."))
+  }
 
   # remove rows with NA in predictors. CausalImpact does not allow NA in predictors (covariates).
   for(var in predictor_column_names) {
@@ -29,8 +40,6 @@ do_causal_impact_ <- function(df, formula, ...) {
     data <- tibble::rownames_to_column(data)
     mutate(data, rowname = as.numeric(rowname))
   }
-
-  grouped_col <- grouped_by(df)
 
   # Calculation is executed in each group.
   # Storing the result in this tmp_col and
