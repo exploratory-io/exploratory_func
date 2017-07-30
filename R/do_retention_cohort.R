78174f75a8addc9a2efc57f09a84519f99b65b72
Hideaki Hayashi <hihayash@gmail.com>
2017-07-29 22:48:01 -0700

    Make measure work

--- R/do_retention_cohort.R
+++ R/do_retention_cohort.R
@@ -1,20 +1,21 @@
 #' Run retention cohort analysis
 #' @param df Data frame to run bayes ab test
 #' @export
-do_retention_cohort <- function(df, timestamp, user_id, time_unit = "month", measure = NULL){
+do_retention_cohort <- function(df, timestamp, user_id, measure, time_unit = "month"){
   # this seems to be the new way of NSE column selection evaluation
   # ref: https://github.com/tidyverse/tidyr/blob/3b0f946d507f53afb86ea625149bbee3a00c83f6/R/spread.R
   timestamp_col <- dplyr::select_var(names(df), !! rlang::enquo(timestamp))
   user_id_col <- dplyr::select_var(names(df), !! rlang::enquo(user_id))
-  if (!is.null(measure)) {
-    measure_col <- dplyr::select_var(names(df), !! rlang::enquo(measure))
-  }
+  measure_col <- dplyr::select_var(names(df), !! rlang::enquo(measure))
 
   grouped_col <- grouped_by(df)
 
   # this will be executed to each group
   each_func <- function(df, ...){
     ret <- df %>% rename_(.dots = list(.timestamp = timestamp_col))
+    if (!is.null(measure)) {
+      ret <- ret %>% rename_(.dots = list(.measure = measure_col))
+    }
     ret <- ret %>% dplyr::mutate(.timestamp =lubridate::floor_date(.timestamp, unit = time_unit))
     if (is.null(measure)) { # if measure is NULL, we are calculating retention. distinct user access for the time period now.
       ret <- ret %>% dplyr::distinct_(".timestamp", user_id_col)
@@ -23,11 +24,17 @@ do_retention_cohort <- function(df, timestamp, user_id, time_unit = "month", mea
       mutate(.start_time = min(.timestamp)) %>%
       ungroup()
     ret <- ret %>% mutate(period = round(as.numeric(as.Date(.timestamp) - as.Date(.start_time))/switch(time_unit, day = 1, week = 7, month = (365.25/12), quarter = (365.25/4), year = 365.25)))
-    ret <- ret %>% group_by(.start_time, period) %>%
-      summarize(retained = n(), timestamp = first(.timestamp))
-    ret <- ret %>% group_by(.start_time) %>%
-      mutate(retained_pct = retained / first(retained) * 100) %>%
-      ungroup()
+    if (is.null(measure)) {
+      ret <- ret %>% group_by(.start_time, period) %>%
+        summarize(retained = n(), timestamp = first(.timestamp))
+      ret <- ret %>% group_by(.start_time) %>%
+        mutate(retained_pct = retained / first(retained) * 100) %>%
+        ungroup()
+    }
+    else {
+      ret <- ret %>% group_by(.start_time, period) %>%
+        summarize(value = sum(.measure), timestamp = first(.timestamp))
+    }
     ret <- ret %>% dplyr::rename(start_time = .start_time)
     ret
   }
