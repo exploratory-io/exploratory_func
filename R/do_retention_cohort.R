93bb18775b594c180530a6e4fb2dc5af518b69c5
Hideaki Hayashi <hihayash@gmail.com>
2017-07-30 01:36:39 -0700

    Fix bug in handling of measure

--- R/do_retention_cohort.R
+++ R/do_retention_cohort.R
@@ -6,27 +6,26 @@ do_retention_cohort <- function(df, timestamp, user_id, measure = NULL, time_uni
   # ref: https://github.com/tidyverse/tidyr/blob/3b0f946d507f53afb86ea625149bbee3a00c83f6/R/spread.R
   timestamp_col <- dplyr::select_var(names(df), !! rlang::enquo(timestamp))
   user_id_col <- dplyr::select_var(names(df), !! rlang::enquo(user_id))
-  if (!is.null(measure)) { # the way with rlang::enquo above cannot handle NULL well. using old way.
-    measure_col <- col_name(substitute(measure))
-  }
+  # the way with rlang::enquo above cannot handle NULL well. using old way.
+  measure_col <- col_name(substitute(measure))
 
   grouped_col <- grouped_by(df)
 
   # this will be executed to each group
   each_func <- function(df, ...){
     ret <- df %>% rename_(.dots = list(.timestamp = timestamp_col))
-    if (!is.null(measure)) {
+    if (!is.null(measure_col)) {
       ret <- ret %>% rename_(.dots = list(.measure = measure_col))
     }
     ret <- ret %>% dplyr::mutate(.timestamp =lubridate::floor_date(.timestamp, unit = time_unit))
-    if (is.null(measure)) { # if measure is NULL, we are calculating retention. distinct user access for the time period now.
+    if (is.null(measure_col)) { # if measure is NULL, we are calculating retention. distinct user access for the time period now.
       ret <- ret %>% dplyr::distinct_(".timestamp", user_id_col)
     }
     ret <- ret %>% dplyr::group_by_(user_id_col) %>%
       mutate(.start_time = min(.timestamp)) %>%
       ungroup()
     ret <- ret %>% mutate(period = round(as.numeric(as.Date(.timestamp) - as.Date(.start_time))/switch(time_unit, day = 1, week = 7, month = (365.25/12), quarter = (365.25/4), year = 365.25)))
-    if (is.null(measure)) {
+    if (is.null(measure_col)) {
       ret <- ret %>% group_by(.start_time, period) %>%
         summarize(retained = n(), timestamp = first(.timestamp))
       ret <- ret %>% group_by(.start_time) %>%
