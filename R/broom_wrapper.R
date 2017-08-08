15082204f1d58491c71cf658b8cc6bf2d7e1eec8
Yosuke Yasuda <double.y.919.quick@gmail.com>
2017-08-08 16:44:44 +0900

    add substitute for do_survfit time

--- R/broom_wrapper.R
+++ R/broom_wrapper.R
@@ -995,7 +995,7 @@ prediction_survfit <- function(df, newdata = NULL, ...){
 do_survfit <- function(df, time, status, start_time = NULL, end_time = NULL, time_unit = "day", ...){
   validate_empty_data(df)
 
-  if (is.null(time)) {
+  if (is.null(substitute(time))) {
     # as.numeric() does not support all units.
     # also support of units are different between Date and POSIXct.
     # let's do it ourselves.
