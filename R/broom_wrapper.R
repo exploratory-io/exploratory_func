96f092b3922209343965ceba10786dbe29443b1c
Hideaki Hayashi <hihayash@gmail.com>
2017-08-16 16:43:37 -0700

    Add time 0 point only when we preprocess survivial time with period

--- R/broom_wrapper.R
+++ R/broom_wrapper.R
@@ -995,13 +995,12 @@ prediction_survfit <- function(df, newdata = NULL, ...){
 do_survfit <- function(df, time, status, start_time = NULL, end_time = NULL, time_unit = "day", ...){
   validate_empty_data(df)
 
-  # for each group_by group, create row for time 0 with 100 survival rate
-  time_zero_rows <- df %>% summarize(time = 0, n_risk = n(), n_event = 0, n_censor = 0, estimate = 1, std_error = 0, conf_high = 1, conf_low = 1)
-
   # substitute is needed because time can be
   # NSE column name and it throws an evaluation error
   # without it
   if (is.null(substitute(time))) {
+    # for each group_by group, create row for time 0 with 100% survival rate
+    time_zero_rows <- df %>% summarize(time = 0, n_risk = n(), n_event = 0, n_censor = 0, estimate = 1, std_error = 0, conf_high = 1, conf_low = 1)
 
     start_time_col <- col_name(substitute(start_time))
     df[[start_time_col]] <- as.Date(df[[start_time_col]]) # convert to Date in case it is POSIXct.
@@ -1025,8 +1024,9 @@ do_survfit <- function(df, time, status, start_time = NULL, end_time = NULL, tim
                                 quarter = "(365.25/4)",
                                 year = "365.25",
                                 "1")
-    # we are flooring survival time to make it integer in the specified time unit.
+    # we are flooring survival time, then plus 1 to make it integer in the specified time unit.
     # this is to make resulting survival curve to have integer data point in the specified time unit.
+    # with plus 1, even an observation with time 0 is moved to time 1, but this is necessary to make things consistent with our addition of time 0 point.
     fml <- as.formula(paste0("survival::Surv(as.numeric(`", end_time_col, "`-`", start_time_col, "`, units = \"days\")%/%", time_unit_days_str, "+1, `", substitute(status), "`) ~ 1"))
   }
   else {
@@ -1044,8 +1044,10 @@ do_survfit <- function(df, time, status, start_time = NULL, end_time = NULL, tim
   colnames(ret)[colnames(ret) == "conf.low"] <- "conf_low"
   colnames(ret)[colnames(ret) == "conf.high"] <- "conf_high"
 
-  # bind with time 0 rows. sort it by time.
-  ret <- ret %>% bind_rows(time_zero_rows) %>% arrange(time)
+  # bind with time 0 rows for analytics view. sort it by time.
+  if (is.null(substitute(time))) {
+    ret <- ret %>% bind_rows(time_zero_rows) %>% arrange(time)
+  }
   ret
 }
 
