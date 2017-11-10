23727a3af2020ec42e78941cd25d7f6c3df90697
Hideaki Hayashi <hihayash@gmail.com>
2017-11-09 16:18:29 -0800

    edarf: Handle categorical values and numerical values at the same time

--- R/randomForest_tidiers.R
+++ R/randomForest_tidiers.R
@@ -1016,10 +1016,11 @@ tidy.ranger <- function(x, type = "importance", pretty.name = FALSE, ...) {
       imp_vars <- imp_df$variable[1:min(length(imp_df),6)] # take maximum of 6 most important variables
       imp_vars <- as.character(imp_vars) # for some reason imp_vars is converted to factor at this point. turn it back to character.
       ret <- edarf::partial_dependence(x, vars=imp_vars, data=x$df)
-      ret <- ret %>% select_if(is.numeric) # keep numeric only since this will be a line chart.
+      # ret <- ret %>% select_if(is.numeric) # keep numeric only since this will be a line chart.
       var_cols <- colnames(ret)[colnames(ret) %in% colnames(x$df)]
       ret <- ret %>% gather_("x_name", "x_value", var_cols, na.rm = TRUE, convert = TRUE)
       ret <- ret %>% gather("y_name", "y_value", -x_name, -x_value, na.rm = TRUE, convert = TRUE)
+      ret <- ret %>% mutate(x_value = forcats::fct_inorder(x_value)) # TODO: if same number appears for different variables, order will be broken.
       ret
     },
     {
