7b015ee0b6890a9046017b21f9631ff21c3dd2d4
Hideaki Hayashi <hihayash@gmail.com>
2017-11-14 14:23:02 -0800

    smote: make default TRUE

--- R/randomForest_tidiers.R
+++ R/randomForest_tidiers.R
@@ -763,7 +763,7 @@ calc_feature_imp <- function(df,
                              nodesize = 12,
                              target_n = 20,
                              predictor_n = 12, # so that at least months can fit in it.
-                             smote = FALSE
+                             smote = TRUE
                              ){
   # this seems to be the new way of NSE column selection evaluation
   # ref: https://github.com/tidyverse/tidyr/blob/3b0f946d507f53afb86ea625149bbee3a00c83f6/R/spread.R
