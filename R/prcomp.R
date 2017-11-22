d118c20e13083f4898362024e5060744e53e7070
Hideaki Hayashi <hihayash@gmail.com>
2017-11-22 11:43:54 -0800

    prcomp: make biplot work

--- R/prcomp.R
+++ R/prcomp.R
@@ -41,32 +41,31 @@ do_prcomp <- function(df,
 #' extracts results from prcomp as a dataframe
 #' @export
 tidy.prcomp_exploratory <- function(x, type="variances", ...) { #TODO: add test
-  browser()
   if (type == "variances") {
     res <- as.data.frame(x$sdev*x$sdev) # square it to make it variance
     colnames(res)[1] <- "variance"
     res <- tibble::rownames_to_column(res, var="component") %>% # square it to make it variance
-      mutate(component = forcats::fct_inorder(component)) # fct_inorder is to make order on chart right, e.g. Comp.2 before Comp.10
+      mutate(component = forcats::fct_inorder(component)) # fct_inorder is to make order on chart right, e.g. PC2 before PC10
     res <- res %>% dplyr::mutate(cum_pct_variance = cumsum(variance), cum_pct_variance = cum_pct_variance/max(cum_pct_variance)*100)
   }
   else if (type == "loadings") {
     res <- tibble::rownames_to_column(as.data.frame(x$rotation[,]), var="measure")
     res <- res %>% tidyr::gather(component, value, dplyr::starts_with("PC"), na.rm = TRUE, convert = TRUE)
-    res <- res %>% dplyr::mutate(component = forcats::fct_inorder(component)) # fct_inorder is to make order on chart right, e.g. Comp.2 before Comp.10
+    res <- res %>% dplyr::mutate(component = forcats::fct_inorder(component)) # fct_inorder is to make order on chart right, e.g. PC2 before PC10
   }
   else { # should be "biplot"
     res <- x$df
-    scores_matrix <- x$scores[,1:2] # keep only Comp.1 and Comp.2 for biplot
+    scores_matrix <- x$x[,1:2] # keep only PC1 and PC2 for biplot
     max_abs_score <- max(abs(scores_matrix))
     res <- res %>% dplyr::bind_cols(as.data.frame(scores_matrix))
-    loadings_matrix <- x$rotation[,1:2] # keep only Comp.1 and Comp.2 for biplot
+    loadings_matrix <- x$rotation[,1:2] # keep only PC1 and PC2 for biplot
     max_abs_loading <- max(abs(loadings_matrix))
     scale_ratio <- max_abs_score/max_abs_loading
     # scale loading_matrix so that the scale of measures and data points matches in the scatter plot.
     loadings_matrix <- loadings_matrix * scale_ratio
     loadings_df <- tibble::rownames_to_column(as.data.frame(loadings_matrix), var="measure_name") #TODO: what if name conflicts?
-    loadings_df <- loadings_df %>% rename(Measures=Comp.2) # use different column name for Comp.2 of measures.
-    loadings_df0 <- loadings_df %>% mutate(Comp.1=0, Measures=0) # create df for origin of coordinates.
+    loadings_df <- loadings_df %>% rename(Measures=PC2) # use different column name for PC2 of measures.
+    loadings_df0 <- loadings_df %>% mutate(PC1=0, Measures=0) # create df for origin of coordinates.
     loadings_df <- loadings_df0 %>% bind_rows(loadings_df)
     res <- res %>% dplyr::bind_rows(loadings_df)
     # fill group_by column so that Repeat By on chart works fine. loadings_df does not have values for the group_by column.
