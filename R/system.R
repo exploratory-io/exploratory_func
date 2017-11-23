1f804dfb25a7c566d1775fa102be50e36afa98d0
Hidetaka Kojima <hidekoji@gmail.com>
2017-11-23 00:42:31 -0800

    clean up.

--- R/system.R
+++ R/system.R
@@ -1486,7 +1486,7 @@ read_rds_file <- function(file, refhook = NULL){
 #'Wrapper for readr::rea_lines to support vector to data frame conversion
 #'@export
 read_raw_lines <- function(file, locale = readr::default_locale(), na = character(),
-                            comment = "", trim_ws = FALSE, skip = 0, n_max = Inf, guess_max = min(1000, n_max), progress = interactive()){
+                            skip = 0, n_max = Inf, progress = interactive()){
   loadNamespace("readr")
     # if it's local file simply call readr::read_delim
   x <- readr::read_lines(file, locale = locale, na = na, skip = skip, n_max = n_max, progress = progress)
