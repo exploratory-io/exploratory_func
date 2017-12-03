1b5522a9cd9c4af9dfac3d5be2379123c708ac2e
Hideaki Hayashi <hihayash@gmail.com>
2017-12-02 19:03:33 -0800

    Add loess_tidiers.R

---
+++ R/loess_tidiers.R
@@ -0,0 +1,11 @@
+#' loess tidiers
+#' @export
+glance.loess <- function(x, ...) {
+  ret <- data.frame(
+    n_observations=x$n # Number of Observations
+    enp=x$enp # Equivalent Number of Parameters
+    residual_std_error=x$s # Residual Standard Error
+    trace_of_smoother_matrix=$trace.hat # Trace of smoother matrix (exact)
+  )
+  ret
+}
