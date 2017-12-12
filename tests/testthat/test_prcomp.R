9f0e2f4eee3e87102dc4b9ff0c0990bb6a2692b0
Hideaki Hayashi <hihayash@gmail.com>
2017-12-11 19:43:11 -0800

    Add do_prcomp test with strange column name

--- tests/testthat/test_prcomp.R
+++ tests/testthat/test_prcomp.R
@@ -9,3 +9,12 @@ test_that("do_prcomp", {
   model_df %>% tidy(model, type="biplot")
   model_df %>% tidy(model, type="data")
 })
+
+test_that("do_prcomp with strange column name", {
+  df <- mtcars %>% rename('Cy l' = cyl)
+  model_df <- do_prcomp(mtcars, `Cy l`, mpg, hp)
+  model_df %>% tidy(model, type="variances")
+  model_df %>% tidy(model, type="loadings")
+  model_df %>% tidy(model, type="biplot")
+  model_df %>% tidy(model, type="data")
+})
