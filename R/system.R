9c07c6da5e5a1b964fc86bc7cb4fafaf32d12e4f
Hidetaka Kojima <hidekoji@gmail.com>
2017-12-22 15:02:59 -0800

    argument name sync with Exploratory Desktop

--- R/system.R
+++ R/system.R
@@ -195,7 +195,7 @@ getMongoURL <- function(host, port, database, username, pass, isSSL=FALSE, authS
 
 
 #' @export
-queryMongoDB <- function(host, port, database, collection, username, password, query = "{}", isFlatten, limit=100, isSSL=FALSE, authSource=NULL, fields="{}", sort="{}", skip=0, queryType = "find", pipeline="{}"){
+queryMongoDB <- function(host, port, database, collection, username, password, query = "{}", flatten, limit=100, isSSL=FALSE, authSource=NULL, fields="{}", sort="{}", skip=0, queryType = "find", pipeline="{}"){
   if(!requireNamespace("mongolite")){stop("package mongolite must be installed.")}
   loadNamespace("jsonlite")
   if(!requireNamespace("GetoptLong")){stop("package GetoptLong must be installed.")}
@@ -228,7 +228,7 @@ queryMongoDB <- function(host, port, database, collection, username, password, q
     stop(err)
   })
   result <-data
-  if (isFlatten) {
+  if (flatten) {
     result <- jsonlite::flatten(data)
   }
   if (nrow(result)==0) {
