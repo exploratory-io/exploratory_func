1cb6753948ff29f1806fb9a140facf08720fc30f
Hideaki Hayashi <hihayash@gmail.com>
2017-12-28 17:34:38 -0800

    Add test

--- tests/testthat/test_build_lm.R
+++ tests/testthat/test_build_lm.R
@@ -165,6 +165,11 @@ test_that("prediction with glm model with SMOTE by build_lm.fast", {
   # otherwise, the number of rows of the result of prediction becomes 0
   test_data <- dplyr::bind_rows(test_data, test_data)
 
+  model_data <- build_lm.fast(test_data, `CANCELLED X`, `Carrier Name`, CARRIER, DISTANCE, model_type = "glm", smote=FALSE)
+  ret <- model_data %>% broom::glance(model, pretty.name=TRUE)
+  ret <- model_data %>% broom::tidy(model)
+  ret <- model_data %>% broom::augment(model)
+
   model_data <- build_lm.fast(test_data, `CANCELLED X`, `Carrier Name`, CARRIER, DISTANCE, model_type = "glm", smote=TRUE)
   ret <- model_data %>% broom::glance(model, pretty.name=TRUE)
   ret <- model_data %>% broom::tidy(model)
