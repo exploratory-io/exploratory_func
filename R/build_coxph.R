3cc592d9ece80e45062b0bf41fd7f2a369522678
Hideaki Hayashi <hihayash@gmail.com>
2018-01-28 15:30:14 -0800

    Convert ordered factor into unordered in build_coxph.fast too

--- R/build_coxph.R
+++ R/build_coxph.R
@@ -210,12 +210,15 @@ build_coxph.fast <- function(df,
           }
           df[[col]] <- NULL # drop original Date/POSIXct column to pass SMOTE later.
         } else if(!is.numeric(df[[col]])) {
-          # convert data to factor if predictors are not numeric or logical
-          # and limit the number of levels in factor by fct_lump.
-          # we use ties.method to handle the case where there are many unique values. (without it, they all survive fct_lump.)
-          # TODO: see if ties.method would make sense for calc_feature_imp.
-          # also, turn NA into (Missing) factor level so that lm will not drop all the rows.
-          df[[col]] <- forcats::fct_explicit_na(forcats::fct_lump(as.factor(df[[col]]), n=predictor_n, ties.method="first"))
+          # 1. if the data is ordered factor, turn it into unordered. For ordered factor,
+          #    coxph takes polynomial terms (Linear, Quadratic, Cubic, and so on) and use them as variables,
+          #    which we do not want for this function.
+          # 2. convert data to factor if predictors are not numeric or logical
+          #    and limit the number of levels in factor by fct_lump.
+          #    we use ties.method to handle the case where there are many unique values. (without it, they all survive fct_lump.)
+          #    TODO: see if ties.method would make sense for calc_feature_imp.
+          # 3. turn NA into (Missing) factor level so that lm will not drop all the rows.
+          df[[col]] <- forcats::fct_explicit_na(forcats::fct_lump(factor(df[[col]], ordered=FALSE), n=predictor_n, ties.method="first"))
         } else {
           # for numeric cols, filter NA rows, because lm will anyway do this internally, and errors out
           # if the remaining rows are with single value in any predictor column.
--- tests/testthat/test_build_coxph.R
+++ tests/testthat/test_build_coxph.R
@@ -3,6 +3,7 @@ context("test build_coxph")
 test_that("test build_coxph.fast", {
   df <- survival::lung
   df <- df %>% rename(`ti me`=time, `sta tus`=status, `a ge`=age)
+  df <- df %>% mutate(ph.ecog = factor(ph.ecog, ordered=TRUE)) # test handling of ordered factor
   model_df <- df %>% build_coxph.fast(`ti me`, `sta tus`, `a ge`, sex, ph.ecog, ph.karno, pat.karno, meal.cal, wt.loss)
   expect_equal(class(model_df$model[[1]]), c("coxph_exploratory","coxph"))
   ret <- model_df %>% broom::tidy(model)
