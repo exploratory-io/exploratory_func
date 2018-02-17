be7037c67a3d36d2c553d061b8cd98bd151b380e
Hideaki Hayashi <hihayash@gmail.com>
2018-02-17 07:30:06 -0800

    Update comment

--- R/build_coxph.R
+++ R/build_coxph.R
@@ -221,11 +221,11 @@ build_coxph.fast <- function(df,
             df[[col]] <- factor(df[[col]], ordered=FALSE)
           }
         } else if(!is.numeric(df[[col]])) {
-          # 1. convert data to factor if predictors are not numeric or logical
-          #    and limit the number of levels in factor by fct_lump.
+          # 1. convert data to factor if predictors are not numeric or logical.
+          # 2. sort levels by frequency so that base level is the most frequent category.
+          # 3. limit the number of levels in factor by fct_lump.
           #    we use ties.method to handle the case where there are many unique values. (without it, they all survive fct_lump.)
-          #    TODO: see if ties.method would make sense for calc_feature_imp.
-          # 2. turn NA into (Missing) factor level so that coxph will not drop all the rows.
+          # 4. turn NA into (Missing) factor level so that lm will not drop all the rows.
           df[[col]] <- forcats::fct_explicit_na(forcats::fct_lump(fct_infreq(as.factor(df[[col]])), n=predictor_n, ties.method="first"))
         } else {
           # for numeric cols, filter NA rows, because lm will anyway do this internally, and errors out
