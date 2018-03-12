a797de11cc38576fd72e4cf125cdeabb9b8eabf6
Hideaki Hayashi <hihayash@gmail.com>
2018-03-11 18:08:21 -0700

    Handle NA

--- R/test_wrapper.R
+++ R/test_wrapper.R
@@ -547,7 +547,7 @@ exp_normality<- function(df, ..., n_sample = 50) {
     df.qqline <- data.frame()
     df.model <- data.frame()
 　  for (col in selected_cols) {
-      if (n_distinct(df[[col]]) <= 1) {
+      if (n_distinct(df[[col]], na.rm=TRUE) <= 1) {
         # skip if the column has only 1 unique value, to avoid error.
         # TODO: show what happened in the summary table.
       }
