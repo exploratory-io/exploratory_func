2d4aee74555ada09f65429d671aaa5af4b606385
Hideaki Hayashi <hihayash@gmail.com>
2018-04-11 17:21:09 -0700

    Correct comment

--- R/prophet.R
+++ R/prophet.R
@@ -173,7 +173,7 @@ do_prophet_ <- function(df, time_col, value_col = NULL, periods, time_unit = "da
       }
       forecast <- stats::predict(m, future)
     }
-    # with prophet 2.1, now forecast$ds is POSIXct. Cast it to Date so that full_join works.
+    # with prophet 0.2.1, now forecast$ds is POSIXct. Cast it to Date so that full_join works.
     forecast$ds <- as.Date(forecast$ds)
     ret <- forecast %>% dplyr::full_join(aggregated_data, by = c("ds" = "ds"))
     # drop cap_scaled column, which is just scaled capacity, which does not seem informative.
