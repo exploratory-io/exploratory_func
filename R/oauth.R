fbb3f0bfa92bc025c46f01914e798d5030f8028e
Hidetaka Kojima <hidekoji@gmail.com>
2018-04-13 22:14:35 -0700

    clean up

--- R/oauth.R
+++ R/oauth.R
@@ -149,7 +149,6 @@ refreshGoogleTokenForSheet <- function(tokenFileId){
 getTwitterToken <- function(tokenFileId="", useCache=TRUE){
   if(!requireNamespace("twitteR")){stop("package twitteR must be installed.")}
   consumer_key = "0lWpnop0HLfWRbpkDEJ0XA"
-  consumer_secret = "xYNUMALkRnvuT3vls48LW7k2XK1l9xjZTLnRv2JaFaM"
   appName = "twitter"
   endpointType = "twitter"
   # retrieve token info from environment
@@ -162,7 +161,7 @@ getTwitterToken <- function(tokenFileId="", useCache=TRUE){
       access = "https://api.twitter.com/oauth/access_token",
       appname = "twitter",
       key = consumer_key,
-      secret = consumer_secret,
+      secret = token_info$consumer_sc,
       credentials = list(
         oauth_token = token_info$oauth_token,
         oauth_token_secret = token_info$oauth_token_secret,
