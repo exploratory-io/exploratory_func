ce80cd94435d419f804481783cd4ec88f1abecf4
Hideaki Hayashi <hihayash@gmail.com>
2018-05-19 18:20:04 -0700

    Import tidy,glance, and augment from broom rather than from tidytext

--- R/importer.R
+++ R/importer.R
@@ -24,21 +24,17 @@ anonymize <- function(vec, algo = "sha256", seed = 0, chars = letters, n_chars =
   anonymizer::anonymize(vec, .algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
 }
 
-# TODO: not sure why we are importing tidy from tidytext and not from broom.
-# probably some historical reason, but this does not seem to be working either.
-# tidy does not seem to by on the namespace after loading exploratory pkg.
-#
 # Importing tidy is needed to define tidy methods to other classes
-#' @importFrom tidytext tidy
+#' @importFrom broom tidy
 #' @export
 tidy
 
 # Importing glance is needed to define glance methods to other classes
-#' @importFrom tidytext glance
+#' @importFrom broom glance
 #' @export
 glance
 
 # Importing augment is needed to define augment methods to other classes
-#' @importFrom tidytext augment
+#' @importFrom broom augment
 #' @export
 augment
