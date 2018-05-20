2a0aa78a2311e1611a0cac6dbc2e4c9b1e5d307b
Hideaki Hayashi <hihayash@gmail.com>
2018-05-19 17:47:56 -0700

    Add TODO comment

--- R/importer.R
+++ R/importer.R
@@ -24,6 +24,10 @@ anonymize <- function(vec, algo = "sha256", seed = 0, chars = letters, n_chars =
   anonymizer::anonymize(vec, .algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
 }
 
+# TODO: not sure why we are importing tidy from tidytext and not from broom.
+# probably some historical reason, but this does not seem to be working either.
+# tidy does not seem to by on the namespace after loading exploratory pkg.
+#
 # Importing tidy is needed to define tidy methods to other classes
 #' @importFrom tidytext tidy
 #' @export
