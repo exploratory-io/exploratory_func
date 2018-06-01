d40689605938212a3aca41f43fecc7f81caf3916
Hideaki Hayashi <hihayash@gmail.com>
2018-06-01 14:59:58 -0700

    Handle the case where column for count is not passed in

--- tests/testthat/test_exp_bayes_ab.R
+++ tests/testthat/test_exp_bayes_ab.R
@@ -5,30 +5,34 @@ test_that("test exp_bayes_ab test with summary output", {
   data_a <- data.frame(
     access_count = round(runif(50) * 100 + 100)
   ) %>%
-    dplyr::mutate(click = 0.2 + 0.8 * runif(nrow(.)), group = "a")
+    dplyr::mutate(click = 0.2 + 0.8 * runif(nrow(.)), `gro up` = "a")
   data_b <- data.frame(
     access_count = round(runif(20) * 20 + 20)
   ) %>%
-    dplyr::mutate(click = 0.3 + 0.7 * runif(nrow(.)), group = "b")
+    dplyr::mutate(click = 0.3 + 0.7 * runif(nrow(.)), `gro up` = "b")
 
   full_data <- dplyr::bind_rows(data_a, data_b) %>%
     mutate(grouping = rbinom(n(), 1, 0.3)) %>%
     group_by(grouping)
   full_data <- full_data %>%
     dplyr::mutate(converted_true=as.integer(access_count*click), converted_false=as.integer(access_count*(1-click))) %>%
-    tidyr::gather(key="converted", value="count", converted_true, converted_false) %>%
-    dplyr::mutate(converted = converted=="converted_true")
+    tidyr::gather(key="conver ted", value="count col", converted_true, converted_false) %>%
+    dplyr::mutate(`conver ted` = `conver ted`=="converted_true")
 
   #full_data <- full_data %>% rename(`access count`=access_count, `cli ck`=click)
-  ret <- exp_bayes_ab(full_data, converted, group, count, prior_mean=0.1, prior_sd=0.01, type = "summary")
+  ret <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, type = "summary")
   expect_equal(nrow(ret), 4)
 
   # without prior
-  ret <- exp_bayes_ab(full_data, converted, group, count, type = "summary")
+  ret <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, type = "summary")
+  expect_equal(nrow(ret), 4)
+
+  # without count with NULL
+  ret <- exp_bayes_ab(full_data, `conver ted`, `gro up`, NULL, type = "summary")
   expect_equal(nrow(ret), 4)
 
   # without count
-  ret <- exp_bayes_ab(full_data, converted, group, , type = "summary")
+  ret <- exp_bayes_ab(full_data, `conver ted`, `gro up`, , type = "summary")
   expect_equal(nrow(ret), 4)
 })
 
