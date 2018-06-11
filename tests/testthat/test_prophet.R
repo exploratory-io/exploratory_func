a20a1b3e4ba12d9262950ad8a4ae2f30e42c19b3
Hideaki Hayashi <hihayash@gmail.com>
2018-06-11 13:44:56 -0700

    Add test for do_prophet with hour, minute, second

--- tests/testthat/test_prophet.R
+++ tests/testthat/test_prophet.R
@@ -8,16 +8,30 @@ test_that("do_prophet with aggregation", {
     do_prophet(`time stamp`, `cou nt`, 10, time_unit = "day")
   ret <- raw_data %>%
     do_prophet(`time stamp`, `cou nt`, 10, time_unit = "hour")
-  # comment out for now since the following takes time. TODO: modify data and make it faster.
-  # ret <- raw_data %>%
-  #   do_prophet(`time stamp`, `cou nt`, 10, time_unit = "minute")
-  # ret <- raw_data %>%
-  #   do_prophet(`time stamp`, `cou nt`, 10, time_unit = "second")
+  ret <- raw_data %>% tail(100) %>%
+    do_prophet(`time stamp`, `cou nt`, 10, time_unit = "minute")
+  ret <- raw_data %>% tail(100) %>%
+    do_prophet(`time stamp`, `cou nt`, 10, time_unit = "second")
 
   # test for test mode.
   raw_data$`cou nt`[[length(raw_data$`cou nt`) - 2]] <- NA # inject NA near the end to test #9211
   ret <- raw_data %>%
     do_prophet(`time stamp`, `cou nt`, 2, time_unit = "day", test_mode=TRUE)
+  # verify that the last forecasted_value is not NA to test #9211
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+
+  ret <- raw_data %>%
+    do_prophet(`time stamp`, `cou nt`, 2, time_unit = "hour", test_mode=TRUE)
+  # verify that the last forecasted_value is not NA to test #9211
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+
+  ret <- raw_data %>% tail(100) %>%
+    do_prophet(`time stamp`, `cou nt`, 2, time_unit = "minute", test_mode=TRUE)
+  # verify that the last forecasted_value is not NA to test #9211
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+
+  ret <- raw_data %>% tail(100) %>%
+    do_prophet(`time stamp`, `cou nt`, 2, time_unit = "second", test_mode=TRUE)
   # verify that the last forecasted_value is not NA to test #9211
   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
 })
