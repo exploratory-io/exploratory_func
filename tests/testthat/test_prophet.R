11d0a952d2eca943e8b15be7d3539292cf768b79
Hideaki Hayashi <hihayash@gmail.com>
2018-07-13 14:02:05 -0700

    Add test

--- tests/testthat/test_prophet.R
+++ tests/testthat/test_prophet.R
@@ -36,6 +36,45 @@ test_that("do_prophet with aggregation", {
   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
 })
 
+
+# This test is too slow. TODO: make it faster and enable.
+# test_that("do_prophet test mode with second as time units", {
+#   ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-3:00:00"), by="sec")
+#   raw_data <- data.frame(timestamp=ts, data=runif(length(ts))) %>% dplyr::rename(`time stamp`=timestamp, `da ta`=data)
+#   raw_data$`da ta`[[length(ts) - 2]] <- NA # inject NA near the end to test #9211
+#   ret <- raw_data %>%
+#     do_prophet(`time stamp`, `da ta`, 10, time_unit = "second", test_mode=TRUE)
+#   # verify that the last forecasted_value is not NA to test #9211
+#   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+#   # verify that daily, weekly is enabled to test #9361.
+#   expect_equal(c("daily") %in% colnames(ret),c(T))
+# })
+
+# This test is slow. TODO: make it faster.
+test_that("do_prophet test mode with minute as time units", {
+  ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-15:00:00"), by="min")
+  raw_data <- data.frame(timestamp=ts, data=runif(length(ts))) %>% dplyr::rename(`time stamp`=timestamp, `da ta`=data)
+  raw_data$`da ta`[[length(ts) - 2]] <- NA # inject NA near the end to test #9211
+  ret <- raw_data %>%
+    do_prophet(`time stamp`, `da ta`, 10, time_unit = "minute", test_mode=TRUE)
+  # verify that the last forecasted_value is not NA to test #9211
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+  # verify that daily, weekly is enabled to test #9361.
+  expect_equal(c("daily","weekly") %in% colnames(ret),c(T,T))
+})
+
+test_that("do_prophet test mode with hour as time units", {
+  ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-15:00:00"), by="hour")
+  raw_data <- data.frame(timestamp=ts, data=runif(length(ts))) %>% dplyr::rename(`time stamp`=timestamp, `da ta`=data)
+  raw_data$`da ta`[[length(ts) - 2]] <- NA # inject NA near the end to test #9211
+  ret <- raw_data %>%
+    do_prophet(`time stamp`, `da ta`, 10, time_unit = "hour", test_mode=TRUE)
+  # verify that the last forecasted_value is not NA to test #9211
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+  # verify that daily, weekly is enabled to test #9361.
+  expect_equal(c("daily","weekly") %in% colnames(ret),c(T,T))
+})
+
 test_that("do_prophet test mode with month as time units", {
   ts <- seq.Date(as.Date("2010-01-01"), as.Date("2030-01-01"), by="month")
   raw_data <- data.frame(timestamp=ts, data=runif(length(ts))) %>% dplyr::rename(`time stamp`=timestamp, `da ta`=data)
