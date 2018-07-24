716f6d5548ca152b670d585874b00c201d480cd8
Hideaki Hayashi <hihayash@gmail.com>
2018-07-24 16:47:39 -0700

    Initial implementation of exp_rpart

---
+++ R/rpart.R
@@ -0,0 +1,21 @@
+#' @export
+exp_rpart <- function(df,
+                      target,
+                      ...) {
+  # this seems to be the new way of NSE column selection evaluation
+  # ref: https://github.com/tidyverse/tidyr/blob/3b0f946d507f53afb86ea625149bbee3a00c83f6/R/spread.R
+  target_col <- dplyr::select_var(names(df), !! rlang::enquo(target))
+  # this evaluates select arguments like starts_with
+  selected_cols <- dplyr::select_vars(names(df), !!! rlang::quos(...))
+
+  grouped_cols <- grouped_by(df)
+
+  each_func <- function(df) {
+    rhs <- paste0("`", selected_cols, "`", collapse = " + ")
+    fml <- as.formula(paste0("`", target_col, "`", " ~ ", rhs))
+    model_df <- rpart::rpart(fml, df)
+    model_df
+  }
+
+  do_on_each_group(df, each_func, name = "model", with_unnest = FALSE)
+}
