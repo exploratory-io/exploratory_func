2445505efea98107114ac2078e821474af18c607
Hideaki Hayashi <hihayash@gmail.com>
2018-09-01 16:40:40 -0700

    Add test for holiday with monthly data

--- tests/testthat/test_prophet.R
+++ tests/testthat/test_prophet.R
@@ -146,6 +146,21 @@ test_that("do_prophet with holiday column", {
   expect_equal(ret$timestamp[[length(ret$timestamp)]], as.Date("2012-01-11"))
 })
 
+test_that("do_prophet with holiday column with monthly data", {
+  ts <- seq.Date(as.Date("2010-01-01"), as.Date("2012-01-01"), by="month")
+  raw_data <- data.frame(timestamp=ts, data=runif(length(ts)))
+  ts2 <- seq.Date(as.Date("2010-01-01"), as.Date("2013-01-01"), by="month")
+  regressor_data <- data.frame(timestamp=ts2, regressor=runif(length(ts2)), holiday=if_else(runif(length(ts2)) > 0.90,"holiday",NA_character_)) %>%
+    mutate(holiday=as.character(holiday)) %>%
+    rename(`holi day`=holiday)
+  combined_data <- raw_data %>% full_join(regressor_data, by=c("timestamp"="timestamp"))
+  ret <- combined_data %>%
+    do_prophet(timestamp, data, 10, time_unit = "month", holiday=`holi day`)
+  # verify that the last forecasted_value is not NA
+  expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
+  expect_equal(ret$timestamp[[length(ret$timestamp)]], as.Date("2012-11-01"))
+})
+
 test_that("do_prophet with holiday column with hourly data", {
   ts <- seq(as.POSIXct("2010-01-01 00:00:00"), as.POSIXct("2010-01-15 00:00:00"), by="hour")
   raw_data <- data.frame(timestamp=ts, data=runif(length(ts)))
