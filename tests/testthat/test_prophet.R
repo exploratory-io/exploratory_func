d923354434251eb0efe16483dbbbfbf340eeec3a
Hideaki Hayashi <hihayash@gmail.com>
2018-09-01 16:33:10 -0700

    Test holiday with strange column name

--- tests/testthat/test_prophet.R
+++ tests/testthat/test_prophet.R
@@ -136,10 +136,11 @@ test_that("do_prophet with holiday column", {
   raw_data <- data.frame(timestamp=ts, data=runif(length(ts)))
   ts2 <- seq.Date(as.Date("2010-01-01"), as.Date("2013-01-01"), by="day")
   regressor_data <- data.frame(timestamp=ts2, regressor=runif(length(ts2)), holiday=if_else(runif(length(ts2)) > 0.90,"holiday",NA_character_)) %>%
-    mutate(holiday=as.character(holiday))
+    mutate(holiday=as.character(holiday)) %>%
+    rename(`holi day`=holiday)
   combined_data <- raw_data %>% full_join(regressor_data, by=c("timestamp"="timestamp"))
   ret <- combined_data %>%
-    do_prophet(timestamp, data, 10, time_unit = "day", holiday=holiday)
+    do_prophet(timestamp, data, 10, time_unit = "day", holiday=`holi day`)
   # verify that the last forecasted_value is not NA
   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
   expect_equal(ret$timestamp[[length(ret$timestamp)]], as.Date("2012-01-11"))
