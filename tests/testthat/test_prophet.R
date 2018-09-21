aae4ea89cd35c99b965e92324bd7504f0444f62e
IEUser <hideaki@exploratory.io>
2018-09-20 17:42:21 -0700

    Avoid memory error on 32bit on prophet test

--- tests/testthat/test_prophet.R
+++ tests/testthat/test_prophet.R
@@ -52,7 +52,12 @@ test_that("do_prophet with aggregation", {
 
 # This test is slow. TODO: make it faster.
 test_that("do_prophet test mode with minute as time units", {
-  ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-15:00:00"), by="min")
+  if (Sys.info()["machine"] == "x86") { # need to be shorter on win 32bit to avoid memory error.
+    ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-3:00:00"), by="min")
+  }
+  else {
+    ts <- seq(as.POSIXct("2010-01-01:00:00:00"), as.POSIXct("2010-01-15:00:00"), by="min")
+  }
   raw_data <- data.frame(timestamp=ts, data=runif(length(ts))) %>% dplyr::rename(`time stamp`=timestamp, `da ta`=data)
   raw_data$`da ta`[[length(ts) - 2]] <- NA # inject NA near the end to test #9211
   ret <- raw_data %>%
@@ -60,7 +65,12 @@ test_that("do_prophet test mode with minute as time units", {
   # verify that the last forecasted_value is not NA to test #9211
   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
   # verify that daily, weekly is enabled to test #9361.
-  expect_equal(c("daily","weekly") %in% colnames(ret),c(T,T))
+  if (Sys.info()["machine"] == "x86") { # cannot make it long enough for weekly on win 32bit to avoid memory error.
+    expect_equal(c("daily","weekly") %in% colnames(ret),c(T,F))
+  }
+  else {
+    expect_equal(c("daily","weekly") %in% colnames(ret),c(T,T))
+  }
 })
 
 test_that("do_prophet test mode with hour as time units", {
