97b6fb084a7e1896066f79bf46d72011556276fa
Hidetaka Kojima <hidekoji@gmail.com>
2018-10-24 11:00:44 -0700

    refactoring.

--- R/system.R
+++ R/system.R
@@ -1573,9 +1573,9 @@ read_delim_file <- function(file, delim, quote = '"',
     # if it's local file simply call readr::read_delim
     # reading through file() is to be able to read files with path that includes multibyte chars.
     # without it, error is thrown from inside read_delim.
-    file_path = file
+    file_path <- file
     if(stringi::stri_enc_mark(file) != "ASCII"){
-      file_path = file(file)
+      file_path <- file(file)
     }
     readr::read_delim(file_path, delim, quote = quote, escape_backslash = escape_backslash, escape_double = escape_double, col_names = col_names, col_types = col_types,
                       locale = locale, na = na, quoted_na = quoted_na, comment = comment, trim_ws = trim_ws, skip = skip, n_max = n_max, guess_max = guess_max, progress = progress)
