103f24c47d611e2814cf1c023b5cf211a0c8b58a
Hidetaka Kojima <hidekoji@gmail.com>
2018-12-11 20:21:39 -0800

    update comment.

--- R/system.R
+++ R/system.R
@@ -393,7 +393,7 @@ getAWSAthenaConnection <- function(driver = "", region = "", authenticationType 
   }
   if (is.null(conn)) {
     #https://www.rdocumentation.org/packages/RODBC/versions/1.3-15/topics/odbcConnect
-    #For better performance, set max value (i.e. 1024) for rows_at_time's
+    #For better performance, set max value (i.e. 1024) for rows_at_time
     conn <- RODBC::odbcDriverConnect(connection = connectionString, rows_at_time = 1024)
     if (user_env$pool_connection) { # pool connection if connection pooling is on.
       connection_pool[[connectionString]] <- conn
