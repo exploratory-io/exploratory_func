9eaf819682372de3d21e38fc67d9ddb3674910c5
Hideaki Hayashi <hihayash@gmail.com>
2018-12-16 15:38:18 -0800

    Add test for NA fill

--- tests/testthat/test_anomaly_detection.R
+++ tests/testthat/test_anomaly_detection.R
@@ -62,3 +62,23 @@ test_that("do_anomary_detection with daily POSIXct data with timezone with dayli
   # Check that aggretated value at a daylight saving day has a valid value. Once there was an issue that broke this.
   expect_true(ret$y[200] > 0)
 })
+
+test_that("do_anomary_detection with missin days filled", {
+  ts <- seq.Date(as.Date("2018-01-01"), as.Date("2018-12-31"), by="day")
+  raw_data <- data.frame(timestamp=ts, y=runif(length(ts))) %>% filter(wday(timestamp) %nin% c(1,7))
+
+  # Test previous value fill.
+  ret <- raw_data %>%
+    do_anomaly_detection(timestamp, y, time_unit = "day", na_fill_type = "previous")
+  # Check that missing values are filled with previous valid value.
+  expect_true(ret$y[5] == ret$y[6] & ret$y[6] == ret$y[7])
+
+  # Test previous value fill.
+  ret <- raw_data %>%
+    do_anomaly_detection(timestamp, y, time_unit = "day", na_fill_type = "interpolate")
+  # Check that missing values are filled linearly.
+  interpolate_diff <- ret$y[5] - ret$y[6]
+  expect_equal(ret$y[6] - ret$y[7], interpolate_diff)
+  expect_equal(ret$y[7] - ret$y[8], interpolate_diff)
+})
+
