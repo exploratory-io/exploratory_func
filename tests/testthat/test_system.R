4df33a8844219d43bc99c6da96a365923daa0f94
Hideaki Hayashi <hihayash@gmail.com>
2018-12-23 19:39:01 -0800

    Update test

--- tests/testthat/test_system.R
+++ tests/testthat/test_system.R
@@ -140,9 +140,12 @@ test_that("odbc_glue_transformer", {
   exploratory_env$v <- c(1,2,3)
   res <- glue::glue("@{ v }", .transformer=odbc_glue_transformer, .open="@{", .close="}")
   expect_equal(as.character(res), "1, 2, 3")
+  exploratory_env$v <- 1
+  res <- glue::glue("{a: {x: @{v}}} ", .transformer=odbc_glue_transformer, .open="@{", .close="}")
+  expect_equal(as.character(res), "{a: {x: 1}}")
   exploratory_env$v <- c("a","b","c")
-  res <- glue::glue("@{ `v` }", .transformer=odbc_glue_transformer, .open="@{", .close="}")
-  expect_equal(as.character(res), "'a', 'b', 'c'") # Not sure if this behavior works for all types of databases.
+  res <- glue::glue("[@{ `v` }]", .transformer=odbc_glue_transformer, .open="@{", .close="}")
+  expect_equal(as.character(res), "['a', 'b', 'c']") # Not sure if this behavior works for all types of databases.
 })
 
 test_that("bigquery_glue_transformer", {
