bcb8e221c9ac6082568c8e8fec237b28b1b93e38
Hideaki Hayashi <hihayash@gmail.com>
2018-12-27 16:33:31 -0800

    Move fill_ts_na to na_util.R

--- R/anomaly_detection.R
+++ R/anomaly_detection.R
@@ -7,49 +7,6 @@ do_anomaly_detection <- function(df, time, value = NULL, ...){
   do_anomaly_detection_(df, time_col, value_col, ...)
 }
 
-#' Fill NAs in time series data. TODO: Merge into impute_na?
-#' @param type - Type of NA fill:
-#'                       "previous" - Fill with previous non-NA value.
-#'                       "value" - Fill with the value of na_fill_value.
-#'                       "interpolate" - Linear interpolation.
-#'                       "spline" - Spline interpolation.
-#'                       NULL - Skip NA fill. Use this only when you know there is no NA.
-#' @param val - Value to fill NA when na_fill_type is "value"
-fill_ts_na <- function(target, time, type = "previous", val = 0) {
-  df_zoo <- zoo::zoo(target, time)
-  # fill NAs in the input
-  # when some date or time are missing,
-  # AnomalyDetection::AnomalyDetectionTs throws this error
-  # "Anom detection needs at least 2 periods worth of data"
-  if (type == "spline") {
-    df_zoo <- zoo::na.spline(df_zoo)
-  }
-  else if (type == "interpolate") {
-    df_zoo <- zoo::na.approx(df_zoo)
-  }
-  else if (type == "previous") {
-    df_zoo <- zoo::na.locf(df_zoo)
-  }
-  # TODO: Getting this error with some input with na.StructTS().
-  #       Error in rowSums(tsSmooth(StructTS(y))[, -2]) : 'x' must be an array of at least two dimensions
-  #
-  # else if (type == "StructTS") {
-  #   df_zoo <- zoo::na.StructTS(df_zoo)
-  # }
-  else if (type == "value") {
-    df_zoo <- zoo::na.fill(df_zoo, val)
-  }
-  else if (is.null(type)) {
-    # skip when it is NULL. this is for the case caller is confident that
-    # there is no NA and want to skip overhead of checking for NA.
-  }
-  else {
-    stop(paste0(type, " is not a valid option for NA fill type."))
-  }
-  ret <- zoo::coredata(df_zoo)
-  ret
-}
-
 #' Detect anomaly data
 #' @param df Data frame
 #' @param time_col Column that has time data
