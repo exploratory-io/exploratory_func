f87dea1661d165de8302c524abaffa3be73d44a8
Hideaki Hayashi <hihayash@gmail.com>
2019-02-11 18:18:49 -0800

    Add test for ordered factor as a predictor for smote

--- tests/testthat/test_smote.R
+++ tests/testthat/test_smote.R
@@ -40,6 +40,18 @@ test_that("test exp_balance with factor", {
   expect_equal(levels(res$y), c("a","b"))
 })
 
+test_that("test exp_balance with ordered factor with NA as a predictors", {
+  sample_data <- data.frame(
+    y = factor(c("a", "b", "b", "b", "b", "b")),
+    x = factor(c("A", "A", "B", "B", NA, "B"), ordered=TRUE),
+    num = runif(6)
+  )
+  res <- exp_balance(sample_data, y)
+  expect_true("data.frame" %in% class(res))
+  expect_equal(class(res$x), "factor")
+  expect_equal(levels(res$x), c("A", "B", "(Missing)"))
+})
+
 test_that("test exp_balance with logical", {
   sample_data <- data.frame(
     y = c(TRUE, rep(FALSE,5)),
