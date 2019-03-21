d1f3e2b675c458eba63d9257fb1c9441307ee8dc
Hideaki Hayashi <hihayash@gmail.com>
2019-03-21 16:36:03 -0700

    Comment on underscore

--- R/build_lm.R
+++ R/build_lm.R
@@ -600,7 +600,7 @@ glance.glm_exploratory <- function(x, pretty.name = FALSE, ...) { #TODO: add tes
 xlevels_to_base_level_table <- function(xlevels) {
   term <- purrr::flatten_chr(purrr::map(names(xlevels), function(vname) {
     # Quote variable name with backtick if it includes special characters or space.
-    # Special characters to detect besides space. Note that period should *not* be included here. : ~!@#$%^&*()+={}|:;'<>,/?"[]-\
+    # Special characters to detect besides space. Note that period and underscore should *not* be included here. : ~!@#$%^&*()+={}|:;'<>,/?"[]-\
     paste0(if_else(stringr::str_detect(vname,"[ ~!@#$%^&*()+={}|:;'<>,/?\"\\[\\]\\-\\\\]"),paste0('`',vname,'`'),vname),xlevels[[vname]])
   }))
   base_level <- purrr::flatten_chr(purrr::map(xlevels, function(v){rep(v[[1]],length(v))}))
