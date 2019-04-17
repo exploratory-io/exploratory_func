96b1e3a2960e45d6f1fac9eeb12be33904960f61
Hidetaka Kojima <hidekoji@gmail.com>
2019-04-17 10:34:22 -0700

    Treasure Data Support

--- R/presto.R
+++ R/presto.R
@@ -1,9 +1,17 @@
 #' @export
-queryPresto <- function(host, port, username, password, schema, catalog, numOfRows = -1, query, ...){
+queryPresto <- function(host, port, username, password = "", schema, catalog, numOfRows = -1, query, ...){
+  if(!requireNamespace("httr")){stop("package httr must be installed.")}
+  if(!requireNamespace("stringr")){stop("package stringr must be installed.")}
+  if(!requireNamespace("RPresto")){stop("package RPresto must be installed.")}
 
   # read stored password
   pass = saveOrReadPassword("presto", username, password)
   drv <- RPresto::Presto()
+  # To workaround Presto Authentication issue, set X-Presto-User to http header.
+  # Please refer https://github.com/prestodb/RPresto/issues/103 for details.
+  httr::set_config(
+    httr::add_headers(stringr::str_c("X-Presto-User", username, sep = "="))
+  )
   conn <- RPresto::dbConnect(drv, user = username,
                                 password = pass, host = host, port = port, schema = schema, catalog = catalog, session.timezone = Sys.timezone(location = TRUE))
   # set envir = parent.frame() to get variables from users environment, not papckage environment
