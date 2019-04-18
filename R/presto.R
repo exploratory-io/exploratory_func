b2deeb7f18fefdef23e796236b10e05bf43eb3d7
Hidetaka Kojima <hidekoji@gmail.com>
2019-04-18 09:31:10 -0700

    Changed to use centralized getDBConnection and clearDBConection for Presto and Treasure Data also supported connection pooling.

--- R/presto.R
+++ R/presto.R
@@ -4,23 +4,23 @@ queryPresto <- function(host, port, username, password = "", schema, catalog, nu
   if(!requireNamespace("stringr")){stop("package stringr must be installed.")}
   if(!requireNamespace("RPresto")){stop("package RPresto must be installed.")}
 
+
   # read stored password
   pass = saveOrReadPassword("presto", username, password)
-  drv <- RPresto::Presto()
-  # To workaround Presto Authentication issue, set X-Presto-User to http header.
-  # Please refer https://github.com/prestodb/RPresto/issues/103 for details.
-  httr::set_config(
-    httr::add_headers(stringr::str_c("X-Presto-User", username, sep = "="))
-  )
-  conn <- RPresto::dbConnect(drv, user = username,
-                                password = pass, host = host, port = port, schema = schema, catalog = catalog, session.timezone = Sys.timezone(location = TRUE))
-  query <- convertUserInputToUtf8(query)
-  # set envir = parent.frame() to get variables from users environment, not papckage environment
-  # glue_sql does not quote Date or POSIXct. Let's use our sql_glue_transformer here.
-  query <- glue_exploratory(query, .transformer=sql_glue_transformer, .envir = parent.frame())
-  resultSet <- RPresto::dbSendQuery(conn,query)
-  df <- DBI::dbFetch(resultSet, n = numOfRows)
+  conn = getDBConnection("presto", host = host, port = port, databaseName = "", username = username, password = pass, catalog = catalog, schema = schema, dsn="", additionalParams = "",
+  collection = "", isSSL = FALSE, authSource = NULL, cluster = NULL, timeout = NULL)
+  tryCatch({
+    query <- convertUserInputToUtf8(query)
+    # set envir = parent.frame() to get variables from users environment, not papckage environment
+    # glue_sql does not quote Date or POSIXct. Let's use our sql_glue_transformer here.
+    query <- glue_exploratory(query, .transformer=sql_glue_transformer, .envir = parent.frame())
+    resultSet <- RPresto::dbSendQuery(conn,query)
+    df <- DBI::dbFetch(resultSet, n = numOfRows)
+  }, error = function(err) {
+    # clear connection in pool so that new connection will be used for the next try
+    clearDBConnection("presto", host, port, databaseName, username)
+    stop(err)
+  })
   RPresto::dbClearResult(resultSet)
-  RPresto::dbDisconnect(conn)
   df
 }
