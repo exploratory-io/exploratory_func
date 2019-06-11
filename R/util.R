2a484d557822fcfb913b9e815543caa94110c14e
Hidetaka Kojima <hidekoji@gmail.com>
2019-06-11 14:36:56 -0700

    Updated comment.

--- R/util.R
+++ R/util.R
@@ -1484,7 +1484,7 @@ extract_argument_names <- function(...) {
 #'Wrapper function for dplyr::bind_rows to support named data frames when it's called inside dplyr chain.
 #'@export
 bind_rows <- function(..., id_column_name = NULL, current_df_name = '', force_data_type = FALSE, .id = NULL) {
-  # for backward compatiblity
+  # for compatiblity with dply::bind_rows
   # if old .id argument is passed and id_column_name is NA
   # use old .id argumetn value as id_column_name
   if(!is.null(.id) && is.null(id_column_name)) {
