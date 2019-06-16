ef92ccb98c5a86d847da5efc010ef8633bf406b1
Hideaki Hayashi <hihayash@gmail.com>
2019-06-16 08:15:40 -0700

    Skeleton of power analysis in glance.chisq_exploratory

--- R/test_wrapper.R
+++ R/test_wrapper.R
@@ -352,7 +352,13 @@ tidy.chisq_exploratory <- function(x, type = "observed") {
 glance.chisq_exploratory <- function(x) {
   # ret <- x %>% broom:::glance.htest() # for some reason this does not work. just do it like following.
   ret <- data.frame(statistic=x$statistic, parameter=x$parameter, p.value=x$p.value)
-  ret <- ret %>% rename(`Chi-Square`=statistic, `Degree of Freedom`=parameter, `P Value`=p.value)
+  if (is.null(x$power)) {
+    # If power is not specified in the arguments, estimate current power.
+    ret <- ret %>% rename(`Chi-Square`=statistic, `Degree of Freedom`=parameter, `P Value`=p.value)
+  }
+  else {
+    ret <- ret %>% rename(`Chi-Square`=statistic, `Degree of Freedom`=parameter, `P Value`=p.value)
+  }
   ret
 }
 
