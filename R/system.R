e6de21e5faca148599c7684cecdacecea3087466
Hidetaka Kojima <hidekoji@gmail.com>
2019-06-18 09:12:43 -0700

    fix Data Source: Could not open an SPSS file and got No method asJSON S3 class: haven_labelle error

--- R/system.R
+++ R/system.R
@@ -1317,7 +1317,8 @@ scrape_html_table <- function(url, index, heading, encoding = NULL) {
 #' see https://github.com/exploratory-io/tam/issues/1481
 #' @export
 handleLabelledColumns = function(df){
-  is_labelled <- which(lapply(df, class) == "labelled")
+  # check if column class is labelled, have_labelled, etc
+  is_labelled <- which(stringr::str_detect(lapply(df, class), "labelled"))
   df[is_labelled] <- lapply(df[is_labelled], haven::as_factor)
   df
 }
