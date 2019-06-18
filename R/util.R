934cff8cfc97769f7959fdb04981a051ca5294a8
Hideaki Hayashi <hihayash@gmail.com>
2019-06-18 13:21:01 -0700

    Rename Mode to get_mode

--- R/util.R
+++ R/util.R
@@ -1638,7 +1638,7 @@ setdiff <- function(x, y, force_data_type = FALSE, ...){
 #'Calculates mode. Function name is capitalized to avoid conflict with base::mode(), which does something other than calculating mode.
 # Reference: https://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
 #'@export
-Mode <- function(x, na.rm = FALSE) {
+get_mode <- function(x, na.rm = FALSE) {
   if(na.rm){
     x = x[!is.na(x)]
   }
