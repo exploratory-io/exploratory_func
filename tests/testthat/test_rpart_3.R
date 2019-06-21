2d995cb38dd942757debeeedecf170a5b0213ce1
Hideaki Hayashi <hihayash@gmail.com>
2019-06-21 13:00:18 -0700

    Add test for rpart training/test prediction with group_by

---
+++ tests/testthat/test_rpart_3.R
@@ -0,0 +1,74 @@
+# how to run this test:
+# devtools::test(filter="rpart_3_group")
+# This is same test as test_rpart_2.R with group_by.
+
+context("test tidiers for randomForest 3")
+
+testdata_dir <- "~/.exploratory/"
+testdata_filename <- "airline_2013_10_tricky_v3.csv" 
+testdata_file_path <- paste0(testdata_dir, testdata_filename)
+
+filepath <- if (!testdata_filename %in% list.files(testdata_dir)) {
+  "https://www.dropbox.com/s/f47baw5f3v0xoll/airline_2013_10_tricky_v3.csv?dl=1"
+} else {
+  testdata_file_path
+}
+if (!exists("flight_downloaded")) {
+  flight_downloaded <- exploratory::read_delim_file(filepath, ",", quote = "\"", skip = 0 , col_names = TRUE , na = c("","NA") , locale=readr::locale(encoding = "UTF-8", decimal_mark = "."), trim_ws = FALSE , progress = FALSE) %>% exploratory::clean_data_frame()
+  write.csv(flight_downloaded, testdata_file_path)
+  flight <- flight_downloaded
+} else {
+  flight <- flight_downloaded
+}
+
+# Add group_by. Cases without group_by is covered in test_randomForest_tidiers_3.R.
+flight <- flight %>% sample_n(5000) %>% group_by(`CAR RIER`)
+
+
+test_that("calc_feature_map(regression) evaluate training and test", {
+  model_df <- flight %>%
+                exp_rpart(`FL NUM`, `DIS TANCE`, `DEP TIME`, test_rate = 0.3)
+  # Skipping following to focus on prediction for now.
+  #ret <- rf_evaluation_training_and_test(model_df, test_rate = 0.3)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "evaluation_by_class", test_rate = 0.3)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "conf_mat", test_rate = 0.3)
+
+  ret <- model_df %>% prediction_training_and_test()
+  test_ret <- ret %>% filter(is_test_data==TRUE)
+  # expect_equal(nrow(test_ret), 1500) # Fails for now
+  train_ret <- ret %>% filter(is_test_data==FALSE)
+  # expect_equal(nrow(train_ret), 3500) # Fails for now
+})
+
+test_that("calc_feature_map(binary) evaluate training and test", {
+  model_df <- flight %>%
+                # filter(`CAR RIER` %in% c("VA","AA")) %>%
+                dplyr::mutate(is_delayed = as.factor(`is delayed`)) %>%
+                exp_rpart(is_delayed, `DIS TANCE`, `DEP TIME`, test_rate = 0.3)
+  # Skipping following to focus on prediction for now.
+  #ret <- rf_evaluation_training_and_test(model_df, test_rate = 0.3)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "evaluation_by_class", test_rate = 0.3)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "conf_mat", test_rate = 0.3)
+
+  ret <- model_df %>% prediction_training_and_test()
+  test_ret <- ret %>% filter(is_test_data==TRUE)
+  # expect_equal(nrow(test_ret), 1500) # Fails for now
+  train_ret <- ret %>% filter(is_test_data==FALSE)
+  # expect_equal(nrow(train_ret), 3500) # Fails for now
+})
+
+test_that("calc_feature_map(multi) evaluate training and test", {
+  model_df <- flight %>%
+                exp_rpart(`ORI GIN`, `DIS TANCE`, `DEP TIME`, test_rate = 0.3)
+  # Skipping following to focus on prediction for now.
+  #ret <- rf_evaluation_training_and_test(model_df, test_rate = 0.3, pretty.name = TRUE)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "evaluation_by_class", test_rate = 0.3)
+  #ret <- rf_evaluation_training_and_test(model_df, type = "conf_mat", test_rate = 0.3)
+
+  ret <- model_df %>% prediction_training_and_test()
+  test_ret <- ret %>% filter(is_test_data==TRUE)
+  # expect_equal(nrow(test_ret), 1500) # Fails for now
+  train_ret <- ret %>% filter(is_test_data==FALSE)
+  # expect_equal(nrow(train_ret), 3500) # Fails for now
+})
+
