7cc9c5ea775dd5a5f98d92da29edfdab78565520
Hideaki Hayashi <hihayash@gmail.com>
2019-06-21 15:32:35 -0700

    Add comment

--- R/randomForest_tidiers.R
+++ R/randomForest_tidiers.R
@@ -1070,6 +1070,7 @@ rf_evaluation_by_class <- function(data, ...) {
 rf_evaluation_training_and_test <- function(data, type = "evaluation", test_rate = 0.0, pretty.name = FALSE, ...) {
   model <-  data %>% dplyr::filter(!is.null(model)) %>% `[[`(1, "model", 1)
 
+  # Get evaluation for training part. Just passing down to rf_evaluation does it, since it is done off of data embeded in the model.
   if (!is.null(model) && model$classification_type != "regression") {
     training_ret <- switch(type,
                            evaluation = rf_evaluation(data, pretty.name = pretty.name, ...),
