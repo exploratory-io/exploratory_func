a3fc58b66d174d7b6b726d54635416d4254dd930
Hideaki Hayashi <hihayash@gmail.com>
2019-06-29 22:07:22 -0700

    Adjust test expectation

--- tests/testthat/test_lm_1.R
+++ tests/testthat/test_lm_1.R
@@ -28,9 +28,9 @@ test_that("build_lm.fast (linear regression) evaluate training and test", {
 
   ret <- model_df %>% prediction(data="training_and_test")
   test_ret <- ret %>% filter(is_test_data==TRUE)
-  expect_equal(nrow(test_ret), 1464)
+  expect_equal(nrow(test_ret), 1475)
   train_ret <- ret %>% filter(is_test_data==FALSE)
-  expect_equal(nrow(train_ret), 3418)
+  expect_equal(nrow(train_ret), 3444)
   ret <- model_df %>% evaluate_lm_training_and_test(pretty.name=TRUE)
   expect_equal(nrow(ret), 2) # 2 for train and test
 })
@@ -41,9 +41,9 @@ test_that("build_lm.fast (logistic regression) evaluate training and test", {
 
   ret <- model_df %>% prediction_binary(data="training_and_test", threshold = 0.5)
   test_ret <- ret %>% filter(is_test_data==TRUE)
-  expect_equal(nrow(test_ret), 1470)
+  expect_equal(nrow(test_ret), 1480)
   train_ret <- ret %>% filter(is_test_data==FALSE)
-  expect_equal(nrow(train_ret), 3433)
+  expect_equal(nrow(train_ret), 3454)
   ret <- model_df %>% evaluate_binary_training_and_test("is delayed", pretty.name=TRUE)
   expect_equal(nrow(ret), 2) # 2 for train and test
   ret <- model_df %>% prediction_training_and_test(prediction_type = 'conf_mat', threshold = 0.5)
@@ -55,9 +55,9 @@ test_that("build_lm.fast (gaussian regression) evaluate training and test", {
 
   ret <- model_df %>% prediction(data="training_and_test")
   test_ret <- ret %>% filter(is_test_data==TRUE)
-  expect_equal(nrow(test_ret), 1464)
+  expect_equal(nrow(test_ret), 1475)
   train_ret <- ret %>% filter(is_test_data==FALSE)
-  expect_equal(nrow(train_ret), 3418)
+  expect_equal(nrow(train_ret), 3444)
   ret <- model_df %>% evaluate_lm_training_and_test(pretty.name=TRUE)
   expect_equal(nrow(ret), 2) # 2 for train and test
 })
@@ -68,9 +68,9 @@ test_that("build_lm.fast (binomial regression) evaluate training and test", {
 
   ret <- model_df %>% prediction_binary(data="training_and_test", threshold = 0.5)
   test_ret <- ret %>% filter(is_test_data==TRUE)
-  expect_equal(nrow(test_ret), 1470)
+  expect_equal(nrow(test_ret), 1480)
   train_ret <- ret %>% filter(is_test_data==FALSE)
-  expect_equal(nrow(train_ret), 3433)
+  expect_equal(nrow(train_ret), 3454)
   ret <- model_df %>% evaluate_binary_training_and_test("is delayed", pretty.name=TRUE)
   expect_equal(nrow(ret), 2) # 2 for train and test
   ret <- model_df %>% prediction_training_and_test(prediction_type = 'conf_mat', threshold = 0.5)
