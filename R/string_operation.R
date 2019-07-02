2ec9191c359ad5a90fcd297edc8eec15404ad6e1
Hidetaka Kojima <hidekoji@gmail.com>
2019-07-02 08:18:49 -0700

    updated comment.

--- R/string_operation.R
+++ R/string_operation.R
@@ -465,10 +465,12 @@ str_logical <- function(column, true_value = NULL) {
    } else if (is.numeric(column)) { # for numeric columns (integer, double, numeric, etc), use as.logical
      as.logical(column)
    } else { # default handling.
-      # if value is either "true", "yes", "1", or 1, return TRUE
+      # if value is "true" or "yes" or "1", return TRUE
       target <- stringr::str_to_lower(stringr::str_trim(column))
       ifelse (target %in% c("true", "yes", "1"),
               TRUE,
+              # if value is "false" or "no" or "0", return FALSE.
+              # All the other cases are NA
               ifelse(target %in%  c("false", "no", "0"), FALSE, NA))
    }
 }
