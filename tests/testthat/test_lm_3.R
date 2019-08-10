5a9d6b1ce52596c8f1a58acbafad2319ec0fd270
Hideaki Hayashi <hihayash@gmail.com>
2019-08-09 18:34:17 -0700

    Add test for marginal effect error

--- tests/testthat/test_lm_3.R
+++ tests/testthat/test_lm_3.R
@@ -21,11 +21,23 @@ if (!testdata_filename %in% list.files(testdata_dir)) {
   write.csv(flight, testdata_file_path) # save sampled-down data for performance.
 }
 
+test_that("build_lm.fast (logistic regression) with marginal effect with NA Date value for an entire category", {
+  # There used to be an issue where marginal effect throws error because of unused factor level. This is handled now by pre-filtering rows with NA Date values.
+  df <- flight %>% mutate(`FL DATE`=if_else(`CAR RIER`!="UA",`FL DATE`,as.Date(NA)))
+  model_df <- df %>%
+                build_lm.fast(`is delayed`, `CAR RIER`, `FL DATE`, model_type = "glm", test_rate = 0.3, variable_metric = "ame", with_marginal_effects_confint = FALSE)
+
+  # Just run prediction etc. for sanity.
+  ret <- model_df %>% prediction_binary(data="training_and_test", threshold = 0.5)
+  ret <- model_df %>% evaluate_binary_training_and_test("is delayed", pretty.name=TRUE)
+  ret <- model_df %>% prediction_training_and_test(prediction_type = 'conf_mat', threshold = 0.5)
+})
+
 test_that("build_lm.fast (linear regression) with single predictor should skip relative importance", {
   model_df <- flight %>%
                 build_lm.fast(`ARR DELAY`, `CAR RIER`, test_rate = 0.3, seed=1, relimp = TRUE, relimp_type = "first", relimp_bootstrap_type = "perc")
 
-  # Just running prediction.
+  # Just run prediction for sanity.
   ret <- model_df %>% prediction(data="training_and_test")
 
   ret <- model_df %>% evaluate_lm_training_and_test(pretty.name=TRUE)
