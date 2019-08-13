2a61c5944e3b1067f68a29a172e2e6f08565338e
Hideaki Hayashi <hihayash@gmail.com>
2019-08-12 23:20:23 -0700

    Make regressors NA fill work

--- R/prophet.R
+++ R/prophet.R
@@ -305,7 +305,7 @@ do_prophet_ <- function(df, time_col, value_col = NULL, periods = 10, time_unit 
     }
 
     # TODO: Check if this would not have daylight saving days issue we had with anomaly detection.
-    if (!is.null(na_fill_type)) {
+    if (!is.null(na_fill_type) || !is.null(regressors_na_fill_type)) {
       # complete the date time with NA
       aggregated_data <- if(inherits(aggregated_data$ds, "Date")){
         aggregated_data %>%
@@ -318,6 +318,9 @@ do_prophet_ <- function(df, time_col, value_col = NULL, periods = 10, time_unit 
       }
       # fill NAs in y with zoo
       aggregated_data <- aggregated_data %>% dplyr::mutate(y = fill_ts_na(y, ds, type = na_fill_type, val = na_fill_value))
+      for (regressor_col in regressors) {
+        aggregated_data <- aggregated_data %>% dplyr::mutate(!!sym(regressor_col) := fill_ts_na(!!sym(regressor_col), ds, type = !!regressors_na_fill_type, val = !!regressors_na_fill_value))
+      }
     }
 
 
