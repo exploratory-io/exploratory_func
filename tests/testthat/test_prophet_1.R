3e762c7898e5a2b318909c211d43add4212c73e0
Hideaki Hayashi <hihayash@gmail.com>
2019-08-12 19:17:22 -0700

    Added test for seconds

--- tests/testthat/test_prophet_1.R
+++ tests/testthat/test_prophet_1.R
@@ -215,7 +215,6 @@ test_that("do_prophet with extra regressor without target column (Number of Rows
   # verify the last date in the data
   expect_equal(ret$timestamp[[length(ret$timestamp)]], as.Date("2020-10-01"))
 })
-}
 
 test_that("do_prophet with extra regressor without target column (Number of Rows) with time unit of hour", {
   ts <- lubridate::with_tz(seq.POSIXt(as.POSIXct("2010-01-01 00:00:00"), as.POSIXct("2010-01-02 00:00:00"), by="hour"), tz="America/Los_Angeles")
@@ -233,6 +232,25 @@ test_that("do_prophet with extra regressor without target column (Number of Rows
   expect_equal(lubridate::with_tz(last((ret %>% filter(!is.na(forecasted_value)))$timestamp), tz="America/Los_Angeles"), lubridate::with_tz(as.POSIXct("2010-01-02 03:00:00"), tz="America/Los_Angeles")) 
   # verify the last date in the data
   expect_equal(lubridate::with_tz(ret$timestamp[[length(ret$timestamp)]], tz="America/Los_Angeles"), lubridate::with_tz(as.POSIXct("2010-01-02 03:00:00"), tz="America/Los_Angeles"))
+})
+}
+
+test_that("do_prophet with extra regressor without target column (Number of Rows) with time unit of second", {
+  ts <- lubridate::with_tz(seq.POSIXt(as.POSIXct("2010-01-01 00:00:00"), as.POSIXct("2010-01-01 00:00:50"), by="sec"), tz="America/Los_Angeles")
+  raw_data <- data.frame(timestamp=ts, count=round(runif(length(ts))/0.1))
+  ts2 <- lubridate::with_tz(seq.POSIXt(as.POSIXct("2010-01-01 00:00:00"), as.POSIXct("2010-01-01 00:00:53"), by="sec"), tz="America/Los_Angeles")
+  regressor_data <- data.frame(timestamp=ts2, regressor=runif(length(ts2)))
+  combined_data <- raw_data %>% full_join(regressor_data, by=c("timestamp"="timestamp"))
+  combined_data <- combined_data %>% mutate(count=if_else(is.na(count),1,count))
+  uncounted_data <- combined_data %>% tidyr::uncount(count)
+  browser()
+  ret <- uncounted_data %>%
+    do_prophet(timestamp, NULL, 3, time_unit = "second", regressors = c("regressor"), funs.aggregate.regressors = c(mean))
+  browser()
+  # verify the last date with forecasted_value
+  expect_equal(lubridate::with_tz(last((ret %>% filter(!is.na(forecasted_value)))$timestamp), tz="America/Los_Angeles"), lubridate::with_tz(as.POSIXct("2010-01-01 00:00:53"), tz="America/Los_Angeles")) 
+  # verify the last date in the data
+  expect_equal(lubridate::with_tz(ret$timestamp[[length(ret$timestamp)]], tz="America/Los_Angeles"), lubridate::with_tz(as.POSIXct("2010-01-01 00:00:53"), tz="America/Los_Angeles"))
 })
 
 if(F) {
