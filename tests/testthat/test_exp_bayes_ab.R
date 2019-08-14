23c9971f8a629beb2a3fd61a5c7ba27c94e708e0
Hideaki Hayashi <hihayash@gmail.com>
2019-08-13 23:37:14 -0700

    Add test

--- tests/testthat/test_exp_bayes_ab.R
+++ tests/testthat/test_exp_bayes_ab.R
@@ -21,6 +21,19 @@ test_that("test exp_bayes_ab test with summary output", {
     dplyr::mutate(converted_true=as.integer(access_count*click), converted_false=as.integer(access_count*(1-click))) %>%
     tidyr::gather(key="conver ted", value="count col", converted_true, converted_false) %>%
     dplyr::mutate(`conver ted` = `conver ted`=="converted_true")
+  full_data_logical_ab <- full_data %>% dplyr::mutate(`gro up` = `gro up` == "a")
+
+  # revert A/B - character case
+  ret1 <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, type = "summary")
+  ret2 <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, revert_ab = T, type = "summary")
+  expect_equal(ret1$ab_identifier, c("a", "b", "a", "b"))
+  expect_equal(ret2$ab_identifier, c("b", "a", "b", "a"))
+
+  # revert A/B - logical case
+  ret1 <- exp_bayes_ab(full_data_logical_ab, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, type = "summary")
+  ret2 <- exp_bayes_ab(full_data_logical_ab, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, revert_ab = T, type = "summary")
+  expect_equal(ret1$ab_identifier, c(TRUE,FALSE,TRUE,FALSE))
+  expect_equal(ret2$ab_identifier, c("FALSE","TRUE","FALSE","TRUE"))
 
   #full_data <- full_data %>% rename(`access count`=access_count, `cli ck`=click)
   ret <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, type = "summary")
