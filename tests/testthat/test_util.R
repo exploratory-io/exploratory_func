c6b1622e72e6f9222e3698ff3e1f24e570291701
Hideaki Hayashi <hihayash@gmail.com>
2019-08-15 17:23:28 -0700

    Rename the function name

--- tests/testthat/test_util.R
+++ tests/testthat/test_util.R
@@ -864,12 +864,12 @@ test_that("summarize_group", {
  expect_equal(nrow(df2),1)
 })
 
-test_that("clean_logical_factors", {
+test_that("revert_factor_cols_to_logical", {
   df <- data.frame(col1 = I(factor(c(TRUE, FALSE, NA))),
                    col2 = I(forcats::fct_rev(factor(c(TRUE,FALSE,NA)))),
                    col3 = I(factor(c("A","B","C"))))
                   
-  res <- clean_logical_factors(df)
+  res <- revert_factor_cols_to_logical(df)
   expect_equal(res$col1, c(TRUE, FALSE, NA))
   expect_equal(res$col2, c(TRUE, FALSE, NA))
 })
