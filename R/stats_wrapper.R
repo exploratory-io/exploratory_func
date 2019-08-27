4fed02afa4144ab77d9052a15cdd8ebcd9eddaec
Hideaki Hayashi <hihayash@gmail.com>
2019-08-26 20:21:27 -0700

    Add comments

--- R/stats_wrapper.R
+++ R/stats_wrapper.R
@@ -90,7 +90,7 @@ do_cor.kv_ <- function(df,
   }
   # column names are "{subject}.x", "{subject}.y", "value"
   output_cols <- avoid_conflict(grouped_col,
-                           c(paste0(col, c(".x", ".y")),
+                           c(paste0(col, c(".x", ".y")), # We use paste0 since str_c garbles multibyte column names here for some reason.
                              "value")
   )
 
