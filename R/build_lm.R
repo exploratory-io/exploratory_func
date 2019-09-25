db4090fb3615572282f7a9dbdc0979dc61118dc9
Hideaki Hayashi <hihayash@gmail.com>
2019-09-25 14:25:08 -0700

    Add VIF calculation function definition

--- R/build_lm.R
+++ R/build_lm.R
@@ -80,6 +80,37 @@ calc_average_marginal_effects <- function(model, data=NULL, with_confint=FALSE) 
   }
 }
 
+# VIF calculation definition from car::vif.
+# Copied to avoid having to import many dependency packages.
+vif <- function(mod, ...) {
+    if (any(is.na(coef(mod)))) 
+        stop ("there are aliased coefficients in the model")
+    v <- vcov(mod)
+    assign <- attr(model.matrix(mod), "assign")
+    if (names(coefficients(mod)[1]) == "(Intercept)") {
+        v <- v[-1, -1]
+        assign <- assign[-1]
+    }
+    else warning("No intercept: vifs may not be sensible.")
+    terms <- labels(terms(mod))
+    n.terms <- length(terms)
+    if (n.terms < 2) stop("model contains fewer than 2 terms")
+    R <- cov2cor(v)
+    detR <- det(R)
+    result <- matrix(0, n.terms, 3)
+    rownames(result) <- terms
+    colnames(result) <- c("GVIF", "Df", "GVIF^(1/(2*Df))")
+    for (term in 1:n.terms) {
+        subs <- which(assign == term)
+        result[term, 1] <- det(as.matrix(R[subs, subs])) *
+            det(as.matrix(R[-subs, -subs])) / detR
+        result[term, 2] <- length(subs)
+    }
+    if (all(result[, 2] == 1)) result <- result[, 1]
+    else result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+    result
+}
+
 
 #' lm wrapper with do
 #' @return deta frame which has lm model
@@ -678,7 +709,7 @@ build_lm.fast <- function(df,
       }
 
       tryCatch({
-        model$vif <- car::vif(model)
+        model$vif <- vif(model)
       }, error = function(e){
         # in case of perfect multicollinearity, vif throws error with message "there are aliased coefficients in the model".
         # Check if it is the case. If coef() includes NA, corresponding variable is causing perfect multicollinearity.
