392aea7b145917f50ac2143eab2cf03b957e1612
Hideaki Hayashi <hihayash@gmail.com>
2019-10-21 08:31:53 -0700

    Add comment

--- R/build_lm.R
+++ R/build_lm.R
@@ -954,6 +954,7 @@ xlevels_to_base_level_table <- function(xlevels) {
     # Quote variable name with backtick if it includes special characters or space.
     # Special characters to detect besides space. Note that period and underscore should *not* be included here. : ~!@#$%^&*()+={}|:;'<>,/?"[]-\
     # Using grepl() as opposed to str_detect() because str_detect seems to return wrong decision when vname ends with SJIS damemoji.
+    # perl=TRUE is required here, since it seems this regex does not detect space, tilde, etc. without perl=TRUE for some reason.
     paste0(if_else(grepl("[ ~!@#$%^&*()+={}|:;'<>,/?\"\\[\\]\\-\\\\]", vname, perl=TRUE), paste0('`',vname,'`'),vname),xlevels[[vname]])
   }))
   base_level <- purrr::flatten_chr(purrr::map(xlevels, function(v){rep(v[[1]],length(v))}))
