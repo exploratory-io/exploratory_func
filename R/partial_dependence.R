8ee8f025d9b1b69f53f00bf0aa7ade1af2a3aa28
Hideaki Hayashi <hihayash@gmail.com>
2019-12-16 19:35:23 -0800

    Add conf int to binned data for partial dependence

--- R/partial_dependence.R
+++ R/partial_dependence.R
@@ -14,7 +14,10 @@ calc_partial_binning_data <- function(df, target_col, var_cols) {
   ret <- data.frame()
   for (var_col in var_cols) {
     if (is.factor(df[[var_col]])) { # In case of factor, just plot means of training data for each category.
-      actual_ret <- df %>% dplyr::group_by(!!rlang::sym(var_col)) %>% dplyr::summarise(Actual=mean(!!rlang::sym(target_col), na.rm=TRUE))
+      actual_ret <- df %>% dplyr::group_by(!!rlang::sym(var_col)) %>%
+        dplyr::summarise(Actual=mean(!!rlang::sym(target_col), na.rm=TRUE), error=confint_error(!!rlang::sym(target_col))) %>%
+        dplyr::mutate(conf_low=Actual-error, conf_high=Actual+error)
+      actual_ret <- actual_ret %>% dplyr::select(-error)
       ret <- ret %>% dplyr::bind_rows(actual_ret)
     }
     else if (is.numeric(df[[var_col]])) { # Because of proprocessing we do, all columns should be either factor or numeric by now.
@@ -22,8 +25,9 @@ calc_partial_binning_data <- function(df, target_col, var_cols) {
       actual_ret <- df %>% dplyr::mutate(.temp.bin.column=cut(!!rlang::sym(var_col), breaks=20)) %>% dplyr::group_by(.temp.bin.column)
       # Equal frequency cut version:
       # actual_ret <- df %>% dplyr::mutate(.temp.bin.column=ggplot2::cut_number(!!rlang::sym(var_col), 20)) %>% dplyr::group_by(.temp.bin.column)
-      actual_ret <- actual_ret %>% dplyr::summarize(Actual=mean(!!rlang::sym(target_col), na.rm=TRUE),!!rlang::sym(var_col):=mean(!!rlang::sym(var_col), na.rm=TRUE))
-      actual_ret <- actual_ret %>% dplyr::select(-.temp.bin.column)
+      actual_ret <- actual_ret %>% dplyr::summarize(Actual=mean(!!rlang::sym(target_col), na.rm=TRUE), error=confint_error(!!rlang::sym(target_col)), !!rlang::sym(var_col):=mean(!!rlang::sym(var_col), na.rm=TRUE)) %>%
+        dplyr::mutate(conf_low=Actual-error, conf_high=Actual+error)
+      actual_ret <- actual_ret %>% dplyr::select(-.temp.bin.column, -error)
       ret <- ret %>% dplyr::bind_rows(actual_ret)
     }
   }
