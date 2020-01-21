0e9a6439216774c336b8fdbd41feda1199903ba4
Hideaki Hayashi <hihayash@gmail.com>
2020-01-20 18:47:18 -0800

    Add comment

--- R/arima.R
+++ R/arima.R
@@ -556,6 +556,7 @@ glance.ARIMA_exploratory <- function(x, pretty.name = FALSE, ...) { #TODO: add t
   # df <- data.frame(AIC=m$aic, BIC=m$bic, AICc=m$aicc, as.list(forecast::arimaorder(m)), forecast::accuracy(m))
   # TODO: migrate out from forecast package.
 
+  # Extract orders from model.
   order <- m$arma[c(1, 6, 2, 3, 7, 4, 5)]
   names(order) <- c("p", "d", "q", "P", "D", "Q", "Frequency")
   seasonal <- (order[7] > 1 & sum(order[4:6]) > 0)
