146157ba86988da09907deb5acee7727952ebcab
Hidetaka Kojima <hidekoji@gmail.com>
2020-02-18 18:52:01 -0800

    Switch underlying package for GoogleSheets to googlesheets4 from googlesheets

--- R/google_sheets.R
+++ R/google_sheets.R
@@ -4,10 +4,13 @@
 #' @param title name of the new sheet on Google Sheets.
 #' @param overwrite flag to control if you want to overwrite existing sheet
 uploadGoogleSheet <- function(filepath, title, overwrite = FALSE){
-  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  if(!requireNamespace("googlesheets4")){stop("package googlesheets4 must be installed.")}
+  if(!requireNamespace("googledrive")){stop("package googledrive must be installed.")}
+
   token <- getGoogleTokenForSheet("")
-  googlesheets::gs_auth(token)
-  sheet <- googlesheets::gs_upload(filepath, title, overwrite = overwrite)
+  googlesheets4::sheets_set_token(token)
+  googledrive::drive_set_token(token)
+  sheet <- googledrive::drive_upload(filepath, title, overwrite = overwrite)
 }
 
 #' API to get google sheet data
@@ -20,37 +23,45 @@ uploadGoogleSheet <- function(filepath, title, overwrite = FALSE){
 #' @param commentChar - treat the character as comment.
 #' @param guessDataType - flag to tell if you want googlesheets::gs_read to guess column data type
 #' @export
-getGoogleSheet <- function(title, sheetName, skipNRows = 0, treatTheseAsNA, firstRowAsHeader = TRUE, commentChar, tokenFileId=NULL, guessDataType=TRUE, ...){
-  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+getGoogleSheet <- function(title, sheetName, skipNRows = 0, treatTheseAsNA = NULL, firstRowAsHeader = TRUE, commentChar, tokenFileId=NULL, guessDataType=TRUE, ...){
+  if(!requireNamespace("googlesheets4")){stop("package googlesheets4 must be installed.")}
+  if(!requireNamespace("googledrive")){stop("package googledrive must be installed.")}
   if(!requireNamespace("stringr")){stop("package stringr must be installed.")}
 
   token <- getGoogleTokenForSheet(tokenFileId)
-  googlesheets::gs_auth(token)
-  gsheet <- googlesheets::gs_title(title)
+  googlesheets4::sheets_set_token(token)
+  googledrive::drive_set_token(token)
+  gsheet <- googledrive::drive_get(title)
   col_types <- NULL
   if(!guessDataType) {
     # if guessDataType is FALSE, use character as the default column data type.
     col_types <- c(.default="c")
   }
-  df <- gsheet %>% googlesheets::gs_read(ws = sheetName, skip = skipNRows, na = treatTheseAsNA, col_names = firstRowAsHeader, comment = commentChar, col_types = col_types)
+  df <- gsheet %>% googlesheets4::read_sheet(range = sheetName, skip = skipNRows, na = treatTheseAsNA, col_names = firstRowAsHeader, col_types = col_types)
   df
 }
 
 #' API to get a list of available google sheets
 #' @export
 getGoogleSheetList <- function(tokenFileId=""){
-  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  if(!requireNamespace("googlesheets4")){stop("package googlesheets4 must be installed.")}
+  if(!requireNamespace("googledrive")){stop("package googledrive must be installed.")}
+
   token = getGoogleTokenForSheet(tokenFileId)
-  googlesheets::gs_auth(token)
-  googlesheets::gs_ls()
+  googlesheets4::sheets_set_token(token)
+  googledrive::drive_set_token(token)
+  googlesheets4::sheets_find(order_by = "createdTime desc")
 }
 
 #' API to get a list of available google sheets
 #' @export
 getGoogleSheetWorkSheetList <- function(tokenFileId="", title){
-  if(!requireNamespace("googlesheets")){stop("package googlesheets must be installed.")}
+  if(!requireNamespace("googlesheets4")){stop("package googlesheets must be installed.")}
+  if(!requireNamespace("googledrive")){stop("package googledrive must be installed.")}
+
   token = getGoogleTokenForSheet(tokenFileId)
-  googlesheets::gs_auth(token)
-  sheet <- googlesheets::gs_title(title)
-  googlesheets::gs_ws_ls(sheet)
+  googlesheets4::sheets_set_token(token)
+  googledrive::drive_set_token(token)
+  sheet <- googledrive::drive_get(title)
+  googlesheets4::sheets_sheets(sheet)
 }
