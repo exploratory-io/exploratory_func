c161f5316a4bf1a00c7baa5e9b4e1a653115a21b
Hideaki Hayashi <hihayash@gmail.com>
2020-04-17 08:20:36 -0700

    Add test

---
+++ tests/testthat/test_survival_forest.R
@@ -0,0 +1,26 @@
+context("test exp_survival_forest")
+
+test_that("test exp_survival_forest", {
+  df <- survival::lung # this data has NAs.
+  df <- df %>% rename(`ti me`=time, `sta tus`=status, `a ge`=age, `se-x`=sex)
+  df <- df %>% mutate(ph.ecog = factor(ph.ecog, ordered=TRUE)) # test handling of ordered factor
+  df <- df %>% mutate(`se-x` = `se-x`==1) # test handling of logical
+  model_df <- df %>% exp_survival_forest(`ti me`, `sta tus`, `a ge`, `se-x`, ph.ecog, ph.karno, pat.karno, meal.cal, wt.loss, predictor_n = 2)
+  expect_equal(class(model_df$model[[1]]), c("ranger_survival_exploratory"))
+  ret <- model_df %>% broom::tidy(model)
+})
+
+test_that("build_coxpy.fast() error handling for predictor with single unique value", {
+  expect_error({
+    df <- survival::lung # this data has NAs.
+    df <- df %>% mutate(age = 50) # Test for single unique value error handling.
+    df <- df %>% rename(`ti me`=time, `sta tus`=status, `a ge`=age, `se-x`=sex)
+    df <- df %>% mutate(ph.ecog = factor(ph.ecog, ordered=TRUE)) # test handling of ordered factor
+    df <- df %>% mutate(`se-x` = `se-x`==1) # test handling of logical
+    model_df <- df %>% exp_survival_forest(`ti me`, `sta tus`, `a ge`)
+  }, "Invalid Predictors: Only one unique value.")
+})
+
+# Note: we used to have Japanese column name test, but removed since
+# it was not a simple matter to make it work on Windows where we need to use
+# SJIS. We test multibyte column names with other test suite.
