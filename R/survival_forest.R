541e8baa7ecd9e54b5c305a79a9c880b9313898d
Hideaki Hayashi <hihayash@gmail.com>
2020-04-17 08:05:21 -0700

    Add survival forest funcs

---
+++ R/survival_forest.R
@@ -0,0 +1,237 @@
+
+#' builds cox model quickly by way of sampling or fct_lumn, for analytics view.
+#' @export
+exp_survival_forest <- function(df,
+                    time,
+                    status,
+                    ...,
+                    max_nrow = 50000, # With 50000 rows, taking 6 to 7 seconds on late-2016 Macbook Pro.
+                    predictor_n = 12, # so that at least months can fit in it.
+                    seed = 1
+                    ){
+  # TODO: cleanup code only aplicable to randomForest. this func was started from copy of calc_feature_imp, and still adjusting for lm. 
+
+  # using the new way of NSE column selection evaluation
+  # ref: http://dplyr.tidyverse.org/articles/programming.html
+  # ref: https://github.com/tidyverse/tidyr/blob/3b0f946d507f53afb86ea625149bbee3a00c83f6/R/spread.R
+  time_col <- dplyr::select_var(names(df), !! rlang::enquo(time))
+  status_col <- dplyr::select_var(names(df), !! rlang::enquo(status))
+  # this evaluates select arguments like starts_with
+  selected_cols <- dplyr::select_vars(names(df), !!! rlang::quos(...))
+
+  grouped_cols <- grouped_by(df)
+
+  # remove grouped col or time/status col
+  selected_cols <- setdiff(selected_cols, c(grouped_cols, time_col, status_col))
+
+  if (any(c(time_col, status_col, selected_cols) %in% grouped_cols)) {
+    stop("grouping column is used as variable columns")
+  }
+
+  if (predictor_n < 2) {
+    stop("Max # of categories for explanatory vars must be at least 2.")
+  }
+
+  if(!is.null(seed)){
+    set.seed(seed)
+  }
+
+  # check status_col.
+  if (!is.numeric(df[[status_col]]) && !is.logical(df[[status_col]])) {
+    stop(paste0("Status column (", status_col, ")  must be logical or numeric with values of 1 (dead) or 0 (alive)."))
+  }
+  if (is.numeric(df[[status_col]])) {
+    unique_val <- unique(df[[status_col]])
+    sorted_unique_val <- sort(unique_val[!is.na(unique_val)])
+    if (!all(sorted_unique_val == c(0,1)) & !all(sorted_unique_val == c(1,2))) {
+      # we allow 1,2 too since survivial works with it, but we are not promoting it for simplicity.
+      stop(paste0("Status column (", status_col, ")  must be logical or numeric with values of 1 (dead) or 0 (alive)."))
+    }
+  }
+
+  # check time_col
+  if (!is.numeric(df[[time_col]])) {
+    stop(paste0("Time column (", time_col, ") must be numeric"))
+  }
+
+  # cols will be filtered to remove invalid columns
+  cols <- selected_cols
+
+  for (col in selected_cols) {
+    if(all(is.na(df[[col]]))){
+      # remove columns if they are all NA
+      cols <- setdiff(cols, col)
+      df[[col]] <- NULL # drop the column so that SMOTE will not see it. 
+    }
+  }
+
+  # randomForest fails if columns are not clean. TODO is this needed?
+  #clean_df <- janitor::clean_names(df)
+  clean_df <- df # turn off clean_names for lm
+  # this mapping will be used to restore column names
+  name_map <- colnames(clean_df)
+  names(name_map) <- colnames(df)
+
+  # clean_names changes column names
+  # without chaning grouping column name
+  # information in the data frame
+  # and it causes an error,
+  # so the value of grouping columns
+  # should be still the names of grouping columns
+  name_map[grouped_cols] <- grouped_cols
+  colnames(clean_df) <- name_map
+
+  clean_status_col <- name_map[status_col]
+  clean_time_col <- name_map[time_col]
+  clean_cols <- name_map[cols]
+
+  each_func <- function(df) {
+    tryCatch({
+      df <- df %>%
+        # dplyr::filter(!is.na(!!target_col))  TODO: this was not filtering, and replaced it with the next line. check other similar places.
+        # for numeric cols, filter NA rows, because lm will anyway do this internally, and errors out
+        # if the remaining rows are with single value in any predictor column.
+        # filter Inf/-Inf too to avoid error at lm.
+        dplyr::filter(!is.na(df[[time_col]]) & !is.infinite(df[[time_col]])) # this form does not handle group_by. so moved into each_func from outside.
+      df <- df %>%
+        dplyr::filter(!is.na(df[[time_col]])) # this form does not handle group_by. so moved into each_func from outside.
+
+      # sample the data for performance if data size is too large.
+      sampled_nrow <- NULL
+      if (!is.null(max_nrow) && nrow(df) > max_nrow) {
+        # Record that sampling happened.
+        sampled_nrow <- max_nrow
+        df <- df %>% sample_rows(max_nrow)
+      }
+
+      c_cols <- clean_cols
+      for(col in clean_cols){
+        if(lubridate::is.Date(df[[col]]) || lubridate::is.POSIXct(df[[col]])) {
+          c_cols <- setdiff(c_cols, col)
+
+          absolute_time_col <- avoid_conflict(colnames(df), paste0(col, "_abs_time"))
+          wday_col <- avoid_conflict(colnames(df), paste0(col, "_w_"))
+          day_col <- avoid_conflict(colnames(df), paste0(col, "_day_of_month"))
+          yday_col <- avoid_conflict(colnames(df), paste0(col, "_day_of_year"))
+          month_col <- avoid_conflict(colnames(df), paste0(col, "_m_"))
+          year_col <- avoid_conflict(colnames(df), paste0(col, "_year"))
+          new_name <- c(absolute_time_col, wday_col, day_col, yday_col, month_col, year_col)
+          names(new_name) <- paste(
+            names(name_map)[name_map == col],
+            c(
+              "_abs_time",
+              "_w_",
+              "_day_of_month",
+              "_day_of_year",
+              "_m_",
+              "_year"
+            ), sep="")
+
+          name_map <- c(name_map, new_name)
+
+          c_cols <- c(c_cols, absolute_time_col, wday_col, day_col, yday_col, month_col, year_col)
+          df[[absolute_time_col]] <- as.numeric(df[[col]])
+          # turn it into unordered factor since if it is ordered factor, the name of term is broken
+          df[[wday_col]] <- factor(lubridate::wday(df[[col]], label=TRUE), ordered=FALSE)
+          df[[day_col]] <- lubridate::day(df[[col]])
+          df[[yday_col]] <- lubridate::yday(df[[col]])
+          # turn it into unordered factor since if it is ordered factor, the name of term is broken
+          df[[month_col]] <- factor(lubridate::month(df[[col]], label=TRUE), ordered=FALSE)
+          df[[year_col]] <- lubridate::year(df[[col]])
+          if(lubridate::is.POSIXct(df[[col]])) {
+            hour_col <- avoid_conflict(colnames(df), paste0(col, "_hour"))
+            new_name <- c(hour_col)
+            names(new_name) <- paste(
+              names(name_map)[name_map == col],
+              c(
+                "_hour"
+              ), sep="")
+            name_map <- c(name_map, new_name)
+
+            c_cols <- c(c_cols, hour_col)
+            df[[hour_col]] <- factor(lubridate::hour(df[[col]])) # treat hour as category
+          }
+          df[[col]] <- NULL # drop original Date/POSIXct column to pass SMOTE later.
+        } else if(is.factor(df[[col]])) {
+          # 1. if the data is factor, respect the levels and keep first 10 levels, and make others "Others" level.
+          # 2. if the data is ordered factor, turn it into unordered. For ordered factor,
+          #    coxph takes polynomial terms (Linear, Quadratic, Cubic, and so on) and use them as variables,
+          #    which we do not want for this function.
+          if (length(levels(df[[col]])) >= predictor_n + 2) {
+            df[[col]] <- forcats::fct_other(factor(df[[col]], ordered=FALSE), keep=levels(df[[col]])[1:predictor_n])
+          }
+          else {
+            df[[col]] <- factor(df[[col]], ordered=FALSE)
+          }
+        } else if(is.logical(df[[col]])) {
+          # 1. convert data to factor if predictors are logical. (as.factor() on logical always puts FALSE as the first level, which is what we want for predictor.)
+          # 2. turn NA into (Missing) factor level so that lm will not drop all the rows.
+          df[[col]] <- forcats::fct_explicit_na(as.factor(df[[col]]))
+        } else if(!is.numeric(df[[col]])) {
+          # 1. convert data to factor if predictors are not numeric or logical.
+          # 2. sort levels by frequency so that base level is the most frequent category.
+          # 3. limit the number of levels in factor by fct_lump.
+          #    we use ties.method to handle the case where there are many unique values. (without it, they all survive fct_lump.)
+          # 4. turn NA into (Missing) factor level so that lm will not drop all the rows.
+          df[[col]] <- forcats::fct_explicit_na(forcats::fct_lump(forcats::fct_infreq(as.factor(df[[col]])), n=predictor_n, ties.method="first"))
+        } else {
+          # for numeric cols, filter NA rows, because lm will anyway do this internally, and errors out
+          # if the remaining rows are with single value in any predictor column.
+          # filter Inf/-Inf too to avoid error at lm.
+          df <- df %>% dplyr::filter(!is.na(df[[col]]) & !is.infinite(df[[col]]))
+        }
+      }
+
+      # remove columns with only one unique value
+      cols_copy <- c_cols
+      for (col in cols_copy) {
+        unique_val <- unique(df[[col]])
+        if (length(unique_val[!is.na(unique_val)]) == 1) {
+          c_cols <- setdiff(c_cols, col)
+          df[[col]] <- NULL # drop the column so that SMOTE will not see it. 
+        }
+      }
+      if (length(c_cols) == 0) {
+        # Previous version of message - stop("The selected predictor variables are invalid since they have only one unique values.")
+        stop("Invalid Predictors: Only one unique value.") # Message is made short so that it fits well in the Summary table.
+      }
+
+      # build formula for lm
+      rhs <- paste0("`", c_cols, "`", collapse = " + ")
+      # TODO: This clean_target_col is actually not a cleaned column name since we want lm to show real name. Clean up our variable name.
+      # TODO: see if the above is appropriate for coxph
+      fml <- as.formula(paste0("survival::Surv(`", clean_time_col, "`, `", clean_status_col, "`) ~ ", rhs))
+      rf <- ranger::ranger(fml, data = df, importance = 'permutation')
+      # these attributes are used in tidy of randomForest TODO: is this good for lm too?
+      rf$terms_mapping <- names(name_map)
+      names(rf$terms_mapping) <- name_map
+      rf$sampled_nrow <- sampled_nrow
+      # add special lm_coxph class for adding extra info at glance().
+      class(rf) <- c("ranger_survival_exploratory", class(rf))
+      rf
+    }, error = function(e){
+      if(length(grouped_cols) > 0) {
+        # In repeat-by case, we report group-specific error in the Summary table,
+        # so that analysis on other groups can go on.
+        class(e) <- c("ranger_survival_exploratory", class(e))
+        e
+      } else {
+        stop(e)
+      }
+    })
+  }
+
+  do_on_each_group(clean_df, each_func, name = "model", with_unnest = FALSE)
+}
+
+#' special version of tidy.coxph function to use with build_coxph.fast.
+#' @export
+tidy.ranger_survival_exploratory <- function(x, pretty.name = FALSE, ...) { #TODO: add test
+  if ("error" %in% class(x)) {
+    ret <- data.frame()
+    return(ret)
+  }
+  importance_vec <- ranger::importance(x)
+  ret <- tibble::tibble(term=names(importance_vec), importance=importance_vec)
+  ret
+}
