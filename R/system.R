a2d0d874c43ad3304b8f58f606270d76d249a236
hide kojima <hidekoji@users.noreply.github.com>
2020-05-25 15:54:50 -0700

    Update R/system.R
    Co-authored-by: Hideaki Hayashi <hihayash@gmail.com>

--- R/system.R
+++ R/system.R
@@ -286,7 +286,7 @@ sql_glue_transformer <- function(code, envir) {
   }
   else if (is.numeric(val)) {
     # Do not convert number to scientific notation.
-    val <- format(val, scientific = F)
+    val <- format(val, scientific = FALSE)
   }
   else if (is.character(val) || is.factor(val)) {
     # escape for SQL
