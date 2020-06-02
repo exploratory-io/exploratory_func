4062585eb6102e024aa9e7c64115d8157c60fe5d
Hideaki Hayashi <hihayash@gmail.com>
2020-06-01 18:47:24 -0700

    Update R/system.R

--- R/system.R
+++ R/system.R
@@ -2018,7 +2018,7 @@ read_delim_file <- function(file, delim, quote = '"',
       # When an incorrect encoding is used, "Error in make.names(x) : invalid multibyte string 1" error message is returned.
       if(Sys.info()["sysname"]=="Linux" && stringr::str_detect(stringr::str_to_lower(e$message), "invalid multibyte")) {
         if(locale$encoding == "Shift_JIS") {
-          stop("The CSV file could be CP932 instead of Shift_JIS. Select CP932 as encoding and try again.");
+          stop("The encoding of the file may be CP932 instead of Shift_JIS. Select CP932 as encoding and try again.");
         } else if (locale$encoding == "CP932") {
           stop("The CSV file could be CP932 instead of Shift_JIS. Select CP932 as encoding and try again.");
         } else {
