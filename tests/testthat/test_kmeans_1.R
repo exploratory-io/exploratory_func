af838261e3f5cc2ebcf6604a56bd386b47cee71b
Hideaki Hayashi <hihayash@gmail.com>
2020-08-10 17:11:00 -0700

    Use tidy_rowwise in test scripts

--- tests/testthat/test_kmeans_1.R
+++ tests/testthat/test_kmeans_1.R
@@ -4,12 +4,12 @@ context("test kmeans analytics view functions")
 test_that("exp_kmeans", {
   df <- mtcars %>% mutate(new_col = c(rep("A", n() - 10), rep("B", 10)))
   model_df <- exp_kmeans(df, cyl, mpg, hp, max_nrow=30)
-  model_df %>% tidy(model, type="variances")
-  model_df %>% tidy(model, type="loadings")
-  model_df %>% tidy(model, type="biplot")
-  model_df %>% tidy(model, type="data")
-  res <- model_df %>% tidy(model, type="gathered_data")
-  res <- model_df %>% tidy(model, type="gathered_data", normalize_data=TRUE, n_sample=20)
+  model_df %>% tidy_rowwise(model, type="variances")
+  model_df %>% tidy_rowwise(model, type="loadings")
+  model_df %>% tidy_rowwise(model, type="biplot")
+  model_df %>% tidy_rowwise(model, type="data")
+  res <- model_df %>% tidy_rowwise(model, type="gathered_data")
+  res <- model_df %>% tidy_rowwise(model, type="gathered_data", normalize_data=TRUE, n_sample=20)
 })
 
 test_that("exp_kmeans with strange column name", {
@@ -17,22 +17,22 @@ test_that("exp_kmeans with strange column name", {
     rename(`Cy l` = cyl) %>%
     mutate(new_col = c(rep("A", n() - 10), rep("B", 10)))
   model_df <- exp_kmeans(df, `Cy l`, mpg, hp)
-  model_df %>% tidy(model, type="variances")
-  model_df %>% tidy(model, type="loadings")
-  model_df %>% tidy(model, type="biplot")
-  model_df %>% tidy(model, type="data")
-  model_df %>% tidy(model, type="gathered_data")
-  res <- model_df %>% tidy(model, type="gathered_data", normalize_data=TRUE, n_sample=100) # testing n_sample more than nrow()
+  model_df %>% tidy_rowwise(model, type="variances")
+  model_df %>% tidy_rowwise(model, type="loadings")
+  model_df %>% tidy_rowwise(model, type="biplot")
+  model_df %>% tidy_rowwise(model, type="data")
+  model_df %>% tidy_rowwise(model, type="gathered_data")
+  res <- model_df %>% tidy_rowwise(model, type="gathered_data", normalize_data=TRUE, n_sample=100) # testing n_sample more than nrow()
 })
 
 test_that("exp_kmeans with single column name", {
   model_df <- exp_kmeans(mtcars, mpg)
-  model_df %>% tidy(model, type="variances")
-  # model_df %>% tidy(model, type="loadings") # Not used.
-  # model_df %>% tidy(model, type="biplot") # Will skip for single column case.
-  model_df %>% tidy(model, type="data")
-  model_df %>% tidy(model, type="gathered_data")
-  res <- model_df %>% tidy(model, type="gathered_data", normalize_data=TRUE, n_sample=100) # testing n_sample more than nrow()
+  model_df %>% tidy_rowwise(model, type="variances")
+  # model_df %>% tidy_rowwise(model, type="loadings") # Not used.
+  # model_df %>% tidy_rowwise(model, type="biplot") # Will skip for single column case.
+  model_df %>% tidy_rowwise(model, type="data")
+  model_df %>% tidy_rowwise(model, type="gathered_data")
+  res <- model_df %>% tidy_rowwise(model, type="gathered_data", normalize_data=TRUE, n_sample=100) # testing n_sample more than nrow()
 })
 
 test_that("exp_kmeans elbow method mode", {
