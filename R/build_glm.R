e7cf4fe65a9e703fcb3ee4e981e961b2c0523684
Hideaki Hayashi <hihayash@gmail.com>
2020-08-15 12:47:14 -0700

    Do the same for glm prediction step

--- R/build_glm.R
+++ R/build_glm.R
@@ -30,13 +30,27 @@ augment.glm_exploratory_0 <- function(x, data = NULL, newdata = NULL, ...) {
     }
   }
   if (is.null(data)) { # Giving data argument when it is NULL causes error from augment.glm.
-    ret <- broom:::augment.glm(x, newdata = newdata, se=TRUE, ...)
+    ret <- tryCatch({
+      broom:::augment.glm(x, newdata = newdata, se = TRUE, ...)
+    }, error = function(e){
+      # se=TRUE throws error that looks like "singular matrix 'a' in solve",
+      # in some subset of cases of perfect collinearity.
+      # Try recovering from it by running with se=FALSE.
+      broom:::augment.glm(x, newdata = newdata, se = FALSE, ...)
+    })
     if (!is.null(ret$.rownames)) { # Clean up .rownames column augment.glm adds for some reason.
       ret$.rownames <- NULL
     }
   }
   else {
-    ret <- broom:::augment.glm(x, data = data, newdata = newdata, se=TRUE, ...)
+    ret <- tryCatch({
+      broom:::augment.glm(x, data = data, newdata = newdata, se = TRUE, ...)
+    }, error = function(e){
+      # se=TRUE throws error that looks like "singular matrix 'a' in solve",
+      # in some subset of cases of perfect collinearity.
+      # Try recovering from it by running with se=FALSE.
+      broom:::augment.glm(x, data = data, newdata = newdata, se = FALSE, ...)
+    })
   }
   ret
 }
