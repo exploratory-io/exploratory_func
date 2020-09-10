b2cf3596ba194f3a9f4f99211c555d54f53998b5
Hideaki Hayashi <hihayash@gmail.com>
2020-09-09 19:55:02 -0700

    Update test

--- tests/testthat/test_arima_1.R
+++ tests/testthat/test_arima_1.R
@@ -3,10 +3,16 @@ test_that("do_arima with aggregation", {
   data("raw_data", package = "AnomalyDetection")
   raw_data$timestamp <- as.POSIXct(raw_data$timestamp)
   raw_data <- raw_data %>% rename(`time stamp`=timestamp, `cou nt`=count)
-  browser()
+  model_df <- raw_data %>%
+    do_arima(`time stamp`, `cou nt`, 2, time_unit = "day", seasonal=F, test_mode=T) # With seasolal=T, the data would be too short.
+  ret <- model_df %>% glance_with_ts_metric()
+  model_df %>% glance_rowwise(model)
   ret <- raw_data %>%
-    do_arima(`time stamp`, `cou nt`, 10, time_unit = "day")
-  browser()
+    do_arima(`time stamp`, `cou nt`, 10, time_unit = "day", seasonal=FALSE)
+  ret <- raw_data %>%
+    do_arima(`time stamp`, `cou nt`, 10, time_unit = "day", auto=FALSE, p=0, d=1, q=0)
+  ret <- raw_data %>%
+    do_arima(`time stamp`, `cou nt`, 10, time_unit = "day", auto=FALSE, p=0, d=1, q=0, seasonal=FALSE)
   ret <- raw_data %>%
     do_arima(`time stamp`, `cou nt`, 10, time_unit = "hour")
   ret <- raw_data %>% tail(100) %>%
@@ -16,8 +22,9 @@ test_that("do_arima with aggregation", {
 
   # test for test mode.
   raw_data$`cou nt`[[length(raw_data$`cou nt`) - 2]] <- NA # inject NA near the end to test #9211
-  ret <- raw_data %>%
-    do_arima(`time stamp`, `cou nt`, 2, time_unit = "day", test_mode=TRUE) %>% tidy_rowwise(model)
+  model_df <- raw_data %>%
+    do_arima(`time stamp`, `cou nt`, 2, time_unit = "day", test_mode=TRUE)
+  ret <- model_df %>% tidy_rowwise(model)
   # verify that the last forecasted_value is not NA to test #9211
   expect_true(!is.na(ret$forecasted_value[[length(ret$forecasted_value)]]))
 
