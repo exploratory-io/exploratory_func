68d49598085ec9cfefc15de3bf589ac7c887da76
Hideaki Hayashi <hihayash@gmail.com>
2020-10-21 19:11:21 -0700

    Handle test mode in summary - coxph

--- tests/testthat/test_build_coxph.R
+++ tests/testthat/test_build_coxph.R
@@ -30,6 +30,41 @@ test_that("test build_coxph.fast", {
   ret <- model_df %>% augment_rowwise(model)
 })
 
+test_that("test build_coxph.fast with test mode", {
+  df <- survival::lung # this data has NAs.
+  df <- df %>% mutate(status = status==2)
+  df <- df %>% rename(`ti me`=time, `sta tus`=status, `a ge`=age, `se-x`=sex)
+  df <- df %>% mutate(ph.ecog = factor(ph.ecog, ordered=TRUE)) # test handling of ordered factor
+  df <- df %>% mutate(`se-x` = `se-x`==1) # test handling of logical
+  browser()
+  model_df <- df %>% build_coxph.fast(`ti me`, `sta tus`, `a ge`, `se-x`, ph.ecog, ph.karno, pat.karno, meal.cal, wt.loss, predictor_n = 2, test_rate=0.3)
+  browser()
+  expect_equal(class(model_df$model[[1]]), c("coxph_exploratory","coxph"))
+  browser()
+  ret <- model_df %>% evaluation()
+  browser()
+  ret <- model_df %>% tidy_rowwise(model, type='permutation_importance')
+  ret <- model_df %>% tidy_rowwise(model, type='partial_dependence')
+  ret <- model_df %>% tidy_rowwise(model, type='partial_dependence_survival_curve')
+  ret <- model_df %>% tidy_rowwise(model, type='vif')
+  ret <- model_df %>% tidy_rowwise(model)
+  expect_equal(colnames(ret),
+               c("term","estimate","std_error","t_ratio",
+                 "p_value","conf_low","conf_high","hazard_ratio","base.level"))
+  # Verify that base levels are not NA for `se-x` (testing - in the name) columns.
+  ret2 <- ret %>% dplyr::filter(stringr::str_detect(term,"(se-x)")) %>% dplyr::summarize(na_count=sum(is.na(base.level)))
+  expect_equal(ret2$na_count, 0)
+
+  ret <- model_df %>% glance_rowwise(model, pretty.name=TRUE)
+  expect_equal(colnames(ret),
+               c("Number of Rows","Number of Events","Likelihood Ratio Test","Likelihood Ratio Test P Value",
+                 "Score Test","Score Test P Value","Wald Test","Wald Test P Value",
+                 # "Robust Statistic","Robust P Value", # These columns are hidden for now.
+                 "R Squared","R Squared Max","Concordance","Std Error Concordance",
+                 "Log Likelihood","AIC","BIC")) 
+  ret <- model_df %>% augment_rowwise(model)
+})
+
 test_that("test build_coxph.fast with outlier filtering", {
   df <- survival::lung # this data has NAs.
   df <- df %>% mutate(status = status==2)
