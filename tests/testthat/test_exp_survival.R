4963e20876b827965b2699e64662fc41e24133b2
Hideaki Hayashi <hihayash@gmail.com>
2020-10-27 10:56:56 -0700

    Add test for survival_rate with no cohort

--- tests/testthat/test_exp_survival.R
+++ tests/testthat/test_exp_survival.R
@@ -37,7 +37,10 @@ test_that("test exp_survival", {
   ret1 <- ret %>% tidy_rowwise(model1)
   ret2 <- ret %>% tidy_rowwise(model2)
   ret3 <- ret %>% glance_rowwise(model2)
+
+  # No cohort case
   ret <- data %>% exp_survival(`weeks on service`, `is churned`)
+  ret1 <- ret %>% tidy_rowwise(model1, type="survival_rate")
   ret1 <- ret %>% tidy_rowwise(model1)
   ret2 <- ret %>% tidy_rowwise(model2)
   ret3 <- ret %>% glance_rowwise(model2)
