d8fb269d915de1e324dea551d66668165186f576
Hideaki Hayashi <hihayash@gmail.com>
2020-11-02 02:43:09 -0800

    Reorder Data tab table columns for Cox regression and linear regression, and other numeric GLM

--- R/broom_wrapper.R
+++ R/broom_wrapper.R
@@ -655,10 +655,8 @@ prediction <- function(df, data = "training", data_frame = NULL, conf_int = 0.95
     ret[[conf_low_colname]] <- get_confint(ret[[".fitted"]], ret[[".se.fit"]], conf_int = lower)
     ret[[conf_high_colname]] <- get_confint(ret[[".fitted"]], ret[[".se.fit"]], conf_int = higher)
 
-    # move confidece interval columns next to standard error
-    ret <- move_col(ret, '.se.fit', which(colnames(ret) == ".fitted") + 1)
-    ret <- move_col(ret, conf_low_colname, which(colnames(ret) == ".se.fit") + 1)
-    ret <- move_col(ret, conf_high_colname, which(colnames(ret) == conf_low_colname) + 1)
+    # Bring conf_low and conf_high right after predicted_value, instead of .se.fit column.
+    ret <- ret %>% dplyr::relocate(any_of(c(!!conf_low_colname, !!conf_high_colname, ".se.fit")), .after=.fitted)
   }
 
   colnames(ret)[colnames(ret) == ".fitted"] <- avoid_conflict(colnames(ret), "predicted_value")
