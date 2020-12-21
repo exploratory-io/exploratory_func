22d729637a071f0c96586749162b26f8f59c6d07
Hideaki Hayashi <hihayash@gmail.com>
2020-12-20 19:59:09 -0800

    Floor date

--- R/ts_cluster.R
+++ R/ts_cluster.R
@@ -1,6 +1,33 @@
-exp_ts_cluster <- function(df, ..., centers = 3L) {
+exp_ts_cluster <- function(df, time, value, category, time_unit = "day", centers = 3L) {
+  time_col <- tidyselect::vars_select(names(df), !! rlang::enquo(time))
+  value_col <- tidyselect::vars_select(names(df), !! rlang::enquo(value))
+  category_col <- tidyselect::vars_select(names(df), !! rlang::enquo(category))
+
+  # Copied from do_prophet.
+  if (time_unit == "min") {
+    time_unit <- "minute"
+  }
+  else if (time_unit == "sec") {
+    time_unit <- "second"
+  }
+
   model_df <- df %>% nest_by() %>% ungroup() %>%
     mutate(model = purrr::map(data, function(df) {
+      # Floor date. The code is copied form do_prophet.
+      df[[time_col]] <- if (time_unit %in% c("day", "week", "month", "quarter", "year")) {
+        # Take care of issue that happened in anomaly detection here for prophet too.
+        # In this case, convert (possibly) from POSIXct to Date first.
+        # If we did this without converting POSIXct to Date, floor_date works, but later at complete stage,
+        # data on day-light-saving days would be skipped, since the times seq.POSIXt gives and floor_date does not match.
+        # We give the time column's timezone to as.Date, so that the POSIXct to Date conversion is done
+        # based on that timezone.
+        lubridate::floor_date(as.Date(df[[time_col]], tz = lubridate::tz(df[[time_col]])), unit = time_unit)
+      } else {
+        lubridate::floor_date(df[[time_col]], unit = time_unit)
+      }
+      # summarize
+      # fill NAs
+      # pivot wider
       dtwclust::tsclust(t(as.matrix(df)), k = centers, distance = "sdtw", centroid = "sdtw_cent")
     }))
   model_df <- model_df %>% rowwise()
