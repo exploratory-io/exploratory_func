17eb2a18e4043cc7e7d1df8c81b551c3ff39530a
hide kojima <hidekoji@gmail.com>
2021-01-20 10:26:40 -0800

    Updated comment.

--- R/string_operation.R
+++ R/string_operation.R
@@ -165,7 +165,7 @@ word_to_sentiment <- function(words, lexicon="bing"){
 #' @param stopwords_lang Language for the stopwords that need to be excluded from the result.
 #' @param hiragana_word_length_to_remove Length of a Hiragana word that needs to be excluded from the result.
 #' @param summary_level Either "row" or "all". If this is "all", it ignores document and summarizes the result by token.
-#' @param sort_by Either "count" or "name" or "doc"
+#' @param sort_by Supported options are "count", "name", and "doc"
 #' @param ngrams - by default it's 1. Pass 2 for bigram, 3 for trigram.
 #' @return Data frame with tokenized column.
 #' @export
