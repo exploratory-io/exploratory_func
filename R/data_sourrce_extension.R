0f78945165369933e9c2e3e7eec195a5038e342e
hide kojima <hidekoji@gmail.com>
2021-02-22 12:30:48 -0800

    1. Updated get_riem_measures to the latest version. 2.  Added riem_stations_exp

--- R/data_sourrce_extension.R
+++ R/data_sourrce_extension.R
@@ -1,12 +1,55 @@
 #' Wrapper API for riem_measure data soruces
 #' @export
-get_riem_measures <- function(station = "SFO", date_start = "2020-01-01", date_end = NULL){
+get_riem_measures <- function(station = "SFO", date_start = "2020-01-01", date_end = NULL, full_columns = "Yes"){
   loadNamespace("riem")
-
   if(is.null(date_end)){
     date_end <- as.character(Sys.Date())
   }
-  riem::riem_measures(station = station, date_start = date_start, date_end = date_end)
+  df <- riem::riem_measures(station = station, date_start = date_start, date_end = date_end)
+  if(full_columns == "Yes") {
+    df
+  } else {
+    df %>% mutate(tmpc = (tmpf - 32) * 5/9, dwpc = (dwpf - 32) * 5/9) %>%
+      select(station, valid, tmpf, tmpc, dwpf, dwpc, relh, drct, sknt, p01i, alti, mslp, vsby, gust, lon, lat) %>%
+      rename(
+        Station = station,
+        Time = valid,
+        Temperature_F = tmpf,
+        Temperature_C = tmpc,
+        Dew_Point_Temp_F = dwpf,
+        Dew_Point_Temp_C = dwpc,
+        Humidity = relh,
+        Wind_Direction = drct,
+        Wind_Speed_Knot = sknt,
+        Precipitation_Inch = p01i,
+        Pressure_Altimeter_Inch = alti,
+        Sea_Level_Pressure_Millibar = mslp,
+        Visibility_Mile = vsby,
+        Wind_Gust_Knot = gust,
+        Longitude = lon,
+        Latitude = lat
+      )
+  }
+}
+
+#' Wrapper API for riem_measure data soruces
+#' @export
+riem_stations_exp <- function(network = NULL) {
+  df <- riem::riem_stations(network)
+  df <- df %>% dplyr::mutate(name = dplyr::case_when(
+    id == "RJOA" ~ "Hiroshima-shi",
+    id == "RJOO" ~ "Osaka (Itami)",
+    id == "RJOY" ~ "Osaka (Yao)",
+    id == "RJBB" ~ "Kansai International",
+    id == "RJCO" ~ "Sapporo (Okadama)",
+    id == "RJCC" ~ "Sapporo (New Chitose)",
+    id == "RJAA" ~ "Tokyo (Narita)",
+    id == "RJTT" ~ "Tokyo (Haneda)",
+    id == "RJTF" ~ "Tokyo (Chofu)",
+    TRUE ~ name
+  )
+  )
+  df
 }
 
 #' Wrapper API for tidyquant data source
