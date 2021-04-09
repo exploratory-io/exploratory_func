aaa408685c71172b45fb70e9465a4650b395a5d7
Hideaki Hayashi <hihayash@gmail.com>
2021-04-08 18:57:23 -0700

    Avoid error from categorical variable with unused factor level

--- R/partial_dependence.R
+++ R/partial_dependence.R
@@ -76,6 +76,7 @@ calc_firm_from_pd <- function(..., weight, class) {
 
 # Returns tabulation of vector v with entries for all the values that appear in v_all.
 # Tolerates NULL for v.
+# If v_all is not numeric, empty table object is returned. This is to avoid error while calculating FIRM for categorical variable
 # e.g.
 # v:     c(2,3,3,4)
 # v_all: c(1,2,3,4,5)
@@ -83,7 +84,12 @@ calc_firm_from_pd <- function(..., weight, class) {
 # 1 2 3 4 5 
 # 0 1 2 1 0 
 get_aligned_table <- function(v, v_all) {
-  table(c(v_all, v)) - table(v_all)
+  if (is.numeric(v_all)) {
+    table(c(v_all, v)) - table(v_all)
+  }
+  else {
+    table(as.numeric(c()))
+  }
 }
 
 # Caluculate FIRM variable importance.
