f28885a09a64a340680fa07578576233fdac336a
hide kojima <hidekoji@gmail.com>
2021-06-02 08:43:30 -0700

    Added more comment.

--- R/salesforce.R
+++ R/salesforce.R
@@ -78,6 +78,11 @@ querySalesforceDataWithQuery <- function(server = NULL, username, password, secu
 #' @param securityToken - (optional) security token to login
 #' @param table - The table you want to get data
 #' @param columns - list of columns that you want to get data from the table.
+#' @param dataType - list of column data types for the above columns parameter
+#' @param guessType - flag to tell that dataType should be detected. If FALSE is passed, all the data types become character.
+#' @param conditions - (optional) SOQL where conditions.
+#' @param limit - number of rows to return.
+#' @param logicalOperator (optional) - required if the conditions are set. By default it uses "AND" logical operator to construction SOQL where clause.
 querySalesforceDataFromTable <- function(server = NULL, username, password, securityToken = NULL, table = NULL, columns = NULL, dataTypes = NULL, guessType = TRUE, conditions = NULL, limit = NULL, logicalOperator = "AND"){
   if (!requireNamespace("salesforcer")) {
     stop("package salesforcer must be installed.")
@@ -99,8 +104,8 @@ querySalesforceDataFromTable <- function(server = NULL, username, password, secu
       # make sure to resolve parameter
       condition <- glue_exploratory(conditions[i], .transformer=salesforce_glue_transformer, .envir = parent.frame())
       # When the IN (NULL) condition is detected after resolving parameter, remove the condition to support select "ALL" option.
-      # NOTE: IN (@{PARA}) became IN (NULL) when nothing is selected from UI and Salesforce SOQL does not allow using IS_NULL function,
-      # remove the "Column IN (NULL)" condition.
+      # NOTE: IN (@{PARAM}) became IN (NULL) when nothing is selected from UI and Salesforce SOQL does not allow using IS_NULL function,
+      # so remove the "Column IN (NULL)" condition.
       if (hasParameter && stringr::str_detect(condition, "IN \\(NULL\\)")) {
         # do not append the condition.
       } else {
@@ -121,8 +126,10 @@ querySalesforceDataFromTable <- function(server = NULL, username, password, secu
   }
   df <- querySalesforceDataWithQuery(server = server, username = username, password = password, securityToken = securityToken, query = query)
   resultNames <- colnames(df)
+  # Check if there are any columns dropped from the query result.
   coldiff <- setdiff(columns, resultNames)
   colLength <- length(coldiff)
+  # Bring back the dropped column by adding it with all NA.
   for(i in 1:colLength) {
     col <- coldiff[i]
     dataType <- dataTypes[which(columns == col)]
@@ -138,5 +145,6 @@ querySalesforceDataFromTable <- function(server = NULL, username, password, secu
       df[col] <- NA
     }
   }
+  # Make sure resulting column order is as same as column order selected in UI.
   df %>% select(columns)
 }
