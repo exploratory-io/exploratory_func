65dc0d21b98b314e5595ea4071c4b59608e08dd4
Hideaki Hayashi <hihayash@gmail.com>
2021-06-07 10:26:09 -0700

    Update R/textanal.R
    Co-authored-by: Kei Saito <kei.saito@gmail.com>

--- R/textanal.R
+++ R/textanal.R
@@ -293,7 +293,7 @@ get_cooccurrence_graph_data <- function(model_df, max_vertex_size = 25, vertex_s
   else { # "equal_freq"
     vertex_sizes <- floor((dplyr::min_rank(feat_counts)-1)/(length(feat_counts)/5)) + 1
   }
-  vertex_sizes = vertex_sizes/max(vertex_sizes) * max_vertex_size
+  vertex_sizes <- vertex_sizes/max(vertex_sizes) * max_vertex_size
   vertices <- tibble::tibble(name=feat_names, size=vertex_sizes)
 
   ret <- list(edges=edges, vertices=vertices)
