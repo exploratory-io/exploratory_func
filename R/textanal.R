bf0dbf9257d501b3a6cb6de62469594ece8f26db
Hideaki Hayashi <hihayash@gmail.com>
2021-07-14 14:49:52 -0700

    Use tokenize_tweets instead of tokenize_words to better handle urls and social tags.

--- R/textanal.R
+++ R/textanal.R
@@ -61,7 +61,7 @@ tokenize_with_postprocess <- function(text,
                                       hiragana_word_length_to_remove = 2,
                                       compound_tokens = NULL
                                       ) {
-  tokenized <- tokenizers::tokenize_words(text, lowercase = TRUE, stopwords = NULL, strip_punct = remove_punct, simplify = FALSE)
+  tokenized <- tokenizers::tokenize_tweets(text, lowercase = TRUE, stopwords = NULL, strip_punct = remove_punct, simplify = FALSE)
   names(tokenized) <- paste0("text", 1:length(tokenized)) # Add unique names to the list so that it can be passed to quanteda::tokens().
   tokens <- quanteda::tokens(tokenized)
   # tokens <- tokens %>% quanteda::tokens_wordstem() # TODO: Revive stemming and expose as an option.
@@ -80,7 +80,7 @@ tokenize_with_postprocess <- function(text,
     # Handle ones that are not separated by spaces.
     if (length(compound_tokens_without_space) > 0) {
       # Tokenize those words with the same options with the original tokinizing, to know where such word would have been splitted.
-      compound_tokens_list <- tokenizers::tokenize_words(compound_tokens_without_space, lowercase = TRUE, stopwords = NULL, strip_punct = remove_punct, simplify = FALSE)
+      compound_tokens_list <- tokenizers::tokenize_tweets(compound_tokens_without_space, lowercase = TRUE, stopwords = NULL, strip_punct = remove_punct, simplify = FALSE)
       # Create space-separated expression of the word, which can be used with quanteda::tokens_compound.
       compound_tokens_with_space_inserted <- purrr::flatten_chr(purrr::map(compound_tokens_list, function(x){stringr::str_c(x, collapse=' ')}))
       tokens <- tokens %>% quanteda::tokens_compound(pattern = quanteda::phrase(compound_tokens_with_space_inserted), concatenator = '')
