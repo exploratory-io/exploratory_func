4331e625683df831ab2259b58283be2aabb5d442
hide kojima <hidekoji@gmail.com>
2021-08-19 14:41:34 -0700

    Updated date_start argument description.

--- R/system.R
+++ R/system.R
@@ -2968,8 +2968,7 @@ filter_cascade <- function(.data, ...){
 
 #'API to load economic data from FRED (Federal Reserve Bank Economic Data)
 #'@param series_id - e.g. UNRATE
-#'@param date_start - Start Date for the query. This is required field.
-#'                    When it's called from Exploratory Desktop, this date_start is always passed since it has required input field validation.
+#'@param date_start - Start Date for the query. This is optional field.
 #'@param date_end - End Date for the query. By default it's today.
 #'@export
 load_fred <- function(series_id, date_start = "", date_end = "", password) {
