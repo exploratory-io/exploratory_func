e56ee681d9815c1057918e8b12d448da9c8064a5
hide kojima <hidekoji@gmail.com>
2021-09-29 08:55:46 -0700

    readParquetFromAzure support.

--- R/azure.R
+++ R/azure.R
@@ -110,7 +110,7 @@ getCSVFileFromAzure <- function(fileName, host, securityToken, container, delim,
                                progress = progress)
 }
 
-getParquetFileFromAzure <- function(host = "", securityToken = "", container = "", fileName = "") {
+getParquetFileFromAzure <- function(host = "", securityToken = "", container = "", fileName = "", col_select = NULL) {
   tryCatch({
     filePath <- donwloadDataFileFromAzure(host = host, securityToken = securityToken, container = container, fileName = fileName)
   }, error = function(e) {
@@ -124,80 +124,18 @@ getParquetFileFromAzure <- function(host = "", securityToken = "", container = "
       stop(e)
     }
   })
-  read_parquet(filePath)
+  exploratory::read_parquet_file(filePath, col_select = col_select)
 }
 
-tam_read_parquet.workaround_applied <- FALSE # To make sure we only apply the workaround explained below once.
-
-read_parquet <- function(file) {
-  loadNamespace("arrow")
-  is.win <- Sys.info()['sysname'] == 'Windows'
-
-  # Backup the locale info and set English locale for reading parquet issue
-  # on Windows https://issues.apache.org/jira/browse/ARROW-7288
-  # Also, we do it just once in an R session, since it seems to be enough,
-  # and reqd_parquet under English locale has problem in reading from a path with multibyte chars,
-  # which is unavoidable when repository is on Google Drive on Japanese Windows, even through a symbolic link.
-  if (is.win && !tam_read_parquet.workaround_applied) {
-    # Backup the current locale info.
-    lc.all<- Sys.getlocale("LC_ALL")
-    lc.collate<- Sys.getlocale("LC_COLLATE")
-    lc.ctype<- Sys.getlocale("LC_CTYPE")
-    lc.monetary<- Sys.getlocale("LC_MONETARY")
-    lc.numeric<- Sys.getlocale("LC_NUMERIC")
-    lc.time <- Sys.getlocale("LC_TIME")
-    # Set English.
-    Sys.setlocale("LC_ALL", "English")
-
-    # Catch errors to guarantee restoring the locale info later.
-    tryCatch({ # This tryCatch is just for restoring locale. Note that we do not catch errors here.
-      tf0 <- tempfile()
-      on.exit(unlink(tf0))
-      df <- tibble::tibble(x=1)
-      arrow::write_parquet(df, tf0, compression="uncompressed");
-      arrow::read_parquet(tf0)
-      tam_read_parquet.workaround_applied <<- TRUE
-    }, finally = {
-      # Restore the original locale info.
-      Sys.setlocale("LC_ALL", lc.all)
-      Sys.setlocale("LC_COLLATE", lc.collate)
-      Sys.setlocale("LC_CTYPE", lc.ctype)
-      Sys.setlocale("LC_MONETARY", lc.monetary)
-      Sys.setlocale("LC_NUMERIC", lc.numeric)
-      Sys.setlocale("LC_TIME", lc.time);
-    })
-  }
-
-  tf <- NULL
-  res <- NULL
-  if (stringr::str_detect(file, "^https://") ||
-      stringr::str_detect(file, "^http://") ||
-      stringr::str_detect(file, "^ftp://")) {
-
-    # Download the remote parquet file to the local temp file.
-    tf <- tempfile()
-    # Remove on exit.
-    on.exit(unlink(tf))
-    # mode="wb" for binary download
-    utils::download.file(file, tf, mode = "wb")
-    # Read the local parquet file.
-    res <- read_parquet_internal(tf)
-
-  } else {
-    res <- read_parquet_internal(file)
-  }
-  res
+getParquetFilesFromAzure <- function(host = "", securityToken = "", container = "", files = "", col_select = NULL) {
+  files <- setNames(as.list(files), files)
+  df <- purrr::map_dfr(files, exploratory::getParquetFileFromAzure, col_select = col_select, .id = "exp.file.id") %>% mutate(exp.file.id = basename(exp.file.id))  # extract file name from full path with basename and create file.id column.
+  id_col <- avoid_conflict(colnames(df), "id")
+  # copy internal exp.file.id to the id column.
+  df[[id_col]] <- df[["exp.file.id"]]
+  # drop internal column and move the id column to the very beginning.
+  df %>% dplyr::select(!!rlang::sym(id_col), dplyr::everything(), -exp.file.id)
 }
 
 
-# Wrapper around arrow::read_parquet to work around #22609 by applying group_by column stored in the parquet file
-# as one of the class names by the above _tam_write_parquet_internal.
-read_parquet_internal <- function(filepath) {
-  res <- arrow::read_parquet(filepath)
-  if (stringr::str_starts(class(res)[1], '...grouped_by_')) {
-    group_col <- stringr::str_remove(class(res)[1], '^\\.\\.\\.grouped_by_')
-    res <- res %>% dplyr::group_by(!!rlang::sym(group_col)) # applying group_by seems to remove the ...grouped_by_... class.
-  }
-  res
-}
 
