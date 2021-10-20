020814f2abcdbfe96b1c979c3783e7a7edef3c4c
Hideaki Hayashi <hihayash@gmail.com>
2021-10-19 19:16:27 -0700

    Add min_edge_width

--- R/textanal.R
+++ R/textanal.R
@@ -338,13 +338,13 @@ tidy.textanal_exploratory <- function(x, type="word_count", max_words=NULL, max_
 }
 
 # vertex_size_method - "equal_length" or "equal_freq"
-get_cooccurrence_graph_data <- function(model_df, max_vertex_size = 20, vertex_size_method = "equal_length", max_edge_width=8, font_size_ratio=1.0, area_factor=50, vertex_opacity=0.6, cluster_method="louvain") {
+get_cooccurrence_graph_data <- function(model_df, max_vertex_size = 20, vertex_size_method = "equal_length", min_edge_width=1, max_edge_width=8, font_size_ratio=1.0, area_factor=50, vertex_opacity=0.6, cluster_method="louvain") {
   # Prepare edges data
   edges <- exploratory:::fcm_to_df(model_df$model[[1]]$fcm_selected) %>% dplyr::rename(from=token.x,to=token.y) %>% filter(from!=to)
   edges <- edges %>% dplyr::mutate(from = stringr::str_to_title(from), to = stringr::str_to_title(to))
 
   edges <- edges %>% dplyr::mutate(width=log(value+1)) # +1 to avoid 0 width.
-  edges <- edges %>% dplyr::mutate(width=max_edge_width*width/max(width))
+  edges <- edges %>% dplyr::mutate(width=(max_edge_width - min_edge_width)*(width - min(width))/(max(width) - min(width)) + min_edge_width)
 
   # Set edge colors based on number of co-occurrence.
   c_scale <- grDevices::colorRamp(c("white","#4A90E2"))
