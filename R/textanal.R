2512bf263043c98dba01ae73bf5de20d64911a4d
Hideaki Hayashi <hihayash@gmail.com>
2021-10-19 19:21:36 -0700

    Add min_vertex_size

--- R/textanal.R
+++ R/textanal.R
@@ -338,7 +338,8 @@ tidy.textanal_exploratory <- function(x, type="word_count", max_words=NULL, max_
 }
 
 # vertex_size_method - "equal_length" or "equal_freq"
-get_cooccurrence_graph_data <- function(model_df, max_vertex_size = 20, vertex_size_method = "equal_length", min_edge_width=1, max_edge_width=8, font_size_ratio=1.0, area_factor=50, vertex_opacity=0.6, cluster_method="louvain") {
+get_cooccurrence_graph_data <- function(model_df, min_vertex_size = 4, max_vertex_size = 20, vertex_size_method = "equal_length",
+                                        min_edge_width=1, max_edge_width=8, font_size_ratio=1.0, area_factor=50, vertex_opacity=0.6, cluster_method="louvain") {
   # Prepare edges data
   edges <- exploratory:::fcm_to_df(model_df$model[[1]]$fcm_selected) %>% dplyr::rename(from=token.x,to=token.y) %>% filter(from!=to)
   edges <- edges %>% dplyr::mutate(from = stringr::str_to_title(from), to = stringr::str_to_title(to))
@@ -364,7 +365,7 @@ get_cooccurrence_graph_data <- function(model_df, max_vertex_size = 20, vertex_s
   else { # "equal_freq"
     vertex_sizes <- floor((dplyr::min_rank(feat_counts)-1)/(length(feat_counts)/5)) + 1
   }
-  vertex_sizes <- vertex_sizes/max(vertex_sizes) * max_vertex_size
+  vertex_sizes <- (vertex_sizes - min(vertex_sizes)/(max(vertex_sizes) - min(vertex_sizes)) * (max_vertex_size - min_vertex_size) + min_vertex_size
   vertices <- tibble::tibble(name=feat_names, size=vertex_sizes)
 
   if (cluster_method != "none") {
