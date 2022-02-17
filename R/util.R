b57281cd21a9d4c35588c070d632f01af7bbc749
Hideaki Hayashi <hihayash@gmail.com>
2022-02-17 10:22:05 -0800

    Add explanatory comment

--- R/util.R
+++ R/util.R
@@ -308,6 +308,7 @@ mat_to_df <- function(mat, cnames=NULL, na.rm=TRUE, zero.rm = TRUE, diag=TRUE) {
     tibble::rownames_to_column(".Var2.temp") %>% # The column name .Var2.temp is to avoid name conflict as much as possible.
     tidyr::pivot_longer(-.Var2.temp, "Var1","value", values_drop_na = na.rm) %>%
     dplyr::rename(Var2 = .Var2.temp)
+  # Remove V from names like V1, V2 to align the output to the previous implementation that used reshape2::melt.
   df <- df %>% dplyr::mutate(Var1 = str_replace(Var1, "^V", ""))
 
   if(zero.rm){
