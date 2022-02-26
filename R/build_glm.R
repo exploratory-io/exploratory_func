31ec9ad82e31a58a93ddb7e5c936a6ed14c92c1c
Hideaki Hayashi <hihayash@gmail.com>
2022-02-26 12:17:43 -0800

    With dplyr 1.0.8, now it seems that the message is separated into e$message and e$parant$message. Handle it

--- R/build_glm.R
+++ R/build_glm.R
@@ -201,13 +201,13 @@ build_glm <- function(data, formula, ..., keep.source = TRUE, augment = FALSE, g
     ret <- dplyr::rowwise(ret)
     ret
   }, error = function(e){
-    # error message was changed when upgrading dplyr to 0.7.1
-    # so use stringr::str_detect to make this robust
-    if(stringr::str_detect(e$message, "contrasts can be applied only to factors with 2 or more levels")){
+    # Error message was changed when upgrading dplyr to 0.7.1
+    # so use stringr::str_detect to make this robust.
+    # With dplyr 1.0.8, now it seems that the message is separated into e$message and e$parant$message.
+    if(stringr::str_detect(e$parent$message, "contrasts can be applied only to factors with 2 or more levels")){
       stop("more than 1 unique values are expected for categorical columns assigned as predictors")
     }
-
-    stop(e$message)
+    stop(stringr::str_c(e$message, " ", e$parent$message))
   })
   if(augment){
     if(test_rate == 0){
