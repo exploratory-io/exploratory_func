d381ec39fbae0deac56a602f62614cd780679173
hide kojima <hidekoji@gmail.com>
2022-03-29 16:46:16 -0700

    Added test case for case_when mixed data types.

--- tests/testthat/test_system.R
+++ tests/testthat/test_system.R
@@ -448,6 +448,15 @@ test_that("read_delim_file open local file failed error message", {
   })
 })
 
+test_that("case_when mixed data types error message", {
+  tryCatch({
+    Global_Sales_1_source1 <- exploratory::read_excel_file("https://www.dropbox.com/s/t9ou9hmbqdxj75f/Global_Sales.xlsx?dl=1")
+    Global_Sales_2 <- Global_Sales_1_source1 %>% dplyr::mutate(calculation_1 = case_when(Segment == "Consumer" ~ 1 , TRUE ~ Segment))
+  }, error = function(cond) {
+    expect_equal(stringr::str_detect(cond$message, "must be a double vector, not a character vector."),TRUE)
+  })
+})
+
 test_that("read_excel_file downlod failed error message", {
   tryCatch({
     df <- exploratory::read_excel_file("https://dl.dropbox.com/s/sjkgk9gj0vemq36/sample.xlsx")
