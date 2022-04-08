6020e7561d1e40fe6c74697571673ea8c292d846
Kei Saito <kei.saito@gmail.com>
2022-04-07 22:40:58 -0700

    Add test.

--- tests/testthat/test_system.R
+++ tests/testthat/test_system.R
@@ -365,6 +365,14 @@ test_that("geocode_japan_city", {
   expect_equal(FALSE, any(is.na(res$latitude)))
 })
 
+test_that("is.pacific.ocean.centered argument in geocode_world_country.", {
+  df <- tibble(country.code=c("US"))
+  res <- exploratory::geocode_world_country(df "country.code", is.pacific.ocean.centered=TRUE)
+  expect_equal(264.287109, res$longitude[[1]])
+  res <- exploratory::geocode_world_country(df "country.code", is.pacific.ocean.centered=FALSE)
+  expect_equal(-95.712891, res$longitude[[1]])
+})
+
 test_that("read_parquet_file", {
   df <- read_parquet_file("https://dl.dropbox.com/s/sjkgk9gj0vemq36/sample.parquet")
   expect_equal(TRUE, is.data.frame(df))
