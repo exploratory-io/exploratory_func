ac523c0ca2b9c60a0e827ab9efac7bccac8f295c
Hideaki Hayashi <hihayash@gmail.com>
2022-06-25 12:02:18 -0700

    Fix errors from add_prediction

--- R/broom_wrapper.R
+++ R/broom_wrapper.R
@@ -200,7 +200,7 @@ augment_kmeans <- function(df, model, data){
 #' @param ... Additional argument to be passed to broom::augment
 #' @export
 add_prediction <- function(df, model_df, conf_int = 0.95, ...){
-  if (class(model_df$model[[1]]) %in% c("coxph_exploratory", "ranger_survival_exploratory")) { # For now this is only for Cox regression Analytics View model.
+  if (any(class(model_df$model[[1]]) %in% c("coxph_exploratory", "ranger_survival_exploratory"))) { # For now this is only for Cox regression Analytics View model.
     return(add_prediction2(df, model_df, conf_int = conf_int, ...))
   }
 
