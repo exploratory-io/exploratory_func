8decdc95e958aec489361da71cb5dde2e9b141cb
Hideaki Hayashi <hihayash@gmail.com>
2022-11-18 10:20:04 -0800

    Filter NAs from document id column

--- R/textanal.R
+++ R/textanal.R
@@ -618,6 +618,7 @@ exp_topic_model <- function(df, text = NULL,
     }
     else { # Assuming word and document_id is set as the input instead of text column.
       df <- df %>% dplyr::filter(!is.na(!!rlang::sym(word_col)))
+      df <- df %>% dplyr::filter(!is.na(!!rlang::sym(document_id_col)))
       df <- df %>% dplyr::group_by(!!rlang::sym(document_id_col))
       if (is.null(category_col)) {
         df <- df %>% dplyr::summarize(tokens=list(!!rlang::sym(word_col)))
