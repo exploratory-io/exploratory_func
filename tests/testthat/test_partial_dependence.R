9cb1fd1144f71af53a531b4736d01ca416a45097
Hideaki Hayashi <hihayash@gmail.com>
2023-01-24 15:41:26 -0800

    Add survival model cases

--- tests/testthat/test_partial_dependence.R
+++ tests/testthat/test_partial_dependence.R
@@ -8,6 +8,15 @@ aq_data <- airquality %>%
 aq_data <- aq_data %>% mutate(Ozone_char = as.character(Ozone_category))
 aq_data <- aq_data %>% mutate(Temp_Over_80 = Temp > 80)
 
+cancer_data <- survival::cancer %>%
+  readr::type_convert() %>%
+  exploratory::clean_data_frame() %>%
+  mutate(Start_Date = as.Date("2020-01-01")) %>%
+  mutate(End_Date = Start_Date + lubridate::days(time)) %>%
+  mutate(status = str_logical(status-1)) %>%
+  mutate(sex_category = factor(case_when(sex == 1 ~ "Male" , TRUE ~ "Female"), levels=c("Female","Male"))) %>%
+  mutate(sex_char = as.character(sex_category))
+
 test_that("Test partial dependence by character predictor with random forest", {
   model_df <- aq_data %>% calc_feature_imp(`Temp`, `Ozone_char`, target_fun = "none", predictor_funs = list(`Ozone_char`="none"), smote = FALSE, importance_measure = "permutation", pd_with_bin_means = TRUE, test_split_type = "random", test_rate = 0.1)
   res <- model_df %>% rf_partial_dependence()
@@ -71,3 +80,32 @@ test_that("Test partial dependence by factor predictor with rpart", {
   expect_true(predicted_df$x_value[1] == "1 - 18")
   expect_true(predicted_df$x_value[length(predicted_df$x_value)] == "(Missing)")
 })
+
+test_that("Test partial dependence by character predictor with Cox regression", {
+  model_df <- cancer_data %>% build_coxph.fast(NULL, `status`, `age`, `sex_char`, predictor_funs = list(`age`="none", `sex`="none"), start_time = `Start_Date`, end_time = `End_Date`, time_unit = "day", test_split_type = "random")
+  res <- model_df %>% tidy_rowwise(model, type='partial_dependence')
+  predicted_df <- res %>% filter(type=="Prediction")
+  expect_true(predicted_df$value[1] == "Male")
+})
+
+test_that("Test partial dependence by factor predictor with Cox regression", {
+  model_df <- cancer_data %>% build_coxph.fast(NULL, `status`, `age`, `sex_category`, predictor_funs = list(`age`="none", `sex`="none"), start_time = `Start_Date`, end_time = `End_Date`, time_unit = "day", test_split_type = "random")
+  res <- model_df %>% tidy_rowwise(model, type='partial_dependence')
+  predicted_df <- res %>% filter(type=="Prediction")
+  expect_true(predicted_df$value[1] == "Female")
+})
+
+test_that("Test partial dependence by character predictor with Cox regression", {
+  model_df <- cancer_data %>% exp_survival_forest(NULL, `status`, `age`, `sex_char`, predictor_funs = list(`age`="none", `sex`="none"), start_time = `Start_Date`, end_time = `End_Date`, time_unit = "day", test_split_type = "random")
+  res <- model_df %>% tidy_rowwise(model, type='partial_dependence')
+  predicted_df <- res %>% filter(type=="Prediction")
+  expect_true(predicted_df$value[1] == "Male")
+})
+
+test_that("Test partial dependence by factor predictor with Cox regression", {
+  model_df <- cancer_data %>% exp_survival_forest(NULL, `status`, `age`, `sex_category`, predictor_funs = list(`age`="none", `sex`="none"), start_time = `Start_Date`, end_time = `End_Date`, time_unit = "day", test_split_type = "random")
+  res <- model_df %>% tidy_rowwise(model, type='partial_dependence')
+  predicted_df <- res %>% filter(type=="Prediction")
+  expect_true(predicted_df$value[1] == "Female")
+})
+
