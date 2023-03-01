09da13ef5f7ebfca351724148f18712334781f18
Hideaki Hayashi <hihayash@gmail.com>
2023-03-01 07:07:01 -0800

    Add test for ANCOVA with repeat-by

--- tests/testthat/test_test_wrapper_1.R
+++ tests/testthat/test_test_wrapper_1.R
@@ -672,6 +672,23 @@ test_that("test ANCOVA with exp_anova", {
                c("statistic", "p.value", "method"))
 })
 
+test_that("test ANCOVA with repeat-by", {
+  mtcars2 <- mtcars %>% mutate(`a m`=factor(am), `w t`=wt, `q sec`=qsec) %>% group_by(vs)
+  model_df <- mtcars2 %>% exp_anova(mpg, `a m`, covariates=c("w t", "q sec"),
+                                    covariate_funs=list("w t"="log", "q sec"="none"),
+                                    with_interaction = TRUE)
+  ret <- model_df %>% tidy_rowwise(model, type="model")
+  ret <- model_df %>% tidy_rowwise(model, type="shapiro")
+  ret <- model_df %>% tidy_rowwise(model, type="levene")
+  ret <- model_df %>% tidy_rowwise(model, type="levene", levene_test_center="mean")
+  ret <- model_df %>% tidy_rowwise(model, type="emmeans", pairs_adjust="tukey", sort_factor_levels=TRUE)
+  ret <- model_df %>% tidy_rowwise(model, type="pairs", pairs_adjust="tukey")
+  ret <- model_df %>% tidy_rowwise(model, type="prob_dist")
+  ret <- model_df %>% tidy_rowwise(model, type="anova")
+  ret <- model_df %>% tidy_rowwise(model, type="data", sort_factor_levels=TRUE)
+  ret <- model_df %>% tidy_rowwise(model, type="data_summary")
+})
+
 test_that("test ANCOVA with exp_anova with some NAs in the data", {
   mtcars2 <- mtcars %>% mutate(`a m`=factor(am), `w t`=wt, `q sec`=qsec)
   mtcars2$`a m`[[1]] <- NA
