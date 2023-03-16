7a1a0c8fc360b927580ec307ae094aed1d8eaafc
Hideaki Hayashi <hihayash@gmail.com>
2023-03-16 15:03:53 -0700

    Cover more tidiers for bayes A/B test with tests

--- tests/testthat/test_exp_bayes_ab.R
+++ tests/testthat/test_exp_bayes_ab.R
@@ -24,6 +24,39 @@ test_that("test exp_bayes_ab test with summary output", {
   full_data_logical_ab <- full_data %>% dplyr::mutate(`gro up` = `gro up` == "a")
 
   # revert A/B - character case
+  model_df <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01)
+  res <- model_df %>% tidy_rowwise(model, type = "summary", pretty.name = TRUE)
+  res <- model_df %>% tidy_rowwise(model, type = "improvement")
+  res <- model_df %>% tidy_rowwise(model, type = "posteriors")
+  res <- model_df %>% tidy_rowwise(model, type = "prior")
+  res <- model_df %>% tidy_rowwise(model, type = "observed")
+  expect_equal(colnames(res),c("grouping","ab_identifier","converted","observed"))
+})
+
+test_that("test exp_bayes_ab test with summary output", {
+  set.seed(0)
+  data_a <- data.frame(
+    access_count = round(runif(50) * 100 + 100)
+  ) %>%
+    dplyr::mutate(click = 0.2 + 0.8 * runif(nrow(.)), `gro up` = "a")
+
+  data_a$click[[5]] <- NA # inject NA for test
+
+  data_b <- data.frame(
+    access_count = round(runif(20) * 20 + 20)
+  ) %>%
+    dplyr::mutate(click = 0.3 + 0.7 * runif(nrow(.)), `gro up` = "b")
+
+  full_data <- dplyr::bind_rows(data_a, data_b) %>%
+    mutate(grouping = rbinom(n(), 1, 0.3)) %>%
+    group_by(grouping)
+  full_data <- full_data %>%
+    dplyr::mutate(converted_true=as.integer(access_count*click), converted_false=as.integer(access_count*(1-click))) %>%
+    tidyr::gather(key="conver ted", value="count col", converted_true, converted_false) %>%
+    dplyr::mutate(`conver ted` = `conver ted`=="converted_true")
+  full_data_logical_ab <- full_data %>% dplyr::mutate(`gro up` = `gro up` == "a")
+
+  # revert A/B - character case
   ret1 <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, type = "summary")
   ret2 <- exp_bayes_ab(full_data, `conver ted`, `gro up`, `count col`, prior_mean=0.1, prior_sd=0.01, revert_ab = T, type = "summary")
   expect_equal(ret1$ab_identifier, c("a", "b", "a", "b"))
@@ -62,6 +95,8 @@ test_that("test exp_bayes_ab_aggregated", {
   res <- model_df %>% tidy_rowwise(model, type = "improvement")
   res <- model_df %>% tidy_rowwise(model, type = "posteriors")
   res <- model_df %>% tidy_rowwise(model, type = "prior")
+  res <- model_df %>% tidy_rowwise(model, type = "observed")
+  expect_equal(colnames(res),c("ab_identifier","converted","observed"))
 })
 
 #TODO: do the same set of tests as test_do_bayes_ab.R
