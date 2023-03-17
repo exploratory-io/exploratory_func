a44652bc2969038902c6fe0e6e8c17e98a27e412
Hideaki Hayashi <hihayash@gmail.com>
2023-03-17 11:29:46 -0700

    Add test for corner cases

--- tests/testthat/test_exp_bayes_ab.R
+++ tests/testthat/test_exp_bayes_ab.R
@@ -1,5 +1,4 @@
 context("test exp_bayes_ab")
-
 test_that("test exp_bayes_ab test with summary output", {
   set.seed(0)
   data_a <- data.frame(
@@ -87,7 +86,6 @@ test_that("test exp_bayes_ab test with summary output", {
 
 test_that("test exp_bayes_ab_aggregated", {
   df <- tibble::tibble(cat=c('A','B'), n=c(100,200), cr=c(0.22, 0.2))
-
   # revert A/B - character case
   model_df <- df %>% exp_bayes_ab_aggregated(cat, cr, n, prior_mean=0.1, prior_sd=0.01, seed=0)
   res <- model_df %>% tidy_rowwise(model, type = "summary", pretty.name = TRUE)
@@ -96,6 +94,30 @@ test_that("test exp_bayes_ab_aggregated", {
   res <- model_df %>% tidy_rowwise(model, type = "prior")
   res <- model_df %>% tidy_rowwise(model, type = "observed")
   expect_equal(colnames(res),c("ab_identifier","converted","observed"))
+})
+
+test_that("test exp_bayes_ab_aggregated with multiple rows per group", {
+  # Here we test the case where a group (A or B) has more than 1 row.
+  # It's not the most common use we expect, but this should also work.
+  df <- tibble::tibble(cat=rep(c('A','B'),2), n=rep(c(100,200),2), cr=rep(c(0.22, 0.2),2))
+  # revert A/B - character case
+  model_df <- df %>% exp_bayes_ab_aggregated(cat, cr, n, prior_mean=0.1, prior_sd=0.01, seed=0)
+  res <- model_df %>% tidy_rowwise(model, type = "summary", pretty.name = TRUE)
+  expect_equal(res$`Number of Rows`,c(200,400)) # Number of rows should be added up.
+  expect_equal(res$`Conversion Rate`,c(0.22,0.2)) # Ratios should be weight-averaged.
+  res <- model_df %>% tidy_rowwise(model, type = "improvement")
+  res <- model_df %>% tidy_rowwise(model, type = "posteriors")
+  res <- model_df %>% tidy_rowwise(model, type = "prior")
+  res <- model_df %>% tidy_rowwise(model, type = "observed")
+  expect_equal(colnames(res),c("ab_identifier","converted","observed"))
+})
+
+test_that("test exp_bayes_ab_aggregated with multiple rows per group", {
+  df <- tibble::tibble(cat=c('A','B','C'), n=c(100,200,300), cr=c(0.22, 0.2, 0.2))
+  # revert A/B - character case
+  expect_error({
+    model_df <- df %>% exp_bayes_ab_aggregated(cat, cr, n, prior_mean=0.1, prior_sd=0.01, seed=0)
+  }, "A/B must be 2 unique identifiers")
 })
 
 #TODO: do the same set of tests as test_do_bayes_ab.R
