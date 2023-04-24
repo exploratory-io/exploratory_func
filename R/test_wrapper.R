8735ac40bf69e5b2bca35ef75a9fd3997a6cd54b
Hideaki Hayashi <hihayash@gmail.com>
2023-04-24 12:37:24 -0700

    Map the variable names in the term column back to the original.

--- R/test_wrapper.R
+++ R/test_wrapper.R
@@ -2018,6 +2018,13 @@ tidy.anova_exploratory <- function(x, type="model", conf_level=0.95, pairs_adjus
       }
     }
     ret <- ret %>% mutate(p.value=pf(approx.F, num.Df, den.Df, lower.tail = FALSE))
+    # Map the variable names in the term column back to the original.
+    terms_mapping <- x$terms_mapping
+    # Add mapping for interaction term
+    terms_mapping <- c(terms_mapping,c(`c2_:c3_`=paste0(terms_mapping["c2_"], " * ", terms_mapping["c3_"])))
+    orig_term <- terms_mapping[ret$term]
+    orig_term[is.na(orig_term)] <- ret$term[is.na(orig_term)] # Fill the element that did not have a matching mapping. (Should be "Residual")
+    ret$term <- orig_term
   }
   else if (type == "sphericity") {
     summary_x <- summary(x$Anova)
