a8eca51636473a5e5119ca7fa3e9752f3168fd03
Hide Kojima <hidekoji@gmail.com>
2024-04-02 07:45:51 -0700

    Updated test case.

--- tests/testthat/test_google_sheets.R
+++ tests/testthat/test_google_sheets.R
@@ -2,8 +2,9 @@ context("test for google_sheets functions")
 
 test_that("normalizeDataForGoogleSheetsExport", {
   df <- read_rds_file("https://www.dropbox.com/s/06jmg6k4lnb61p2/flight300.rds?dl=1")
+  df <- df %>% mutate(tstint64 = bit64::as.integer64(1234))
   df_cleaned <- df %>% exploratory::normalizeDataForGoogleSheetsExport()
   expect_equal(class(df_cleaned$`hms 表`), "numeric")
   expect_equal(class(df_cleaned$`difftime 表`), "numeric")
-  expect_equal(class(df_cleaned$numAllInf), "logical")
+  expect_equal(class(df_cleaned$tstint64), "numeric")
 })
