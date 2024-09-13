b6312ed9d0f08172b483bd4067766faf697cb979
Kei Saito <kei.saito@gmail.com>
2024-09-12 22:20:08 -0700

    Remove anonymizer dependency

--- R/importer.R
+++ R/importer.R
@@ -22,11 +22,11 @@ urltools::url_decode
 #' @export
 psych::logistic
 
-#' @importFrom anonymizer anonymize
-#' @export
-anonymize <- function(vec, algo = "sha256", seed = 1, chars = letters, n_chars = 5L, ...){
-  anonymizer::anonymize(vec, .algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
-}
+# #' @importFrom anonymizer anonymize
+# #' @export
+# anonymize <- function(vec, algo = "sha256", seed = 1, chars = letters, n_chars = 5L, ...){
+#   anonymizer::anonymize(vec, .algo = algo, .seed = seed, .chars = chars, .n_chars = n_chars, ...)
+# }
 
 # Importing tidy is needed to define tidy methods to other classes
 #' @importFrom broom tidy
