aa5f5cb68cf5e8d671e96e0b049ff2549c6cf9af
Hide Kojima <hidekoji@gmail.com>
2025-07-22 13:20:20 -0700

    Revert "Refactor text processing functions in textanal.R to ensure case-insensitive keyword matching. Added new test cases for English data handling in test_textanal.R."
    This reverts commit 0a86a48990b465c3f29c3d73804e90b6d7d227bb.

--- tests/testthat/test_textanal.R
+++ tests/testthat/test_textanal.R
@@ -8,15 +8,6 @@ nps_raw <- exploratory::read_delim_file("https://www.dropbox.com/scl/fi/7iur1jvy
 
 nps_cluster <- exploratory::read_delim_file("https://www.dropbox.com/scl/fi/c6saij8if1iq76yfo2v0d/NPS_cluster.csv?rlkey=3lajklwltbe5tnijot1iujr7b&dl=1", ",", quote = "\"", skip = 0 , col_names = TRUE , na = c("","NA") , locale=readr::locale(encoding = "UTF-8", decimal_mark = "."), trim_ws = FALSE , progress = FALSE) %>% exploratory::clean_data_frame()
 
-
-Word_Size_Custler_Tmp <- tempfile(fileext = ".rds")
-download.file("https://www.dropbox.com/scl/fi/s7g3rf7td0f207jsg627w/Word-Size-Cluster.rds?rlkey=bbm01rxf6y7uhirz5flic08xw&dl=1", destfile = Word_Size_Custler_Tmp)
-Word_Size_Cluster <- readRDS(Word_Size_Custler_Tmp)
-
-Survey_English_raw_Tmp <- tempfile(fileext = ".rds")
-download.file("https://www.dropbox.com/scl/fi/tk33gojc1la86zp3nwrms/Survey_text_data_english.rds?rlkey=w4t7doqg9cvnxgz24qhmk4u52&dl=1", destfile = Survey_English_raw_Tmp)
-Survey_English_raw <- readRDS(Survey_English_raw_Tmp)
-
 test_that("exp_textanal with Japanese twitter data", {
   lang_res <- exploratory:::guess_lang_for_stopwords(twitter_df$text)
 
@@ -120,12 +111,4 @@ test_that("exp_get_top5_sentences_for_cluster", {
   expect_equal(nrow(df), 164)
   expect_equal(ncol(df), 5)
   expect_equal(df$`よりよくするための提案`[[3]], c("特にはないですが、もっとアップデート内容を聞きたかったです！ みなさんの発表も参考になりました。"))
-})
-
-test_that("exp_get_top5_sentences_for_cluster: Englihs", {
-  df <- exp_get_top5_sentences_for_cluster(Survey_English_raw, Word_Size_Cluster, "what would it be? (Multiple answers are acceptable)")
-  expect_equal(nrow(df), 30)
-  expect_equal(ncol(df), 2)
-  expect_equal(df$cluster, c("1","1","1","1","1","2","2","2","2","2","3","3","3","3","3","4","4","4","4","4","5","5","5","5","5","6","6","6","6","6"))
-  expect_equal(df$`what would it be? (Multiple answers are acceptable)`[[1]], c("I think it would be better to put some restrictions on the presentation time and the number of presentation slides so that presentation finish on time."))
 })
