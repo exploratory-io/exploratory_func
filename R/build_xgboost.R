33bb22f0926441b62c3f20aecbecedbbc5f26cea
Hide Kojima <hidekoji@gmail.com>
2026-01-06 23:04:58 -0800

    Refactor synthesized column removal in exp_xgboost function
    - Updated the logic for removing the synthesized column to use dplyr::any_of for improved robustness against column name variations.

--- R/build_xgboost.R
+++ R/build_xgboost.R
@@ -1192,7 +1192,7 @@ exp_xgboost <- function(df,
         } else if (smote_applied) {
           # SMOTE was applied but not keeping synthetic samples in output
           # Remove synthesized column
-          df <- df %>% dplyr::select(-synthesized)
+          df <- df %>% dplyr::select(-dplyr::any_of("synthesized"))
         }
       }
 
