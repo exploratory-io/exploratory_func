868025229a4d54c155eb3b36f5f41e1a9bae429b
Hide Kojima <hidekoji@gmail.com>
2026-08-09 15:09:03 -0700

    test: cover K-Medoids report tidy outputs

--- tests/testthat/test_kmedoids.R
+++ tests/testthat/test_kmedoids.R
@@ -47,7 +47,8 @@ test_that('report tidy types are available', {
   result <- mtcars %>% exploratory:::exp_kmedoids(mpg, disp, hp, centers = 3, seed = 1)
   model <- result$model[[1]]
   types <- c('profile', 'silhouette', 'elbow', 'variable_importance',
-    'representative_values', 'distribution', 'cohesion', 'map', 'data')
+    'representative_values', 'distribution', 'cohesion', 'map',
+    'medoid_details', 'counts', 'data')
 
   output <- lapply(types, function(type) broom::tidy(model, type = type))
   names(output) <- types
@@ -60,6 +61,8 @@ test_that('report tidy types are available', {
   expect_true(all(c('cluster', 'variable', 'value') %in% colnames(output$distribution)))
   expect_true(all(c('cluster', 'row_id', 'mpg', 'disp', 'hp') %in%
     colnames(broom::tidy(model, type = 'medoid_details'))))
+  expect_equal(nrow(output$counts), 1)
+  expect_equal(output$counts$original_nrow, nrow(mtcars))
   expect_true(all(c('row_id', 'cluster', 'is_medoid', 'distance_to_medoid',
     'silhouette_score', 'is_excluded') %in% colnames(output$data)))
 })
